1ae499f669226574ae8f7ead52374207f3c5b056
fr.sanfilippo@gmail.com <fr.sanfilippo@gmail.com@35dfebc6-1a16-7261-d8b2-d758fa207421>
2010-11-07 19:12:46 +0000

    qua e la

---
+++ proto_nissa_conf.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+WHERE=AURORA/ROMA3/JUGENE
+
+#this is the path to the programs (ahmidas, tmLQCD)
+base_programs=/home/prace/Prace/Programs
+    
+#nissa location
+base_nissa=/home/prace/nissa
+    
+source $base_nissa/nissa_conf.sh
