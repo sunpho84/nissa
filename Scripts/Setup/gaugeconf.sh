d8e443b87edaab0c3267ef7ff9b05aaf17e2c512
fr.sanfilippo <fr.sanfilippo@35dfebc6-1a16-7261-d8b2-d758fa207421>
2010-11-16 18:02:31 +0000

    asfafesagfeasgvfae

--- Scripts/Setup/gaugeconf.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo ------loading configuration parameters-------
-source gaugeconf_parameters.sh
-
-base_conf=$base_analysis/$confno
-echo -e Conf\#:\\t $confno
-echo -e Path: \\t $base_conf
-echo
