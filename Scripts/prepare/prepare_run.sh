85c7c0968103de53d5c13cdba4bce7cd15a12acc
fr.sanfilippo <fr.sanfilippo@35dfebc6-1a16-7261-d8b2-d758fa207421>
2010-11-16 23:41:11 +0000

    hblh

--- Scripts/prepare/prepare_run.sh
+++ Scripts/prepare/prepare_run.sh
@@ -14,7 +14,14 @@ then
 	echo "use_external_additive_time_offset=1 #idem"
     ) > run_instructions.sh
 
-    echo "Adapt the file 'run_instructions.sh' and rerun"
+    if [ $WHERE == "JUGENE" ]
+    then
+	cp $base_script/proto/work_jugene_header.sh work_header.sh
+    else
+	touch work_header.sh
+    fi
+
+    echo "Adapt the file 'run_instructions.sh' and 'work_header.sh' (if needed) and rerun"
     echo
     exit
 else
@@ -44,16 +51,20 @@ do
 	    additive_time_offset=0
 	fi
 
+	cat ../work_header.sh
+
 	echo "#!/bin/bash"
 	echo "confno="$conf
 	echo "additive_seed="$additive_seed
 	echo "additive_time_offset="$additive_time_offset
-    ) > gauge_parameters.sh
+
+	echo "source ../analysis.sh"
+    ) > work.sh
     cd -
 done
 
-sed 's|SED_L|'$L'|;s|SED_T|'$T'|;s|SED_Base_analysis|'$PWD'|' $base_scripts/proto/proto_analysis_parameters.sh > analysis_parameters.sh
+sed 's|SED_L|'$L'|;s|SED_T|'$T'|;s|SED_Base_analysis|'$PWD'|' $base_scripts/proto/proto_analysis.sh > analysis.sh
 
 echo
-echo "Now adapt the 'analysis_parameters.sh' and delete 'run_instructions.sh'"
+echo "Now adapt the 'analysis.sh' and delete 'run_instructions.sh'"
 echo
