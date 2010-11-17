982605b0541e1097f8406fb2e252f016a57eb2d2
fr.sanfilippo <fr.sanfilippo@35dfebc6-1a16-7261-d8b2-d758fa207421>
2010-11-17 22:05:10 +0000

    ewegvewgewgewgew

--- nissa_conf.sh
+++ nissa_conf.sh
@@ -22,16 +22,15 @@ then
     #if the job have been sent to a queque, cd to the folder
     #nb: all the job should be called on the folder containing the 
     #analysis for the current configuration
-    if [ "$PBS_O_WORKDIR" != "" ]
+    if [ "$USE_MPI" != "1" ]
     then
 	base_ahmidas=$base_ahmidas_pa
 	base_tmLQCD=$base_tmLQCD_pa
-	cd $PBS_O_WORKDIR
-	echo "We are in a job queque"
+	echo "We'll use MPI"
     else
 	base_ahmidas=$base_ahmidas_fe
 	base_tmLQCD=$base_tmLQCD_fe
-	echo "Not in a job queque"
+	echo "We'll not use MPI"
     fi
     
     echo
