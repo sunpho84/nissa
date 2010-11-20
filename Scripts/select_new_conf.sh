2d72ed50155ef7ebd3bfa1be8afe85c0d4d16f99
fr.sanfilippo <fr.sanfilippo@35dfebc6-1a16-7261-d8b2-d758fa207421>
2010-11-20 13:39:55 +0000

    sfwefew

--- Scripts/select_new_conf.sh
+++ Scripts/select_new_conf.sh
@@ -1,20 +1,23 @@
-if [ ! -f conf_list ]
+if [ ! -f "$conf_list_file" ]
 then
-    echo "Configuration list 'conf_list' not present!!!"
+    echo "Configuration list '"$conf_list_file"' not present!!!"
     exit
 fi
-conf_list=$(cat conf_list)
+pwd
+conf_list=$(cat $conf_list_file)
 for conf_test in $conf_list
 do
-    if [ ! -d $conf_test ]
+    if [ ! -f "$conf_test/analysis_"$analysis_name"_completed" ] $$ [ ! -f "$conf_test/analysis_"$analysis_name"_running" ]
     then
         conf=$conf_test
-        echo "Will work on "$conf
+        echo "Will work on conf: "$conf
+	touch "$conf_test/analysis_"$analysis_name"_running"
+	trap "rm -f "$conf_test"/analysis_"$analysis_name"_running" EXIT
         break
     fi
 done
 if [ "$conf" == "" ]
 then
-    echo "All configuration calculated!"
+    echo "All configurations calculated!"
     exit
 fi
