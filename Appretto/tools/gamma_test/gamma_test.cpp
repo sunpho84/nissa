7360e7f7c5dd3d11ac039c1b9e78e81f2081fa48
fr.sanfilippo <fr.sanfilippo@35dfebc6-1a16-7261-d8b2-d758fa207421>
2010-11-23 12:00:10 +0000

    updated

--- Appretto/tools/gamma_test/gamma_test.cpp
+++ Appretto/tools/gamma_test/gamma_test.cpp
@@ -13,9 +13,22 @@ int main(int narg,char **arg)
 
   if(rank==0)
     {
-      print_gamma(base_gamma[0]);
+      for(int i=0;i<=5;i++)
+	{
+	  cout<<"Gamma"<<i<<endl;
+	  print_gamma(base_gamma[i]);
+	  cout<<endl;
+	}
+
+      gamma gamma6;
+      gamma_prod(gamma6,base_gamma[1],base_gamma[5]);
+      cout<<"Gamma6=Gamma1Gamma5 as:"<<endl;
+      cout<<"(a) defined by Silvano"<<endl;
+      print_gamma(base_gamma[6]);
+      cout<<"(b) obtained by making explicite products"<<endl;
+      print_gamma(gamma6);
       cout<<endl;
     }
-
+  
   return 0;
 }
