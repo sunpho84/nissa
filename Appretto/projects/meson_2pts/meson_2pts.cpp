753c8a2f62e8fc06dcd289cfc161e6f15f205ab2
fr.sanfilippo <fr.sanfilippo@35dfebc6-1a16-7261-d8b2-d758fa207421>
2010-11-25 09:11:55 +0000

    Adjusted 2 prs

--- Appretto/projects/meson_2pts/meson_2pts.cpp
+++ Appretto/projects/meson_2pts/meson_2pts.cpp
@@ -1,9 +1,9 @@
 
-//This program calculate a list of two points function contracting all
-//the propagators in the first list with all the propagators in the
-//second list. The propagators on the second list are loaded one by
-//one. The first list is split into block, each of them as large as
-//much it is possible.
+//This program calculates a list of two-point functions contracting 
+//all the propagators in the first list with all the propagators in
+//the second list. The propagators of the second list are loaded one 
+//by one. The first list is split into blocks, each of them as large 
+//as possible.
 
 #include <mpi.h>
 #include <fstream>
@@ -13,10 +13,10 @@
 
 using namespace std;
 
-//Calculate the number of blocks in which to divide the first list
-int compute_nblocks_first_list(int nprop_list1)
+//Calculate the maximum number of allocable propagators
+int compute_allocable_propagators(int nprop_list)
 {
-  //First of all it checks if there is enough room for two propagators.
+  //First of all check if there is enough room for two propagators.
   //This is the minimal requirement for the program to be able to work.
   colorspinspin *fuf;
   fuf=(colorspinspin*)malloc(2*sizeof(colorspinspin)*loc_vol);
@@ -26,31 +26,30 @@ int compute_nblocks_first_list(int nprop_list1)
       if(rank==0) cerr<<"Error: not enough memory for two propagators"<<endl;
       MPI_Abort(MPI_COMM_WORLD,1);
     }
-  else if(debug and rank==0) cout<<"Ok there is enough memory to load 2 propagators"<<endl;
+  else if(debug and rank==0) cout<<"Ok there is enough memory to load two propagators"<<endl;
 
   free(fuf);
 
   //Now determine the largest number of propagator of the first list (and one additional) loadable at once.
-  //We are sure that we can allocate at least 2 props, so it will exit with at least output=1.
-  int nblocks=0;
-  int nprop_per_block;
+  //We are sure that we can allocate at least 2 props, so it will exit with at least nprop_max=1.
+  int nprop_max=nprop_list + 1;
   do
     {
-      nblocks++;
-      nprop_per_block=nprop_list1/nblocks;
-      fuf=(colorspinspin*)malloc((nprop_per_block+1)*sizeof(colorspinspin)*loc_vol);
+      nprop_max--;
+      fuf=(colorspinspin*)malloc((nprop_max+1)*sizeof(colorspinspin)*loc_vol);
     }
   while(fuf==NULL);
 
-  if(debug and rank==0)
-    cout<<"Will split the "<<nprop_list1<<" propagators in the first list in "<<nblocks<<" blocks of "<<nprop_per_block<<" each"<<endl;
+  free(fuf);
 
-  return nblocks;
+  if(debug and rank==0)
+    cout<<"Will allocate "<<nprop_max<<" propagators from a list with "<<nprop_list<<" propagators"<<endl;
+
+  return nprop_max;
 }
 
-//Wrapper to the real calculation
-//This function takes care to make the revert on the SECOND spinor, putting both the needed gamma5
-//It also put the rotators to the physical basis if asked
+//This function takes care to make the revert on the SECOND spinor, putting the needed gamma5
+//It also applies the appropriate rotators to the physical basis if asked
 void meson_two_points(complex *corr,int *list_op1,colorspinspin *s1,int *list_op2,colorspinspin *s2,int ncontr,int f1,int r1,int f2,int r2)
 {
   //Temporary vectors for the internal gamma
@@ -63,10 +62,14 @@ void meson_two_points(complex *corr,int *list_op1,colorspinspin *s1,int *list_op
       dirac_prod(t1[icontr], base_gamma[5],base_gamma[list_op1[icontr]]);
       dirac_prod(t2[icontr], base_gamma[list_op2[icontr]],base_gamma[5]);
       
-      //Remember that D- rotate as 1+ig5, but D-^-1 rotate ad 1-ig5,
-      //morover (D^-1)^dagger rotate again as 1+ig5 (pweee!!!)
+      //Remind that D- rotates as 1+ig5, but D-^-1 rotates as 1-ig5,
+      //moreover (D^-1)^dagger rotates again as 1+ig5 (pweee!!!)
 
-      if(f1!=1)
+      //f1 < 1: do rotation for a quark propagator
+      //f1 = 1: do nothing (physical basis already)
+      //f1 > 1: do rotation for a (charged) sequential propagator
+
+      if(f1<1)
 	switch(r1)
 	  {
 	  case 0: //This is D-^-1
@@ -79,7 +82,20 @@ void meson_two_points(complex *corr,int *list_op1,colorspinspin *s1,int *list_op
 	    break;
 	  }
 
-      if(f2!=1)
+      if(f1>1)
+        switch(r1)
+          {
+          case 0: //This is D-^-1
+            dirac_prod(t1[icontr], t1[icontr],Pminus);
+            dirac_prod(t2[icontr], Pplus,t2[icontr]);
+            break;
+          case 1: //This is D+^-1
+            dirac_prod(t1[icontr], t1[icontr],Pplus);
+            dirac_prod(t2[icontr], Pminus,t2[icontr]);
+            break;
+          }
+
+      if(f2<1)
 	switch(r2)
 	  {
 	  case 0: //This is (D-^-1)^dagger
@@ -91,9 +107,23 @@ void meson_two_points(complex *corr,int *list_op1,colorspinspin *s1,int *list_op
 	    dirac_prod(t1[icontr], Pminus,t1[icontr]);
 	    break;
 	  }
+
+     if(f2>1)
+        switch(r2)
+          {
+          case 0: //This is (D-^-1)^dagger
+            dirac_prod(t2[icontr], t2[icontr],Pplus);
+            dirac_prod(t1[icontr], Pminus,t1[icontr]);
+            break;
+          case 1: //This is (D+^-1)^dagger
+            dirac_prod(t2[icontr], t2[icontr],Pminus);
+            dirac_prod(t1[icontr], Pplus,t1[icontr]);
+            break;
+          }
+
     }
   
-  //Call for the routine which do the real contraction
+  //Call for the routine which does the real contraction
   trace_g_s_g_sdag(corr,t1,s1,t2,s2,ncontr);
 }
 
@@ -114,16 +144,16 @@ int main(int narg,char **arg)
   read_int("L",glb_size[1]);
   read_int("T",glb_size[0]);
 
-  //Read the position of the source
+  //Read the time location of the source
   int twall;
   read_int("TWall",twall);
 
-  //Read the number of propagator of the first list
+  //Read the number of propagators of the first list
   int nprop_list1;
   read_int("NPropFirstlist",nprop_list1);
   if(rank==0) cout<<"Nprop of the first list: "<<nprop_list1<<endl;
 
-  //Read the name, mass etc of the first list
+  //Read the name, mass, theta and other flags for the first list
   char base_filename1[nprop_list1][1024];
   double mass_prop1[nprop_list1];
   double theta_prop1[nprop_list1];
@@ -141,12 +171,12 @@ int main(int narg,char **arg)
 	cout<<" prop "<<iprop<<", m="<<mass_prop1[iprop]<<" th="<<theta_prop1[iprop]<<" phys="<<phys_prop1[iprop]<<" r="<<r_prop1[iprop]<<endl;
     }
       
-  //Read the number of propagator of the first list
+  //Read the number of propagators of the second list
   int nprop_list2;
   read_int("NPropSecondlist",nprop_list2);
   if(rank==0) cout<<"Nprop of the first list: "<<nprop_list2<<endl;
 
-  //Read the name, mass etc of the first list
+  //Read the name, mass, theta and other flags for the second list
   char base_filename2[nprop_list2][1024];
   double mass_prop2[nprop_list2];
   double theta_prop2[nprop_list2];
@@ -174,7 +204,7 @@ int main(int narg,char **arg)
   int *op2=new int[ncontr]; 
   for(int icontr=0;icontr<ncontr;icontr++)
     {
-      //Read the operators pairs
+      //Read the operator pairs
       read_int(op1[icontr]);
       read_int(op2[icontr]);
 
@@ -186,10 +216,10 @@ int main(int narg,char **arg)
   //Init the MPI grid 
   init_grid();
   
-  //Calculate the number of blocks in which to divide the first list
-  int nblocks=compute_nblock_first_list(nprop_list1);
-  int nprop_per_block=nprop_list1/nblocks;
-  if(nprop_list1>nblock*nprop_per_block) nblocks++;
+  //Calculate the number of blocks for the first list
+  int nprop_per_block=compute_allocable_propagators(nprop_list1);
+  int nblocks=nprop_list1/nprop_per_block;
+  if(nprop_list1>nblocks*nprop_per_block) nblocks++;
 
   //allocate the spinors
   colorspinspin *spinor1[nprop_per_block];
@@ -207,8 +237,8 @@ int main(int narg,char **arg)
     }
 
   ofstream fout;
-  fout.precision(12);
-  fout.width(12);
+  fout.precision(16);
+  fout.width(16);
   if(rank==0) fout.open("two_points_correlations");
   int spat_vol=glb_size[1]*glb_size[1]*glb_size[1];
 
@@ -231,7 +261,20 @@ int main(int narg,char **arg)
       for(int iprop2=0;iprop2<nprop_list2;iprop2++)
 	{
 	  //read the second propagator one by one
-	  read_colorspinspin(base_filename2[iprop2],spinor2);
+	  colorspinspin *spinor2_ptr; //This will point to spinor2 if the prop. is not in the first list
+	  //check if the file is already loaded
+	  spinor2_ptr=spinor2;
+	  for(int iprop1=0;iprop1<iblock_length;iprop1++)
+	    {
+	      int counter=iblock_first+iprop1;
+	      if(strcmp(base_filename1[counter],base_filename2[iprop2])==0)
+		{
+		  spinor2_ptr=spinor1[iprop1];
+		  if(debug and rank==0) cout<<"Propagator "<<base_filename2[iprop2]<<" found in the position "<<counter<<" of the first list"<<endl;
+		}
+	    }
+	  //if not found in the first list, load it
+	  if(spinor2_ptr==spinor2) read_colorspinspin(base_filename2[iprop2],spinor2);
 	  
 	  //Calculate all the two points between spinor 1 and spinor2
 	  for(int iprop1=0;iprop1<iblock_length;iprop1++)
@@ -241,9 +284,9 @@ int main(int narg,char **arg)
 	      if(rank==0)
 		fout<<" #"
 		    <<" m1="<<mass_prop1[counter]<<" th1="<<theta_prop1[counter]<<" r1="<<r_prop1[counter]<<" ,"
-		    <<" m2="<<mass_prop2[iprop2]<<" th2="<<theta_prop2[iprop2]<<" r2="<<r_prop2[iprop2]<<endl<<endl;
+		    <<" m2="<<mass_prop2[iprop2] <<" th2="<<theta_prop2[iprop2] <<" r2="<<r_prop2[iprop2]<<endl;
 
-	      meson_two_points((complex*)contr,op1,spinor1[iprop1],op2,spinor2,ncontr,phys_prop1[counter],r_prop1[counter],phys_prop2[iprop2],r_prop2[iprop2]);
+	      meson_two_points((complex*)contr,op1,spinor1[iprop1],op2,spinor2_ptr,ncontr,phys_prop1[counter],r_prop1[counter],phys_prop2[iprop2],r_prop2[iprop2]);
 
 	      if(rank==0)
 		{
@@ -278,6 +321,8 @@ int main(int narg,char **arg)
     }
 
   ///////////////////////////////////////////
+
+ if(rank==0) fout.close();
 
   for(int iprop1=0;iprop1<nprop_per_block;iprop1++) delete[] spinor1[iprop1];
   delete[] spinor2;
