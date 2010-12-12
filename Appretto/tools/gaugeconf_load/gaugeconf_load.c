6fbd3c24b7626d9e4f4b22955dc4164a49c5aaca
fr.sanfilippo <fr.sanfilippo@35dfebc6-1a16-7261-d8b2-d758fa207421>
2010-12-12 23:38:32 +0000

    Added the clover term, tested that it is coherent with translation

--- Appretto/tools/gaugeconf_load/gaugeconf_load.c
+++ Appretto/tools/gaugeconf_load/gaugeconf_load.c
@@ -3,6 +3,24 @@
 
 #include "appretto.h"
 
+void test(quad_su3 *conf)
+{
+  double gplaq; 
+
+  gplaq=global_plaquette(conf);
+  if(rank==0) printf("plaq: %.10g\n",gplaq);
+
+  su3spinspin *clov=(su3spinspin*)malloc(sizeof(su3spinspin)*loc_vol);
+  clover_term(clov,conf);
+  double partial_summ=0;
+  for(int X=0;X<loc_vol;X++) partial_summ+=clov[X][0][0][0][0][0];
+  free(clov);
+  double total_leaves_summ;
+  MPI_Reduce(&partial_summ,&total_leaves_summ,1,MPI_DOUBLE,MPI_SUM,0,MPI_COMM_WORLD);
+
+  if(rank==0) printf("%.12g\n",total_leaves_summ);
+}
+
 int main(int narg,char **arg)
 {
   char filename[1024];
@@ -30,24 +48,23 @@ int main(int narg,char **arg)
 
   ///////////////////////////////////////////
 
-  quad_su3 *conf=(quad_su3*)malloc(sizeof(quad_su3)*(loc_vol+loc_bord+loc_edge));
-  for(int ivol=0;ivol<loc_vol+loc_bord;ivol++)
-    for(int idir=0;idir<4;idir++)
-      for(int ic=0;ic<3;ic++)
-	for(int ir=0;ir<3;ir++)
-	  conf[ivol][idir][ic][ir][0]=conf[ivol][idir][ic][ir][1]=0;
+  quad_su3 *conft=(quad_su3*)malloc(sizeof(quad_su3)*(loc_vol+loc_bord+loc_edge));
+  read_local_gauge_conf(conft,filename);  
+  communicate_gauge_borders(conft);
+  communicate_gauge_edges(conft);
+  test(conft);
+  
+  if(rank_tot==1)
+    {
+      quad_su3 *conf=(quad_su3*)malloc(sizeof(quad_su3)*(loc_vol+loc_bord+loc_edge));
+      ac_rotate_gaugeconf(conf,conft,1);
+      communicate_gauge_borders(conf);
+      communicate_gauge_edges(conf);
+      test(conf);
+      free(conf);
+    }
 
-  read_local_gauge_conf(conf,filename);
-  communicate_gauge_borders(conf);
-  communicate_gauge_edges(conf);
-  
-  su3spinspin *clov;
-  clover_term(clov,conf);
-  
-  double gplaq=global_plaquette(conf);
-  if(rank==0) printf("plaq: %.10g\n",gplaq);
-  
-  free(conf);
+  free(conft);
   
   ///////////////////////////////////////////
 
