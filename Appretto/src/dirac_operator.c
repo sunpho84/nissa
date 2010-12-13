947857b2c0054ec63e7fcf819062aaf4331849f5
fr.sanfilippo <fr.sanfilippo@35dfebc6-1a16-7261-d8b2-d758fa207421>
2010-12-13 01:55:07 +0000

    included the dirac_operator.c file

--- Appretto/src/dirac_operator.c
+++ Appretto/src/dirac_operator.c
@@ -2,6 +2,9 @@
 
 //Apply the dirac operator to a spincolor
 //it is assumed that boundary condition has been already adjusted outside
-void apply_dirac_operator(spincolor *out,spincolor *in,quad_su3_su3 *conf,double kappac,double m)
+void apply_dirac_operator(spincolor *out,spincolor *in,quad_su3 *conf,double kappac,double m)
 {
+  for(int ivol=0;ivol<loc_vol;ivol++)
+    {
+    }
 }
