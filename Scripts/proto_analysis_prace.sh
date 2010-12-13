e4aa52f757d3be29d77814251989b0de6f6073fb
simula@roma3.infn.it <simula@roma3.infn.it@35dfebc6-1a16-7261-d8b2-d758fa207421>
2010-12-13 19:53:21 +0000

    warning on theta

--- Scripts/proto_analysis_prace.sh
+++ Scripts/proto_analysis_prace.sh
@@ -55,7 +55,7 @@ list_source_seed=( [seed] ) # seed
 list_source_prec=( [resd] ) # residual
 
 #list of theta and sea for which we will do first inversion
-list_theta=( [theta1] [theta2] ) #multiples of 2pi
+list_theta=( [theta1] [theta2] ) #multiply by pi/L (periodic = 2)
 list_mu=( [mu1] [mu2] )
 beta=[beta]
 kappa=[kappac]
