3a7390ecf27518ddddbb7cac1a47143712802edd
fr.sanfilippo <fr.sanfilippo@35dfebc6-1a16-7261-d8b2-d758fa207421>
2010-12-15 18:15:02 +0000

    bk calculation

--- Appretto/src/dirac_operator.c
+++ Appretto/src/dirac_operator.c
@@ -8,3 +8,21 @@ void apply_dirac_operator(spincolor *out,spincolor *in,quad_su3 *conf,double kap
     {
     }
 }
+
+//Apply the D+D- operator to a spincolor
+//if no temporary spinor is provided, it will be allocated inside
+void apply_DDdag_operator(spincolor *out,spincolor *in,spincolor *temp,quad_su3 *conf,double kappac,double m)
+{
+  int all=0;
+  if(temp==NULL)
+    {
+      all=1;
+      temp=(spincolor*)malloc(sizeof(spincolor)*(loc_vol+loc_bord));
+    }
+  
+  apply_dirac_operator(temp,in,conf,kappac,+m);
+  //to be added: comunication of the borders
+  apply_dirac_operator(out,temp,conf,kappac,-m);
+  
+  if(all!=0) free(temp);
+}
