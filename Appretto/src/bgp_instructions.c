e8d2b7ba0ad96a02b2b6f167a6c16e62cbf07f20
fr.sanfilippo <fr.sanfilippo@35dfebc6-1a16-7261-d8b2-d758fa207421>
2010-12-29 00:00:57 +0000

    finished to assembly the dirac operator application

---
+++ Appretto/src/bgp_instructions.c
@@ -0,0 +1,184 @@
+#pragma once
+
+#include "su3.c"
+
+#define bgp_aligned __attribute__ ((aligned (16)))
+
+#define bgp_cache_touch_su3(U) \
+  __dcbt(((char*)((unsigned long int)(U))));     \
+  __dcbt(((char*)((unsigned long int)(U)))+32);  \
+  __dcbt(((char*)((unsigned long int)(U)))+64);  \
+  __dcbt(((char*)((unsigned long int)(U)))+96);  \
+  __dcbt(((char*)((unsigned long int)(U)))+128); 
+
+#define bgp_load_complex(A,B) A=__lfpd((double*)B)
+#define bgp_save_complex(A,B) __stfpd((double*)A,B)
+#define bgp_summassign_complex(A,B) A=__fpadd(A,B)
+#define bgp_subtassign_complex(A,B) A=__fpsub(A,B)
+#define bgp_summassign_icomplex(A,B) A=__fxcxnpma(A,B,1)
+#define bgp_subtassign_icomplex(A,B) A=__fxcxnsma(A,B,1)
+#define bgp_complex_put_to_zero(A) bgp_subtassign_complex(A,A)
+
+#define bgp_complex_prod_real(A,B,C) A=__fxpmul(B,C);
+#define bgp_complex_summ_the_prod_real(A,B,C,D)  A=__fxcpmadd(B,C,D);
+#define bgp_complex_summ_the_prod_ireal(A,B,C,D) A=__fxcxnpma(B,C,D);
+#define bgp_complex_subt_the_prod_real(A,B,C,D)  A=__fxcpnmsub(B,C,D);
+#define bgp_complex_subt_the_prod_ireal(A,B,C,D) A=__fxcxnsma(B,C,D);
+
+#define bgp_complex_prod(A,B,C)				\
+  bgp_complex_prod_real(A,B,__creal(C));		\
+  bgp_complex_summ_the_prod_ireal(A,A,B,__cimag(C));
+#define bgp_complex_conj2_prod(A,B,C)			\
+  bgp_complex_prod_real(A,B,__creal(C));		\
+  bgp_complex_subt_the_prod_ireal(A,A,B,__cimag(C));
+#define bgp_complex_conj1_prod(A,B,C) bgp_complex_conj2_prod(A,C,B);
+
+#define bgp_complex_summ_the_prod(A,B,C,D)		\
+  bgp_complex_summ_the_prod_real (A,B,C,__creal(D));	\
+  bgp_complex_summ_the_prod_ireal(A,B,C,__cimag(D));
+#define bgp_complex_summ_the_conj2_prod(A,B,C,D)	\
+  bgp_complex_summ_the_prod_real (A,B,C,__creal(D));	\
+  bgp_complex_subt_the_prod_ireal(A,B,C,__cimag(D));
+#define bgp_complex_summ_the_conj1_prod(A,B,C,D) bgp_complex_summ_the_conj2_prod(A,B,D,C);
+
+#define bgp_color_put_to_zero(A0,A1,A2)	  \
+  bgp_complex_put_to_zero(A0);		  \
+  bgp_complex_put_to_zero(A1);		  \
+  bgp_complex_put_to_zero(A2);
+
+#define bgp_assign_color_prod_real(A0,A1,A2,R)	  \
+  bgp_complex_prod_real(A0,A0,R);		  \
+  bgp_complex_prod_real(A1,A1,R);		  \
+  bgp_complex_prod_real(A2,A2,R);
+
+#define bgp_summassign_color_prod_real(A0,A1,A2,B0,B1,B2,R)	\
+  bgp_complex_summ_the_prod_real(A0,A0,B0,R);			\
+  bgp_complex_summ_the_prod_real(A1,A1,B1,R);			\
+  bgp_complex_summ_the_prod_real(A2,A2,B2,R);
+
+#define bgp_subtassign_color_prod_real(A0,A1,A2,B0,B1,B2,R)	\
+  bgp_complex_subt_the_prod_real(A0,A0,B0,R);			\
+  bgp_complex_subt_the_prod_real(A1,A1,B1,R);			\
+  bgp_complex_subt_the_prod_real(A2,A2,B2,R);
+
+#define bgp_summassign_color_prod_ireal(A0,A1,A2,B0,B1,B2,R)	\
+  bgp_complex_summ_the_prod_ireal(A0,A0,B0,R);			\
+  bgp_complex_summ_the_prod_ireal(A1,A1,B1,R);			\
+  bgp_complex_summ_the_prod_ireal(A2,A2,B2,R);
+
+#define bgp_load_color(A0,A1,A2,B)	  \
+  bgp_load_complex(A0,B[0]);		  \
+  bgp_load_complex(A1,B[1]);		  \
+  bgp_load_complex(A2,B[2]);
+
+#define bgp_save_color(A,B0,B1,B2)	  \
+  bgp_save_complex(A[0],B0);		  \
+  bgp_save_complex(A[1],B1);		  \
+  bgp_save_complex(A[2],B2);
+
+#define bgp_summassign_color(A0,A1,A2,B0,B1,B2)	\
+  bgp_summassign_complex(A0,B0);		\
+  bgp_summassign_complex(A1,B1);		\
+  bgp_summassign_complex(A2,B2);
+
+#define bgp_subtassign_color(A0,A1,A2,B0,B1,B2)	\
+  bgp_subtassign_complex(A0,B0);		\
+  bgp_subtassign_complex(A1,B1);		\
+  bgp_subtassign_complex(A2,B2);
+
+#define bgp_summassign_icolor(A0,A1,A2,B0,B1,B2)	\
+  bgp_summassign_icomplex(A0,B0);			\
+  bgp_summassign_icomplex(A1,B1);			\
+  bgp_summassign_icomplex(A2,B2);
+
+#define bgp_subtassign_icolor(A0,A1,A2,B0,B1,B2)	\
+  bgp_subtassign_icomplex(A0,B0);			\
+  bgp_subtassign_icomplex(A1,B1);			\
+  bgp_subtassign_icomplex(A2,B2);
+
+#define bgp_copy_color(A0,A1,A2,B0,B1,B2)	\
+  bgp_color_put_to_zero(A0,A1,A2);		\
+  bgp_summassign_color(A0,A1,A2,B0,B1,B2);
+  
+#define bgp_load_su3(U00,U01,U02,U10,U11,U12,U20,U21,U22,U)	\
+  bgp_load_color(U00,U01,U02,U[0]);				\
+  bgp_load_color(U10,U11,U12,U[1]);				\
+  bgp_load_color(U20,U21,U22,U[2]);
+
+#define bgp_su3_prod_color(O0,O1,O2,U00,U01,U02,U10,U11,U12,U20,U21,U22,I0,I1,I2) \
+  bgp_complex_prod_real(O0,U00,__creal(I0));				\
+  bgp_complex_prod_real(O1,U11,__creal(I1));				\
+  bgp_complex_prod_real(O2,U22,__creal(I2));				\
+									\
+  bgp_complex_summ_the_prod_ireal(O0,O0,U00,__cimag(I0));		\
+  bgp_complex_summ_the_prod_ireal(O1,O1,U11,__cimag(I1));		\
+  bgp_complex_summ_the_prod_ireal(O2,O2,U22,__cimag(I2));		\
+									\
+									\
+  bgp_complex_summ_the_prod_real (O0,O0,U01,__creal(I1));		\
+  bgp_complex_summ_the_prod_real (O1,O1,U12,__creal(I2));		\
+  bgp_complex_summ_the_prod_real (O2,O2,U20,__creal(I0));		\
+									\
+  bgp_complex_summ_the_prod_ireal(O0,O0,U01,__cimag(I1));		\
+  bgp_complex_summ_the_prod_ireal(O1,O1,U12,__cimag(I2));		\
+  bgp_complex_summ_the_prod_ireal(O2,O2,U20,__cimag(I0));		\
+									\
+									\
+  bgp_complex_summ_the_prod_real (O0,O0,U02,__creal(I2));		\
+  bgp_complex_summ_the_prod_real (O1,O1,U10,__creal(I0));		\
+  bgp_complex_summ_the_prod_real (O2,O2,U21,__creal(I1));		\
+									\
+  bgp_complex_summ_the_prod_ireal(O0,O0,U02,__cimag(I2));		\
+  bgp_complex_summ_the_prod_ireal(O1,O1,U10,__cimag(I0));		\
+  bgp_complex_summ_the_prod_ireal(O2,O2,U21,__cimag(I1));
+
+//this is here only for "pedagogical" use
+#define bgp_su3_prod_color_old(O0,O1,O2,U00,U01,U02,U10,U11,U12,U20,U21,U22,I0,I1,I2) \
+  bgp_complex_prod(O0,U00,I0);						\
+  bgp_complex_prod(O1,U11,I1);						\
+  bgp_complex_prod(O2,U22,I2);						\
+  bgp_complex_summ_the_prod(O0,O0,U01,I1);				\
+  bgp_complex_summ_the_prod(O1,O1,U12,I2);				\
+  bgp_complex_summ_the_prod(O2,O2,U20,I0);				\
+  bgp_complex_summ_the_prod(O0,O0,U02,I2);				\
+  bgp_complex_summ_the_prod(O1,O1,U10,I0);				\
+  bgp_complex_summ_the_prod(O2,O2,U21,I1);
+
+#define bgp_su3_dag_prod_color(O0,O1,O2,U00,U01,U02,U10,U11,U12,U20,U21,U22,I0,I1,I2) \
+  bgp_complex_prod_real(O0,I0,__creal(U00));				\
+  bgp_complex_prod_real(O1,I1,__creal(U11));				\
+  bgp_complex_prod_real(O2,I2,__creal(U22));				\
+									\
+  bgp_complex_subt_the_prod_ireal(O0,O0,I0,__cimag(U00));		\
+  bgp_complex_subt_the_prod_ireal(O1,O1,I1,__cimag(U11));		\
+  bgp_complex_subt_the_prod_ireal(O2,O2,I2,__cimag(U22));		\
+									\
+									\
+  bgp_complex_summ_the_prod_real(O0,O0,I1,__creal(U10));		\
+  bgp_complex_summ_the_prod_real(O1,O1,I2,__creal(U21));		\
+  bgp_complex_summ_the_prod_real(O2,O2,I0,__creal(U02));		\
+									\
+  bgp_complex_subt_the_prod_ireal(O0,O0,I1,__cimag(U10));		\
+  bgp_complex_subt_the_prod_ireal(O1,O1,I2,__cimag(U21));		\
+  bgp_complex_subt_the_prod_ireal(O2,O2,I0,__cimag(U02));		\
+									\
+									\
+  bgp_complex_summ_the_prod_real(O0,O0,I2,__creal(U20));		\
+  bgp_complex_summ_the_prod_real(O1,O1,I0,__creal(U01));		\
+  bgp_complex_summ_the_prod_real(O2,O2,I1,__creal(U12));		\
+									\
+  bgp_complex_subt_the_prod_ireal(O0,O0,I2,__cimag(U20));		\
+  bgp_complex_subt_the_prod_ireal(O1,O1,I0,__cimag(U01));		\
+  bgp_complex_subt_the_prod_ireal(O2,O2,I1,__cimag(U12));
+
+#define bgp_assign_minus_one_half_gamma5_prod_spincolor(A0,A1,A2,B0,B1,B2,C0,C1,C2,D0,D1,D2) \
+  bgp_assign_color_prod_real(A0,A1,A2,-0.5);				\
+  bgp_assign_color_prod_real(B0,B1,B2,-0.5);				\
+  bgp_assign_color_prod_real(C0,C1,C2,+0.5);				\
+  bgp_assign_color_prod_real(D0,D1,D2,+0.5);
+
+#define bgp_load_spincolor(A0,A1,A2,B0,B1,B2,C0,C1,C2,D0,D1,D2,s)	\
+  bgp_load_color(A0,A1,A2,s[0]);					\
+  bgp_load_color(B0,B1,B2,s[1]);					\
+  bgp_load_color(C0,C1,C2,s[2]);					\
+  bgp_load_color(D0,D1,D2,s[3]);
--- Appretto/src/dirac_operator_bgp.c
+++ Appretto/src/dirac_operator_bgp.c
@@ -1,114 +1,253 @@
 #pragma once
 
-#include "su3.c"
+#include "bgp_instructions.c"
+
+#define bgp_summassign_diagonal_term(R00,R01,R02,R10,R11,R12,R20,R21,R22,R30,R31,R32,A0,A1,A2,B0,B1,B2,C0,C1,C2,D0,D1,D2, mass)\
+  bgp_summassign_color_prod_real(R00,R01,R02,A0,A1,A2,__creal(mass));	\
+  bgp_summassign_color_prod_real(R10,R11,R12,B0,B1,B2,__creal(mass));	\
+  bgp_subtassign_color_prod_real(R20,R21,R22,C0,C1,C2,__creal(mass));	\
+  bgp_subtassign_color_prod_real(R30,R31,R32,D0,D1,D2,__creal(mass));	\
+  bgp_summassign_color_prod_ireal(R00,R01,R02,A0,A1,A2,__cimag(mass));	\
+  bgp_summassign_color_prod_ireal(R10,R11,R12,B0,B1,B2,__cimag(mass));	\
+  bgp_summassign_color_prod_ireal(R20,R21,R22,C0,C1,C2,__cimag(mass));	\
+  bgp_summassign_color_prod_ireal(R30,R31,R32,D0,D1,D2,__cimag(mass));
 
 void apply_Q(spincolor *out,spincolor *in,quad_su3 *conf,double kappac,double mu)
 {
-  double kcf=1/(2*kappac);
-  int Xup,Xdw;
+  static double _Complex A0,A1,A2;
+  static double _Complex B0,B1,B2;
 
+  static double _Complex C0,C1,C2;
+  static double _Complex D0,D1,D2;
+  static double _Complex E0,E1,E2;
+
+  static double _Complex T0,T1,T2;
+
+  static double _Complex R00,R01,R02;
+  static double _Complex R10,R11,R12;
+  static double _Complex R20,R21,R22;
+  static double _Complex R30,R31,R32;
+
+  static double _Complex OH,mass;
+
+  //complex cpuOH bgp_aligned ={0.5,0};
+  complex cpumass bgp_aligned ={1/(2*kappac),mu};
+  //bgp_load_complex(OH,cpuOH);
+  bgp_load_complex(mass,cpumass);
+  
+  /*
+  static double _Complex A,B,C;
+  complex test1={1,2};
+  complex test2={2,3};
+  complex test3={0,0};
+
+  bgp_load_complex(A,test1);
+  bgp_load_complex(B,test2);
+  bgp_complex_prod(C,A,B);
+  bgp_save_complex(test3,C);
+  if(rank==0) printf("ecco: %g %g\n",test3[0],test3[1]);
+
+  
+  color temp_c0,temp_c1;
+  unsafe_su3_prod_color(temp_c0,conf[0][0],in[0][0]);
+  bgp_load_color(A0,A1,A2,in[0][0]);
+  bgp_load_su3(B0,B1,B2,C0,C1,C2,D0,D1,D2,conf[0][0]);
+  bgp_su3_prod_color(E0,E1,E2,B0,B1,B2,C0,C1,C2,D0,D1,D2,A0,A1,A2);
+  bgp_save_color(temp_c1,E0,E1,E2);
+  if(rank==0) printf("%g %g\n",temp_c1[0][0],temp_c0[0][0]);
+  */
+  //complex test3 __attribute__ ((aligned (16)));
+  int Xup,Xdw;
   for(int X=0;X<loc_vol;X++)
   {
     color temp_c0,temp_c1,temp_c2,temp_c3;
-
+    
     //Forward 0
     Xup=loclx_neighup[X][0];
-    color_summ(temp_c0,in[Xup][0],in[Xup][2]);
-    color_summ(temp_c1,in[Xup][1],in[Xup][3]);
-    unsafe_su3_prod_color(out[X][0],conf[X][0],temp_c0);
-    unsafe_su3_prod_color(out[X][1],conf[X][0],temp_c1);
-    color_copy(out[X][2],out[X][0]);
-    color_copy(out[X][3],out[X][1]);
-	
+
+    bgp_cache_touch_su3(conf[X][0]);
+
+    bgp_load_color(A0,A1,A2,in[Xup][0]);
+    bgp_load_color(T0,T1,T2,in[Xup][2]);
+    bgp_summassign_color(A0,A1,A2,T0,T1,T2);
+
+    bgp_load_color(B0,B1,B2,in[Xup][1]);
+    bgp_load_color(T0,T1,T2,in[Xup][3]);
+    bgp_summassign_color(B0,B1,B2,T0,T1,T2);
+
+    bgp_load_su3(C0,C1,C2,D0,D1,D2,E0,E1,E2,conf[X][0]);
+
+    bgp_su3_prod_color(R00,R01,R02,C0,C1,C2,D0,D1,D2,E0,E1,E2,A0,A1,A2);
+    bgp_su3_prod_color(R10,R11,R12,C0,C1,C2,D0,D1,D2,E0,E1,E2,B0,B1,B2);
+    bgp_copy_color(R20,R21,R22,R00,R01,R02);
+    bgp_copy_color(R30,R31,R32,R10,R11,R12);
+    
     //Backward 0
     Xdw=loclx_neighdw[X][0];
-    color_subt(temp_c0,in[Xdw][0],in[Xdw][2]);
-    color_subt(temp_c1,in[Xdw][1],in[Xdw][3]);
-    unsafe_su3_dag_prod_color(temp_c2,conf[Xdw][0],temp_c0);
-    unsafe_su3_dag_prod_color(temp_c3,conf[Xdw][0],temp_c1);
-    summassign_color(out[X][0],temp_c2);
-    summassign_color(out[X][1],temp_c3);
-    subtassign_color(out[X][2],temp_c2);
-    subtassign_color(out[X][3],temp_c3);
+
+    bgp_cache_touch_su3(conf[Xdw][0]);
+
+    bgp_load_color(A0,A1,A2,in[Xdw][0]);
+    bgp_load_color(T0,T1,T2,in[Xdw][2]);
+    bgp_subtassign_color(A0,A1,A2,T0,T1,T2);
+
+    bgp_load_color(B0,B1,B2,in[Xdw][1]);
+    bgp_load_color(T0,T1,T2,in[Xdw][3]);
+    bgp_subtassign_color(B0,B1,B2,T0,T1,T2);
+
+    bgp_load_su3(C0,C1,C2,D0,D1,D2,E0,E1,E2,conf[Xdw][0]);
+
+    bgp_su3_dag_prod_color(T0,T1,T2,C0,C1,C2,D0,D1,D2,E0,E1,E2,A0,A1,A2);
+    bgp_su3_dag_prod_color(A0,A1,A2,C0,C1,C2,D0,D1,D2,E0,E1,E2,B0,B1,B2);
+    bgp_summassign_color(R00,R01,R02,T0,T1,T2);
+    bgp_summassign_color(R10,R11,R12,A0,A1,A2);
+    bgp_subtassign_color(R20,R21,R22,T0,T1,T2);
+    bgp_subtassign_color(R30,R31,R32,A0,A1,A2);
 
     //Forward 1
     Xup=loclx_neighup[X][1];
-    color_isumm(temp_c0,in[Xup][0],in[Xup][3]);
-    color_isumm(temp_c1,in[Xup][1],in[Xup][2]);
-    unsafe_su3_prod_color(temp_c2,conf[X][1],temp_c0);
-    unsafe_su3_prod_color(temp_c3,conf[X][1],temp_c1);
-    summassign_color(out[X][0],temp_c2);
-    summassign_color(out[X][1],temp_c3);
-    subtassign_icolor(out[X][2],temp_c3);
-    subtassign_icolor(out[X][3],temp_c2);
+
+    bgp_cache_touch_su3(conf[X][1]);
+
+    bgp_load_color(A0,A1,A2,in[Xup][0]);
+    bgp_load_color(T0,T1,T2,in[Xup][3]);
+    bgp_summassign_icolor(A0,A1,A2,T0,T1,T2);
+
+    bgp_load_color(B0,B1,B2,in[Xup][1]);
+    bgp_load_color(T0,T1,T2,in[Xup][2]);
+    bgp_summassign_icolor(B0,B1,B2,T0,T1,T2);
+
+    bgp_load_su3(C0,C1,C2,D0,D1,D2,E0,E1,E2,conf[X][1]);
+
+    bgp_su3_prod_color(T0,T1,T2,C0,C1,C2,D0,D1,D2,E0,E1,E2,A0,A1,A2);
+    bgp_su3_prod_color(A0,A1,A2,C0,C1,C2,D0,D1,D2,E0,E1,E2,B0,B1,B2);
+    bgp_summassign_color(R00,R01,R02,T0,T1,T2);
+    bgp_summassign_color(R10,R11,R12,A0,A1,A2);
+    bgp_subtassign_icolor(R20,R21,R22,A0,A1,A2);
+    bgp_subtassign_icolor(R30,R31,R32,T0,T1,T2);
 	
     //Backward 1
     Xdw=loclx_neighdw[X][1];
-    color_isubt(temp_c0,in[Xdw][0],in[Xdw][3]);
-    color_isubt(temp_c1,in[Xdw][1],in[Xdw][2]);
-    unsafe_su3_dag_prod_color(temp_c2,conf[Xdw][1],temp_c0);
-    unsafe_su3_dag_prod_color(temp_c3,conf[Xdw][1],temp_c1);
-    summassign_color(out[X][0],temp_c2);
-    summassign_color(out[X][1],temp_c3);
-    summassign_icolor(out[X][2],temp_c3);
-    summassign_icolor(out[X][3],temp_c2);
+
+    bgp_cache_touch_su3(conf[Xdw][1]);
+
+    bgp_load_color(A0,A1,A2,in[Xdw][0]);
+    bgp_load_color(T0,T1,T2,in[Xdw][3]);
+    bgp_subtassign_icolor(A0,A1,A2,T0,T1,T2);
+
+    bgp_load_color(B0,B1,B2,in[Xdw][1]);
+    bgp_load_color(T0,T1,T2,in[Xdw][2]);
+    bgp_subtassign_icolor(B0,B1,B2,T0,T1,T2);
+
+    bgp_load_su3(C0,C1,C2,D0,D1,D2,E0,E1,E2,conf[Xdw][1]);
+
+    bgp_su3_dag_prod_color(T0,T1,T2,C0,C1,C2,D0,D1,D2,E0,E1,E2,A0,A1,A2);
+    bgp_su3_dag_prod_color(A0,A1,A2,C0,C1,C2,D0,D1,D2,E0,E1,E2,B0,B1,B2);
+    bgp_summassign_color(R00,R01,R02,T0,T1,T2);
+    bgp_summassign_color(R10,R11,R12,A0,A1,A2);
+    bgp_summassign_icolor(R20,R21,R22,A0,A1,A2);
+    bgp_summassign_icolor(R30,R31,R32,T0,T1,T2);
     
     //Forward 2
     Xup=loclx_neighup[X][2];
-    color_summ(temp_c0,in[Xup][0],in[Xup][3]);
-    color_subt(temp_c1,in[Xup][1],in[Xup][2]);
-    unsafe_su3_prod_color(temp_c2,conf[X][2],temp_c0);
-    unsafe_su3_prod_color(temp_c3,conf[X][2],temp_c1);
-    summassign_color(out[X][0],temp_c2);
-    summassign_color(out[X][1],temp_c3);
-    subtassign_color(out[X][2],temp_c3);
-    summassign_color(out[X][3],temp_c2);
+
+    bgp_cache_touch_su3(conf[X][2]);
+
+    bgp_load_color(A0,A1,A2,in[Xup][0]);
+    bgp_load_color(T0,T1,T2,in[Xup][3]);
+    bgp_summassign_color(A0,A1,A2,T0,T1,T2);
+
+    bgp_load_color(B0,B1,B2,in[Xup][1]);
+    bgp_load_color(T0,T1,T2,in[Xup][2]);
+    bgp_subtassign_color(B0,B1,B2,T0,T1,T2);
+
+    bgp_load_su3(C0,C1,C2,D0,D1,D2,E0,E1,E2,conf[X][2]);
+
+    bgp_su3_prod_color(T0,T1,T2,C0,C1,C2,D0,D1,D2,E0,E1,E2,A0,A1,A2);
+    bgp_su3_prod_color(A0,A1,A2,C0,C1,C2,D0,D1,D2,E0,E1,E2,B0,B1,B2);
+    bgp_summassign_color(R00,R01,R02,T0,T1,T2);
+    bgp_summassign_color(R10,R11,R12,A0,A1,A2);
+    bgp_subtassign_color(R20,R21,R22,A0,A1,A2);
+    bgp_summassign_color(R30,R31,R32,T0,T1,T2);
 	
     //Backward 2
     Xdw=loclx_neighdw[X][2];
-    color_subt(temp_c0,in[Xdw][0],in[Xdw][3]);
-    color_summ(temp_c1,in[Xdw][1],in[Xdw][2]);
-    unsafe_su3_dag_prod_color(temp_c2,conf[Xdw][2],temp_c0);
-    unsafe_su3_dag_prod_color(temp_c3,conf[Xdw][2],temp_c1);
-    summassign_color(out[X][0],temp_c2);
-    summassign_color(out[X][1],temp_c3);
-    summassign_color(out[X][2],temp_c3);
-    subtassign_color(out[X][3],temp_c2);
+
+    bgp_cache_touch_su3(conf[Xdw][2]);
+
+    bgp_load_color(A0,A1,A2,in[Xdw][0]);
+    bgp_load_color(T0,T1,T2,in[Xdw][3]);
+    bgp_subtassign_color(A0,A1,A2,T0,T1,T2);
+
+    bgp_load_color(B0,B1,B2,in[Xdw][1]);
+    bgp_load_color(T0,T1,T2,in[Xdw][2]);
+    bgp_summassign_color(B0,B1,B2,T0,T1,T2);
+
+    bgp_load_su3(C0,C1,C2,D0,D1,D2,E0,E1,E2,conf[Xdw][2]);
+
+    bgp_su3_dag_prod_color(T0,T1,T2,C0,C1,C2,D0,D1,D2,E0,E1,E2,A0,A1,A2);
+    bgp_su3_dag_prod_color(A0,A1,A2,C0,C1,C2,D0,D1,D2,E0,E1,E2,B0,B1,B2);
+    bgp_summassign_color(R00,R01,R02,T0,T1,T2);
+    bgp_summassign_color(R10,R11,R12,A0,A1,A2);
+    bgp_summassign_color(R20,R21,R22,A0,A1,A2);
+    bgp_subtassign_color(R30,R31,R32,T0,T1,T2);
     
     //Forward 3
     Xup=loclx_neighup[X][3];
-    color_isumm(temp_c0,in[Xup][0],in[Xup][2]);
-    color_isubt(temp_c1,in[Xup][1],in[Xup][3]);
-    unsafe_su3_prod_color(temp_c2,conf[X][3],temp_c0);
-    unsafe_su3_prod_color(temp_c3,conf[X][3],temp_c1);
-    summassign_color(out[X][0],temp_c2);
-    summassign_color(out[X][1],temp_c3);
-    subtassign_icolor(out[X][2],temp_c2);
-    summassign_icolor(out[X][3],temp_c3);
+
+    bgp_cache_touch_su3(conf[X][3]);
+
+    bgp_load_color(A0,A1,A2,in[Xup][0]);
+    bgp_load_color(T0,T1,T2,in[Xup][2]);
+    bgp_summassign_icolor(A0,A1,A2,T0,T1,T2);
+
+    bgp_load_color(B0,B1,B2,in[Xup][1]);
+    bgp_load_color(T0,T1,T2,in[Xup][3]);
+    bgp_subtassign_icolor(B0,B1,B2,T0,T1,T2);
+
+    bgp_load_su3(C0,C1,C2,D0,D1,D2,E0,E1,E2,conf[X][3]);
+
+    bgp_su3_prod_color(T0,T1,T2,C0,C1,C2,D0,D1,D2,E0,E1,E2,A0,A1,A2);
+    bgp_su3_prod_color(A0,A1,A2,C0,C1,C2,D0,D1,D2,E0,E1,E2,B0,B1,B2);
+    bgp_summassign_color(R00,R01,R02,T0,T1,T2);
+    bgp_summassign_color(R10,R11,R12,A0,A1,A2);
+    bgp_subtassign_icolor(R20,R21,R22,T0,T1,T2);
+    bgp_summassign_icolor(R30,R31,R32,A0,A1,A2);
 	
+    bgp_save_color(out[X][0],R00,R01,R02);
+    bgp_save_color(out[X][1],R10,R11,R12);
+    bgp_save_color(out[X][2],R20,R21,R22);
+    bgp_save_color(out[X][3],R30,R31,R32);
+
     //Backward 3
     Xdw=loclx_neighdw[X][3];
-    color_isubt(temp_c0,in[Xdw][0],in[Xdw][2]);
-    color_isumm(temp_c1,in[Xdw][1],in[Xdw][3]);
-    unsafe_su3_dag_prod_color(temp_c2,conf[Xdw][3],temp_c0);
-    unsafe_su3_dag_prod_color(temp_c3,conf[Xdw][3],temp_c1);
-    summassign_color(out[X][0],temp_c2);
-    summassign_color(out[X][1],temp_c3);
-    summassign_icolor(out[X][2],temp_c2);
-    subtassign_icolor(out[X][3],temp_c3);
-    
-    //Put the -1/2 factor on derivative, the gamma5, and the imu
-    //ok this is horrible, but fast
-    for(int c=0;c<3;c++)
-      {
-	out[X][0][c][0]=-0.5*out[X][0][c][0]+kcf*in[X][0][c][0]-mu*in[X][0][c][1];
-	out[X][0][c][1]=-0.5*out[X][0][c][1]+kcf*in[X][0][c][1]+mu*in[X][0][c][0];
-	out[X][1][c][0]=-0.5*out[X][1][c][0]+kcf*in[X][1][c][0]-mu*in[X][1][c][1];
-	out[X][1][c][1]=-0.5*out[X][1][c][1]+kcf*in[X][1][c][1]+mu*in[X][1][c][0];
-	out[X][2][c][0]=+0.5*out[X][2][c][0]-kcf*in[X][2][c][0]-mu*in[X][2][c][1];
-	out[X][2][c][1]=+0.5*out[X][2][c][1]-kcf*in[X][2][c][1]+mu*in[X][2][c][0];
-	out[X][3][c][0]=+0.5*out[X][3][c][0]-kcf*in[X][3][c][0]-mu*in[X][3][c][1];
-	out[X][3][c][1]=+0.5*out[X][3][c][1]-kcf*in[X][3][c][1]+mu*in[X][3][c][0];
-      }
+
+    bgp_cache_touch_su3(conf[Xdw][3]);
+
+    bgp_load_color(A0,A1,A2,in[Xdw][0]);
+    bgp_load_color(T0,T1,T2,in[Xdw][2]);
+    bgp_subtassign_icolor(A0,A1,A2,T0,T1,T2);
+
+    bgp_load_color(B0,B1,B2,in[Xdw][1]);
+    bgp_load_color(T0,T1,T2,in[Xdw][3]);
+    bgp_summassign_icolor(B0,B1,B2,T0,T1,T2);
+
+    bgp_load_su3(C0,C1,C2,D0,D1,D2,E0,E1,E2,conf[Xdw][3]);
+
+    bgp_su3_dag_prod_color(T0,T1,T2,C0,C1,C2,D0,D1,D2,E0,E1,E2,A0,A1,A2);
+    bgp_su3_dag_prod_color(A0,A1,A2,C0,C1,C2,D0,D1,D2,E0,E1,E2,B0,B1,B2);
+    bgp_summassign_color(R00,R01,R02,T0,T1,T2);
+    bgp_summassign_color(R10,R11,R12,A0,A1,A2);
+    bgp_summassign_icolor(R20,R21,R22,T0,T1,T2);
+    bgp_subtassign_icolor(R30,R31,R32,A0,A1,A2);
+	
+    bgp_assign_minus_one_half_gamma5_prod_spincolor(R00,R01,R02,R10,R11,R12,R20,R21,R22,R30,R31,R32);
+    bgp_load_spincolor(A0,A1,A2,B0,B1,B2,C0,C1,C2,D0,D1,D2,in[X]);
+    bgp_summassign_diagonal_term(R00,R01,R02,R10,R11,R12,R20,R21,R22,R30,R31,R32, A0,A1,A2,B0,B1,B2,C0,C1,C2,D0,D1,D2, mass);
+
+    bgp_save_color(out[X][0],R00,R01,R02);
+    bgp_save_color(out[X][1],R10,R11,R12);
+    bgp_save_color(out[X][2],R20,R21,R22);
+    bgp_save_color(out[X][3],R30,R31,R32);
   }
 }
