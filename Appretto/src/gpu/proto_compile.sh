0c22a3b0978871df0594721238c0fc179039ba68
fr.sanfilippo <fr.sanfilippo@35dfebc6-1a16-7261-d8b2-d758fa207421>
2011-07-01 14:31:09 +0000

    resolved conflict

--- Appretto/src/gpu/proto_compile.sh
+++ Appretto/src/gpu/proto_compile.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+<<<<<<< .mine
+comp="nvcc gpu.cu -L/usr/lib/openmpi/lib -lmpi -I/usr/lib/openmpi/include -I/home/francesco/Prace/nissa/Appretto/src/gpu/ -ccbin /home/francesco/Prace/nissa/Appretto/gcc"
+=======
 comp="nvcc gpu.cu -I /usr/lib64/openmpi/1.4-gcc/include -L /usr/lib64/openmpi/1.4-gcc/lib/ -lmpi -arch sm_13"
+>>>>>>> .r335
 
 old_sha=$(cat gpu.sha 2>/dev/null)
 new_sha=$($comp -E|sha1sum|tee gpu.sha)
