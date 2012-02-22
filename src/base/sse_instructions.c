3c3cb5ce8961d5c4a8458c51e37abb681b719cf0
fr.sanfilippo@gmail.com <fr.sanfilippo@gmail.com@35dfebc6-1a16-7261-d8b2-d758fa207421>
2012-02-22 11:18:38 +0000

    going on with sse

--- src/base/sse_instructions.c
+++ src/base/sse_instructions.c
@@ -19,20 +19,26 @@
 			:						\
 			"xmm" a, "xmm" b, "xmm" c)
 
-#define sse_save_color(d, a,b,c)  \
- __asm__ __volatile__ ("movapd %%xmm" a ", %0 \n\t" \
-                       "movapd %%xmm" b ", %1 \n\t" \
-                       "movapd %%xmm" c ", %2" \
-                       : \
-                       "=m" (d[0][0]), \
-                       "=m" (d[1][0]), \
-                       "=m" (d[2][0]))
+#define sse_save_color(d, a,b,c)		    \
+  __asm__ __volatile__ ("movapd %%xmm" a ", %0 \n\t"	\
+			"movapd %%xmm" b ", %1 \n\t"	\
+			"movapd %%xmm" c ", %2"		\
+			:				\
+							"=m" (d[0][0]), \
+							"=m" (d[1][0]), \
+			"=m" (d[2][0]))
 
-#define sse_color_summassign(a,b,c, d,e,f) \
-__asm__ __volatile__ ("addpd %%xmm" d ", %%xmm" a " \n\t" \
-                      "addpd %%xmm" e ", %%xmm" b " \n\t" \
-                      "addpd %%xmm" f ", %%xmm" c \
-                      : \
-                      : \
-                      : \
-                      "xmm" a, "xmm" b, "xmm" c)
+#define sse_color_summassign(a,b,c, d,e,f)		  \
+  __asm__ __volatile__ ("addpd %%xmm" d ", %%xmm" a " \n\t"	\
+			"addpd %%xmm" e ", %%xmm" b " \n\t"	\
+			"addpd %%xmm" f ", %%xmm" c		\
+			:					\
+			:					\
+			:					\
+			"xmm" a, "xmm" b, "xmm" c)
+
+#define sse_load_colors_and_summ(a,b,c, d,e,f, G, H)	\
+  sse_load_color(a,b,c, G);				\
+  sse_load_color(d,e,f, H);				\
+  sse_color_summassign(a,b,c, d,e,f);
+
