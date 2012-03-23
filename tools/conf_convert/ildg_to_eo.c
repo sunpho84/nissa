d23fe46207fa9bd66204953a774cfbd06cf79681
fr.sanfilippo <fr.sanfilippo@35dfebc6-1a16-7261-d8b2-d758fa207421>
2012-03-23 12:52:06 +0000

    fixed the ildg to eo converter

--- tools/conf_convert/ildg_to_eo.c
+++ tools/conf_convert/ildg_to_eo.c
@@ -5,18 +5,10 @@ int main(int narg,char **arg)
   //basic mpi initialization
   init_nissa();
   
-  if(rank_tot>1)
-    {
-      fprintf(stderr,"Cannot run in parallel\n");
-      MPI_Abort(MPI_COMM_WORLD,1);
-    }
+  if(rank_tot>1) crash("Cannot run in parallel");
   
-  if(narg<5 && rank==0)
-    {
-      fprintf(stderr,"Use: %s L T file_in file_out\n",arg[0]);
-      MPI_Abort(MPI_COMM_WORLD,1);
-    }
-
+  if(narg<5) crash("Use: %s L T file_in file_out",arg[0]);
+  
   int L=atoi(arg[1]);
   int T=atoi(arg[2]);
 
@@ -25,16 +17,17 @@ int main(int narg,char **arg)
 
   ///////////////////////////////////////////
 
-  quad_su3 *conf=allocate_quad_su3(loc_vol,"conf");
+  quad_su3 *conf=nissa_malloc("conf",loc_vol,quad_su3);
   read_ildg_gauge_conf(conf,arg[3]);  
   
   FILE *fout=fopen(arg[4],"wb");
-
+  
   fwrite(glb_size,sizeof(int),4,fout);
-
-  double plaq=global_plaquette_lx_conf(conf);
+  
+  double plaq=global_plaquette_lx_conf(conf)*3;
   fwrite(&plaq,sizeof(double),1,fout);
   
+  int remap[4]={0,3,2,1};
   for(size_t t=0;t<T;t++)
     for(size_t z=0;z<L;z++)
       for(size_t y=0;y<L;y++)
@@ -44,19 +37,19 @@ int main(int narg,char **arg)
 	      int iw=loclx_of_coord_list(t,x,y,z);
 	      for(int mu=0;mu<4;mu++)
 		{
-		  int iz=loclx_neighdw[iw][mu];
-		  fwrite(conf[iw][mu],sizeof(quad_su3),1,fout);
-		  fwrite(conf[iz][mu],sizeof(quad_su3),1,fout);
+		  int iz=loclx_neighdw[iw][remap[mu]];
+		  fwrite(conf[iw][remap[mu]],sizeof(su3),1,fout);
+		  fwrite(conf[iz][remap[mu]],sizeof(su3),1,fout);
 		}
 	    }
   
   fclose(fout);
   
-  free(conf);
+  nissa_free(conf);
   
   ///////////////////////////////////////////
-
+  
   close_nissa();
-
+  
   return 0;
 }
