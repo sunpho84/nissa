47241b0d7be1dcf989ee858d16aa72239a84e73a
fr.sanfilippo@gmail.com <fr.sanfilippo@gmail.com@35dfebc6-1a16-7261-d8b2-d758fa207421>
2012-04-04 17:42:39 +0000

    moved the stag program to the projects

---
+++ src/hmc/hmc.c
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "backfield.c"
+
+#include "momenta/momenta_generation.c"
+#include "momenta/momenta_action.c"
+
+#include "gauge/wilson_force.c"
+
+#include "rootst_eoimpr/rat_expansion_database.c"
+#include "rootst_eoimpr/rootst_eoimpr_eigenvalues.c"
+#include "rootst_eoimpr/rootst_eoimpr_force.c"
+#include "rootst_eoimpr/rootst_eoimpr_omelyan_integrator.c"
+#include "rootst_eoimpr/rootst_eoimpr_pseudofermions_generation.c"
+#include "rootst_eoimpr/rootst_eoimpr_action.c"
+
