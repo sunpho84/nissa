7fb652956a6607542b23c311e45d47684fd20559
fr.sanfilippo@gmail.com <fr.sanfilippo@gmail.com@35dfebc6-1a16-7261-d8b2-d758fa207421>
2012-04-07 19:46:41 +0000

    fixed od border communications call in Deo

--- src/dirac_operators/dirac_operator_stD/dirac_operator_stD_bgp.c
+++ src/dirac_operators/dirac_operator_stD/dirac_operator_stD_bgp.c
@@ -27,7 +27,7 @@ void apply_st2Doe(color *out,quad_su3 **conf,color *in)
 	  bgp_summ_the_su3_prod_color(R0,R1,R2, C0,C1,C2,D0,D1,D2,E0,E1,E2, A0,A1,A2);
 	  
 	  bgp_load_color(A0,A1,A2, in[dw]);
-	  bgp_load_su3(C0,C1,C2,D0,D1,D2,E0,E1,E2, conf[!ODD][dw][mu]);
+	  bgp_load_su3(C0,C1,C2,D0,D1,D2,E0,E1,E2, conf[EVN][dw][mu]);
 	  bgp_subt_the_su3_dag_prod_color(R0,R1,R2, C0,C1,C2,D0,D1,D2,E0,E1,E2, A0,A1,A2);
 	}
       
@@ -57,7 +57,7 @@ void apply_st2Deo(color *out,quad_su3 **conf,color *in)
   bgp_complex R0,R1,R2;
   
   communicate_eo_quad_su3_borders(conf);
-  communicate_ev_color_borders(in);
+  communicate_od_color_borders(in);
     
   nissa_loc_volh_loop(isink)
     {
@@ -109,7 +109,6 @@ inline void apply_st2Deo(color *out,quad_su3 **conf,color *in)
   set_borders_invalid(out);
 }
 */
-
 void apply_stD2ee(color *out,quad_su3 **conf,color *temp,double mass,color *in)
 {
   communicate_eo_quad_su3_borders(conf);
