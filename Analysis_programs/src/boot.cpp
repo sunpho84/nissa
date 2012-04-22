d4cdb8a9bc0764f2241d0e9590e6cb26109a4dc9
fr.sanfilippo <fr.sanfilippo@gmail.com>
2012-04-22 20:20:08 +0000

    Removd "anlaysis_programs"

--- Analysis_programs/src/boot.cpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#define BOOT
-#define TYPE boot
-#define N nboot
-#include "bootjack.cpp"
-#undef N
-#undef TYPE
-#undef BOOT
--- Analysis_programs/src/jack.cpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#define JACK
-#define TYPE jack
-#define N njack
-#include "bootjack.cpp"
-#undef N
-#undef TYPE
-#undef JACK
