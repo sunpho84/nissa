7e80f7ec6ce4584895174a7ed3a09a7b477261e2
fr.sanfilippo@gmail.com <fr.sanfilippo@gmail.com@35dfebc6-1a16-7261-d8b2-d758fa207421>
2012-04-25 16:24:41 +0000

    added some missing .h, removed some useless file

---
+++ src/base/vectors.h
@@ -0,0 +1,32 @@
+#ifndef _VECTORS_H
+#define _VECTORS_H
+
+char *get_vec_name(void *v);
+int check_borders_allocated(void *data);
+int check_borders_communicated_at_least_once(void *data);
+int check_borders_valid(void *data);
+int check_edges_allocated(void *data);
+int check_edges_valid(void *data);
+int compute_nissa_vect_memory_usage();
+int get_vec_flag(void *v,int flag);
+nissa_vect* get_nissa_vec(void *v);
+void *internal_nissa_malloc(const char *tag,int nel,int size_per_el,const char *type,const char *file,int line);
+void crash_if_borders_not_allocated(void *v);
+void crash_if_edges_not_allocated(void *v);
+void ignore_borders_communications_warning(void *data);
+void initialize_main_nissa_vect();
+void internal_nissa_free(char **arr,const char *file,int line);
+void last_nissa_vect_content_printf();
+void nissa_vect_content_fprintf(FILE *fout,nissa_vect *vect);
+void nissa_vect_content_printf(nissa_vect *vect);
+void print_all_nissa_vect_content();
+void set_borders_invalid(void *data);
+void set_borders_valid(void *data);
+void set_edges_invalid(void *data);
+void set_edges_valid(void *data);
+void set_vec_flag(void *v,int flag);
+void unset_vec_flag(void *v,int flag);
+void vec_content_fprintf(FILE *f,void *vec);
+void vec_content_printf(void *vec);
+
+#endif
---
+++ src/new_types/spin.h
@@ -0,0 +1,9 @@
+void as2t_saturate(complex out,as2t a,as2t b);
+void get_spincolor_from_colorspinspin(spincolor out,colorspinspin in,int id_source);
+void get_spincolor_from_su3spinspin(spincolor out,su3spinspin in,int id_source,int ic_source);
+void print_spinspin(spinspin s);
+void put_spincolor_into_colorspinspin(colorspinspin out,spincolor in,int id_source);
+void put_spincolor_into_su3spinspin(su3spinspin out,spincolor in,int id_source,int ic_source);
+void spinspin_spinspindag_prod(spinspin out,spinspin a,spinspin b);
+void summ_the_trace_prod_dirac_spinspin(complex c,dirac_matr *a,spinspin b);
+void trace_prod_dirac_spinspin(complex c,dirac_matr *a,spinspin b);
