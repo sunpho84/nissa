4d8b7dba61c131706beafbed309a8f07a6efc4e3
fr.sanfilippo@gmail.com <fr.sanfilippo@gmail.com@35dfebc6-1a16-7261-d8b2-d758fa207421>
2012-07-05 13:08:00 +0000

    added the writer, now going to check

--- src/IO/writer.h
+++ src/IO/writer.h
@@ -1,16 +1,10 @@
-#ifndef _WRITER_H
-#define _WRITER_H
-
+#ifndef _writer_h
+#define _writer_h
 #include <mpi.h>
-
-void paste_eo_parts_and_write_ildg_gauge_conf(char *path,quad_su3 **eo_conf);
-void write_checksum(LemonWriter *writer,checksum check);
+void paste_eo_parts_and_write_ildg_gauge_conf(char *path,quad_su3 **eo_conf,int prec);
 void write_color(char *path,color *v,int prec);
-void write_double_vector(LemonWriter *writer,char *data,const char *header_message,int nreals_per_site,int nbits);
-void write_header(LemonWriter *writer,const char *header,uint64_t record_bytes);
-void write_ildg_gauge_conf(char *path,quad_su3 *in);
+void write_double_vector(ILDG_File &file,double *data,int nreals_per_site,int nbits,const char *header_message);
+void write_ildg_gauge_conf(char *path,quad_su3 *in,int prec);
 void write_spincolor(char *path,spincolor *spinor,int prec);
 void write_su3spinspin(char *path,su3spinspin *prop,int prec);
-void write_text_record(LemonWriter *writer,const char *header,const char *message);
-
 #endif
