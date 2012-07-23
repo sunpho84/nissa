02250805a0319458e7ae09ef201573f68dd9a2b1
fr.sanfilippo <fr.sanfilippo@gmail.com>
2012-07-23 14:01:46 +0000

    added bgp version, still to be checked

--- src/dirac_operators/dirac_operator_tmQ/dirac_operator_tmQ_bgp.cpp
+++ src/dirac_operators/dirac_operator_tmQ/dirac_operator_tmQ_bgp.cpp
@@ -14,33 +14,32 @@
 
 void apply_tmQ(spincolor *out,quad_su3 *conf,double kappa,double mu,spincolor *in)
 {
-  int Xup,Xdw;
-  
-  bgp_complex A0,A1,A2;
-  bgp_complex B0,B1,B2;
-  
-  bgp_complex C0,C1,C2;
-  bgp_complex D0,D1,D2;
-  bgp_complex E0,E1,E2;
-  
-  bgp_complex T0,T1,T2;
-  
-  bgp_complex R00,R01,R02;
-  bgp_complex R10,R11,R12;
-  bgp_complex R20,R21,R22;
-  bgp_complex R30,R31,R32;
-  
-  bgp_complex mass;
-  
   complex cpumass={1/(2*kappa),mu};
-  
-  bgp_complex_load(mass,cpumass);
   
   communicate_lx_spincolor_borders(in);
   communicate_lx_quad_su3_borders(conf);
   
-  nissa_loc_vol_loop(X)
+  nissa_loc_vol_parallel_loop(X)
     {
+      int Xup,Xdw;
+      
+      bgp_complex A0,A1,A2;
+      bgp_complex B0,B1,B2;
+      
+      bgp_complex C0,C1,C2;
+      bgp_complex D0,D1,D2;
+      bgp_complex E0,E1,E2;
+      
+      bgp_complex T0,T1,T2;
+      
+      bgp_complex R00,R01,R02;
+      bgp_complex R10,R11,R12;
+      bgp_complex R20,R21,R22;
+      bgp_complex R30,R31,R32;
+      
+      bgp_complex mass;
+      bgp_complex_load(mass,cpumass);
+      
       //Forward 0
       Xup=loclx_neighup[X][0];
       
