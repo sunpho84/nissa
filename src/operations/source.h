70310088db39ba7b848f1ffc25145ba96e61d61e
fr.sanfilippo <fr.sanfilippo@gmail.com>
2012-10-30 00:02:30 +0000

    added missing file and include

---
+++ src/operations/source.h
@@ -0,0 +1,6 @@
+#include "../new_types/new_types_definitions.h"
+
+#ifndef _SOURCE_H
+#define _SOURCE_H
+void select_propagator_timeslice(colorspinspin *prop_out,colorspinspin *prop_in,int timeslice);
+#endif
