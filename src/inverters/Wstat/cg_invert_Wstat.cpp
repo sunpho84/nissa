9808ed8b5594239488c09f1da5bc6e05a27bd9d3
fr.sanfilippo@gmail.com <fr.sanfilippo@gmail.com@35dfebc6-1a16-7261-d8b2-d758fa207421>
2012-11-09 09:46:18 +0000

    added Wstat inverter (why?)

---
+++ src/inverters/Wstat/cg_invert_Wstat.cpp
@@ -0,0 +1,28 @@
+#include <math.h>
+
+#include "../../new_types/new_types_definitions.h"
+#include "../../linalgs/linalgs.h"
+#include "../../base/global_variables.h"
+#include "../../base/vectors.h"
+#include "../../base/routines.h"
+#include "../../base/debug.h"
+#include "../../dirac_operators/dirac_operator_Wstat/dirac_operator_Wstat.h"
+
+#define basetype spincolor
+
+#define ndoubles_per_site 24
+#define size_of_bulk loc_vol
+#define size_of_bord bord_vol
+
+#define apply_operator apply_Wstat2
+
+#define cg_invert inv_Wstat_cg
+#define cg_parameters_proto quad_su3 *conf
+#define cg_inner_parameters_call conf,t
+
+#define cg_additional_vectors_allocation()                              \
+  basetype *t=nissa_malloc("DD_temp",size_of_bulk+size_of_bord,basetype);
+#define cg_additional_vectors_free()            \
+  nissa_free(t);
+
+#include "../templates/cg_invert_template.cpp"
