0dba13b28eedea4400ee5e5c3a6936c655b39813
fr.sanfilippo@gmail.com <fr.sanfilippo@gmail.com@35dfebc6-1a16-7261-d8b2-d758fa207421>
2012-12-04 09:25:34 +0000

    added stouting parameters to the theory

--- src/operations/smearing/stout.h
+++ src/operations/smearing/stout.h
@@ -1,4 +1,4 @@
 #ifndef _STOUT_H
 #define _STOUT_H
-void stout_smearing(quad_su3 **out,quad_su3 **in,stout_pars rho);
+void stout_smearing(quad_su3 **out,quad_su3 **in,stout_pars rho,int niters=1);
 #endif
