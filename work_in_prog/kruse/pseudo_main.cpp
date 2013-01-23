a1f12340b10e9d6186dce44a903ef0adbabf1654
fr.sanfilippo@gmail.com <fr.sanfilippo@gmail.com@35dfebc6-1a16-7261-d8b2-d758fa207421>
2013-01-23 21:30:12 +0000

    going on with external interface for kruse dirac operator

--- work_in_prog/kruse/pseudo_main.cpp
+++ work_in_prog/kruse/pseudo_main.cpp
@@ -1,14 +1,27 @@
-#include "src/interface/interface.h"
-
 #include "../../src/nissa.h"
+
+#include "src/interface/external_interface.h"
 
 int main(int narg,char **arg)
 {
   init_nissa(narg,arg);
   
-  init_grid(4,4);
+  init_grid(8,4);
   
   init_additional_variables();
+  
+  //////////////////////////////////
+  
+  quad_su3 *conf=nissa_malloc("conf",loc_vol+bord_vol,quad_su3);
+  
+  read_ildg_gauge_conf(conf,"/Users/francesco/Prace/nissa/test/data/L4T8conf");
+  master_printf("Plaquette: %16.16lg\n",global_plaquette_lx_conf(conf));
+  
+  bgq_gaugefield_init();
+  
+  nissa_free(conf);
+  
+  //////////////////////////////////
   
   unset_additional_variables();
   
