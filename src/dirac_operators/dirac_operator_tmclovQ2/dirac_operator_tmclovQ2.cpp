cdc310b6e6de696ee78c944093f5be0733ba1bba
fr.sanfilippo@gmail.com <fr.sanfilippo@gmail.com@35dfebc6-1a16-7261-d8b2-d758fa207421>
2013-01-30 15:06:56 +0000

    fixed a couple of bugs

--- src/dirac_operators/dirac_operator_tmclovQ2/dirac_operator_tmclovQ2.cpp
+++ src/dirac_operators/dirac_operator_tmclovQ2/dirac_operator_tmclovQ2.cpp
@@ -10,9 +10,9 @@
 
 void apply_tmclovQ2(spincolor *out,quad_su3 *conf,double kappa,double csw,as2t_su3 *Pmunu,spincolor *temp,double mu,spincolor *in)
 {
-  apply_tmclovQ(temp,conf,kappa,csw,Pmunu,mu,in);
-  apply_tmclovQ(out,conf,kappa,csw,Pmunu,mu,temp);
+  apply_tmclovQ(temp,conf,kappa,csw,Pmunu,+mu,in);
+  apply_tmclovQ(out,conf,kappa,csw,Pmunu,-mu,temp);
 }
 
 void apply_tmclovQ2_m2(spincolor *out,quad_su3 *conf,double kappa,double csw,as2t_su3 *Pmunu,spincolor *temp,double mu,spincolor *in)
-{apply_tmclovQ2_m2(out,conf,kappa,csw,Pmunu,temp,sqrt(mu),in);}
+{apply_tmclovQ2(out,conf,kappa,csw,Pmunu,temp,sqrt(mu),in);}
