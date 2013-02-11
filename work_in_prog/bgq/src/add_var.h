7d9354cb7cd51fba9100bd823c4afb7ed2870ed8
fr.sanfilippo@gmail.com <fr.sanfilippo@gmail.com@35dfebc6-1a16-7261-d8b2-d758fa207421>
2013-02-11 13:40:41 +0000

    added sending

--- work_in_prog/bgq/src/add_var.h
+++ work_in_prog/bgq/src/add_var.h
@@ -13,6 +13,8 @@ typedef uint8_t coords_5D[5];
 //structure used to hold spi buffers 
 struct spi_comm_t
 {
+  //communication in progress
+  int comm_in_prog;
   //size of the buffers, buffers
   uint64_t buf_size;
   char *send_buf,*recv_buf;
@@ -30,8 +32,8 @@ coords_5D spi_rank_coord;
 //neighbours int the 4 dirs
 MUHWI_Destination_t spi_neigh[2][4];
 
-//spi fifo
-uint64_t *spi_fifo[8];
+//spi fifo and counters for bytes
+uint64_t *spi_fifo[8],spi_desc_count[8];
 MUSPI_InjFifoSubGroup_t spi_fifo_sg_ptr;
 
 //spi barrier
