7f3585b023961dd49b9627c8de3aba700091a13d
fr.sanfilippo@gmail.com <fr.sanfilippo@gmail.com@35dfebc6-1a16-7261-d8b2-d758fa207421>
2013-03-05 00:13:49 +0000

    added ptr

--- src/routines/openmp.cpp
+++ src/routines/openmp.cpp
@@ -15,17 +15,20 @@ void thread_barrier()
 //thread pool
 void thread_pool()
 {
-  //check that thread 0 is not inside the ppol
-  if(thread_id==0) crash("thread 0 must not enter the pool");
+  //check that thread 0 is not inside the pool
+  if(thread_id==0) crash("thread 0 cannot enter the pool");
   
   //loop until not asked to exit
   do
     {
-      //hold until not released, then if we have to stay in pool execute function
+      //hold until not unlocked
       thread_barrier();
+      
+      //if we have to stay in pool execute function, and wait that all the thread sync
       if(thread_stay_in_pool)
 	{
 	  threaded_function_ptr();
+	  thread_barrier();
 	}
     }
   while(thread_stay_in_pool);
@@ -34,12 +37,13 @@ void thread_pool()
 //execute a function using all threads
 void threaded_function_exec(void(*function)(void))
 {
-  //set external function pointer and release threads
+  //set external function pointer and unlock pool threads
   threaded_function_ptr=function;
   thread_barrier();
   
-  //execute the function
+  //execute the function and sync with other threads
   threaded_function_ptr();
+  thread_barrier();
 }
 
 //delete the thread pool
@@ -48,20 +52,18 @@ void thread_pool_stop()
   //check to be thread 0
   if(thread_id!=0) crash("only thread 0 can stop the pool");
   
-  //release threads from the pool
+  //release threads from the pool and unlock them
   thread_stay_in_pool=0;
-  
-  //now we just have to release the other threads
   thread_barrier();
 }
 
-//start the master thread, keeping all the other threads in hold
+//start the master thread, locking all the other threads
 void thread_master_start(int narg,char **arg,void(*main_function)(int narg,char **arg))
 {
   //write the number of thread
   master_printf("Starting %d threads\n",omp_get_num_threads());
   
-  //stuck the other thread in the pool
+  //lock the other threads in the pool
   thread_stay_in_pool=1;
   
   //initialize nissa
@@ -80,10 +82,10 @@ void init_nissa_threaded(int narg,char **arg,void(*main_function)(int narg,char 
 {
 #pragma omp parallel
   {
-    //take local thread id
+    //take thread id
     thread_id=omp_get_thread_num();
     
-    //discriminate master thread from the others
+    //distinguish master thread from the others
     if(thread_id!=0) thread_pool();
     else thread_master_start(narg,arg,main_function);
   }
