039bec6383ecb0c68a991c8af1226aebea8e073a
fr.sanfilippo <fr.sanfilippo@gmail.com>
2013-03-18 08:45:04 +0000

    fixed chiral condensate and slightly changed cg output

--- src/operations/contract_stag.cpp
+++ src/operations/contract_stag.cpp
@@ -8,24 +8,26 @@
 #include "../base/vectors.h"
 #include "../geometry/geometry_eo.h"
 #include "../hmc/backfield.h"
+#include "../linalgs/linalgs.h"
 #include "../inverters/staggered/cg_invert_stD.h"
 #include "../new_types/new_types_definitions.h"
 #include "../new_types/complex.h"
 #include "../routines/ios.h"
 #include "../routines/mpi.h"
+#include "../routines/openmp.h"
 
 //get a propagator
-void get_propagator(color **prop,quad_su3 **conf,quad_u1 **u1b,double m,double residue,color **source)
+THREADABLE_FUNCTION_6ARG(get_propagator, color**,prop, quad_su3**,conf, quad_u1**,u1b, double,m, double,residue, color**,source)
 {
   addrem_stagphases_to_eo_conf(conf);
   add_backfield_to_conf(conf,u1b);
   inv_stD_cg(prop,conf,m,10000,5,residue,source);
   rem_backfield_from_conf(conf,u1b);
   addrem_stagphases_to_eo_conf(conf);
-}
+}}
 
 //compute the chiral condensate
-void chiral_condensate(complex cond,quad_su3 **conf,quad_u1 **u1b,double m,double residue)
+THREADABLE_FUNCTION_5ARG(chiral_condensate, complex*,cond, quad_su3**,conf, quad_u1**,u1b, double,m, double,residue)
 {
   //allocate
   color *rnd[2]={nissa_malloc("rnd_EVN",loc_volh+bord_volh,color),nissa_malloc("rnd_ODD",loc_volh+bord_volh,color)};
@@ -35,18 +37,14 @@ void chiral_condensate(complex cond,quad_su3 **conf,quad_u1 **u1b,double m,doubl
   generate_fully_undiluted_source(rnd,RND_Z4,-1);
   get_propagator(chi,conf,u1b,m,residue,rnd);
   
-  //compute the condensate
-  complex loc_cond={0,0};
-  for(int par=0;par<2;par++)
-    nissa_loc_volh_loop(ivol)
-      for(int icol=0;icol<3;icol++)
-	complex_summ_the_conj2_prod(loc_cond,chi[par][ivol][icol],rnd[par][ivol][icol]);
-
-  //global reduction  
-  glb_reduce_complex(cond,loc_cond);
+  //summ the scalar prod of EVN and ODD parts
+  complex temp[2];
+  for(int eo=0;eo<2;eo++)
+    complex_vector_glb_scalar_prod(temp+eo,(complex*)(rnd[eo]),(complex*)(chi[eo]),3*loc_volh);
+  complex_summ(*cond,temp[0],temp[ODD]);
   
   //add normalization: 1/4vol
-  complex_prodassign_double(cond,1.0/(4*glb_vol));
+  complex_prodassign_double(*cond,1.0/(4*glb_vol));
   
   //free
   for(int par=0;par<2;par++)
@@ -54,7 +52,7 @@ void chiral_condensate(complex cond,quad_su3 **conf,quad_u1 **u1b,double m,doubl
       nissa_free(rnd[par]);
       nissa_free(chi[par]);
     }
-}
+}}
 
 //measure chiral cond
 void measure_chiral_cond(quad_su3 **conf,theory_pars_type &theory_pars,int iconf)
@@ -76,7 +74,7 @@ void measure_chiral_cond(quad_su3 **conf,theory_pars_type &theory_pars,int iconf
           
           //compute and summ
           complex temp;
-          chiral_condensate(temp,conf,theory_pars.backfield[iflav],theory_pars.quark_content[iflav].mass,theory_pars.chiral_cond_pars.residue);
+          chiral_condensate(&temp,conf,theory_pars.backfield[iflav],theory_pars.quark_content[iflav].mass,theory_pars.chiral_cond_pars.residue);
           complex_summ_the_prod_double(cond,temp,1.0/nhits);
         }
       
