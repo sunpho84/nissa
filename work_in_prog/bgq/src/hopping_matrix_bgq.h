7b6fd0564f27d71aab9bb8dd9d09cdc91d2f9416
fr.sanfilippo <fr.sanfilippo@gmail.com>
2013-05-03 16:24:16 +0000

    incredible but true, working...

--- work_in_prog/bgq/src/hopping_matrix_bgq.h
+++ work_in_prog/bgq/src/hopping_matrix_bgq.h
@@ -3,7 +3,7 @@
 
 void apply_Wilson_hopping_matrix_bgq_binded_nocomm_nobarrier(bi_oct_su3 *conf,int istart,int iend,bi_spincolor *in);
 void bgq_Wilson_hopping_matrix_T_VN_comm_and_buff_fill(bi_halfspincolor *out);
-void start_bgq_Wilson_hopping_matrix_communications();
-void finish_bgq_Wilson_hopping_matrix_communications();
+void start_Wilson_hopping_matrix_bgq_binded_communications();
+void finish_Wilson_hopping_matrix_bgq_binded_communications();
 
 #endif
