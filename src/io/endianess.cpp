02d98b7e525e5177438cb14ace7c40cf23f0bf7f
fr.sanfilippo <fr.sanfilippo@gmail.com>
2013-08-26 14:01:11 +0000

    moved io

---
+++ src/io/endianess.cpp
@@ -0,0 +1,202 @@
+#ifdef HAVE_CONFIG_H
+ #include "config.h"
+#endif
+
+#include "../base/debug.h"
+#include "../base/global_variables.h"
+#include "../routines/ios.h"
+
+#include "reader.h"
+
+//check the endianess of the machine
+void check_endianess()
+{
+  little_endian=1;
+  little_endian=(int)(*(char*)(&little_endian));
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//fucking tool to revert the endianess of doubles
+void doubles_to_doubles_changing_endianess(double *dest,double *sour,int ndoubles,int verbose=1)
+{
+  char *cdest,*csour;
+  char temp;
+  
+  if(verbose) verbosity_lv3_master_printf("Reverting the endianess of the data\n");
+  
+  if(dest==sour)
+    for(int idouble=0;idouble<ndoubles;idouble++)
+      {
+	cdest=(char*)(dest+idouble);
+	csour=(char*)(sour+idouble);
+	
+	temp=csour[7];
+	csour[7]=cdest[0];
+	cdest[0]=temp;
+
+	temp=csour[6];
+	csour[6]=cdest[1];
+	cdest[1]=temp;
+
+	temp=csour[5];
+	csour[5]=cdest[2];
+	cdest[2]=temp;
+
+	temp=csour[4];
+	csour[4]=cdest[3];
+	cdest[3]=temp;
+    }
+  else
+    for(int idouble=0;idouble<ndoubles;idouble++)
+      {
+	cdest=(char*)(dest+idouble);
+	csour=(char*)(sour+idouble);
+	
+	cdest[0]=csour[7];
+	cdest[1]=csour[6];
+	cdest[2]=csour[5];
+	cdest[3]=csour[4];
+	cdest[4]=csour[3];
+	cdest[5]=csour[2];
+	cdest[6]=csour[1];
+	cdest[7]=csour[0];
+
+    }
+}
+
+void floats_to_floats_changing_endianess(float *dest,float *sour,int nfloats,int verbose=1)
+{
+  char *cdest,*csour;
+  char temp;
+  
+  if(verbose) verbosity_lv3_master_printf("Reverting the endianess of the data\n");
+
+  if(dest==sour)
+    for(int ifloat=0;ifloat<nfloats;ifloat++)
+      {
+	cdest=(char*)(dest+ifloat);
+	csour=(char*)(sour+ifloat);
+	
+	temp=csour[3];
+	csour[3]=cdest[0];
+	cdest[0]=temp;
+
+	temp=csour[2];
+	csour[2]=cdest[1];
+	cdest[1]=temp;
+    }
+  else
+    for(int ifloat=0;ifloat<nfloats;ifloat++)
+      {
+	cdest=(char*)(dest+ifloat);
+	csour=(char*)(sour+ifloat);
+	
+	cdest[0]=csour[3];
+	cdest[1]=csour[2];
+	cdest[2]=csour[1];
+	cdest[3]=csour[0];
+    }
+}
+
+void uint64s_to_uint64s_changing_endianess(uint64_t *dest,uint64_t *sour,int nints,int verbose=1)
+{doubles_to_doubles_changing_endianess((double*)dest,(double*)sour,nints,verbose);}
+
+void uint32s_to_uint32s_changing_endianess(uint32_t *dest,uint32_t *sour,int nints,int verbose=1)
+{floats_to_floats_changing_endianess((float*)dest,(float*)sour,nints,verbose);}
+
+void uint16s_to_uint16s_changing_endianess(uint16_t *dest,uint16_t *sour,int nshorts,int verbose=1)
+{
+  char *cdest,*csour;
+  char temp;
+  
+  if(verbose) verbosity_lv3_master_printf("Reverting the endianess of the data\n");
+
+  if(dest==sour)
+    for(int ishort=0;ishort<nshorts;ishort++)
+      {
+	cdest=(char*)(dest+ishort);
+	csour=(char*)(sour+ishort);
+	
+	temp=csour[1];
+	csour[1]=cdest[0];
+	cdest[0]=temp;
+    }
+  else
+    for(int ishort=0;ishort<nshorts;ishort++)
+      {
+	cdest=(char*)(dest+ishort);
+	csour=(char*)(sour+ishort);
+	
+	cdest[0]=csour[1];
+	cdest[1]=csour[0];
+    }
+}
+
+////////////////////Copy a vector of floats to doubles. Sweep is reversed to avoid overwriting////////////////
+
+//Do not change endianess
+void floats_to_doubles_same_endianess(double *dest,float *sour,int n,int verbose=1)
+{
+  if(verbose) verbosity_lv3_master_printf("Converting %d floats to doubles\n",n);
+  
+  for(int i=n-1;i>=0;i--) dest[i]=(double)(sour[i]);
+}
+
+//Change endianess
+void floats_to_doubles_changing_endianess(double *dest,float *sour,int n,int verbose=1)
+{
+  char *c;
+  char temp;
+
+  if(verbose) verbosity_lv3_master_printf("Converting %d floats to doubles changing endianess\n",n);
+
+  for(int i=n-1;i>=0;i--)
+    {
+      c=(char*)(sour+i);
+      
+      temp=c[3];
+      c[3]=c[0];
+      c[0]=temp;
+
+      temp=c[2];
+      c[2]=c[1];
+      c[1]=temp;
+      
+      dest[i]=(double)(sour[i]);
+    }
+}
+
+////////////////////Copy a vector of doubles to floats. Sweep is direct, to avoid overwriting////////////////
+
+//Do not change the endianess
+void doubles_to_floats_same_endianess(float *dest,double *sour,int n,int verbose=1)
+{
+  if(verbose) verbosity_lv3_master_printf("Converting %d doubles to floats\n",n);
+
+  for(int i=0;i<n;i++) dest[i]=(float)(sour[i]);
+}
+
+//Change endianess
+void doubles_to_floats_changing_endianess(float *dest,double *sour,int n,int verbose=1)
+{
+  char *c;
+  char temp;
+
+  if(verbose) verbosity_lv3_master_printf("Converting %d doubles to floats changing endianess\n",n);
+
+  for(int i=0;i<n;i++)
+    {
+      dest[i]=(float)(sour[i]);
+
+      c=(char*)(dest+i);
+
+      temp=c[3];
+      c[3]=c[0];
+      c[0]=temp;
+
+      temp=c[2];
+      c[2]=c[1];
+      c[1]=temp;
+    }
+}
