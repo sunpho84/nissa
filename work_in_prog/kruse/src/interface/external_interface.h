91e2d09aef3d28953b492034aa494de18fe60765
fr.sanfilippo@gmail.com <fr.sanfilippo@gmail.com@35dfebc6-1a16-7261-d8b2-d758fa207421>
2013-08-28 14:11:16 +0000

    removed a bit of old stuff

--- work_in_prog/kruse/src/interface/external_interface.h
+++
@@ -1,8 +0,0 @@
-#ifndef _EXTERNAL_INTERFACE_H
-#define _EXTERNAL_INTERFACE_H
-
-#include "interface.h"
-
-#include "../kruse/bgq_gaugefield.h"
-
-#endif
--- work_in_prog/kruse/src/interface/external_routines.h
+++
@@ -1,7 +0,0 @@
-#ifndef _EXTERNAL_ROUTINES_H
-#define _EXTERNAL_ROUTINES_H
-
-int full_lx_of_coords_list(const int x0,const int x1,const int x2,const int x3);
-int loclx_of_coords_list(int x0,int x1,int x2,int x3);
-
-#endif
