df2df2d2e42ae7c6ba3265d28c9c4c4c24c604ed
fr.sanfilippo@gmail.com <fr.sanfilippo@gmail.com@35dfebc6-1a16-7261-d8b2-d758fa207421>
2013-09-16 20:45:40 +0000

    added portable and bgq versions

---
+++ src/inverters/staggered/cg_invert_stD2ee_m2_bgq.cpp
@@ -0,0 +1,38 @@
+#include <math.h>
+
+#include "communicate/communicate.h"
+#include "base/debug.h"
+#include "base/global_variables.h"
+#include "base/vectors.h"
+#include "dirac_operators/dirac_operator_stD/dirac_operator_stD_bgq.h"
+#include "linalgs/linalgs.h"
+#include "new_types/new_types_definitions.h"
+
+#define BASETYPE bi_color
+#define NDOUBLES_PER_SITE 12
+#define BULK_VOL loc_volh/2
+#define BORD_VOL 0
+
+#define APPLY_OPERATOR apply_stD2ee_m2_bgq
+#define CG_OPERATOR_PARAMETERS conf,t,m2,
+
+#define CG_INVERT inv_stD2ee_m2_cg_bgq
+#define CG_NPOSSIBLE_REQUESTS 0
+
+//maybe one day async comm
+//#define cg_start_communicating_borders start_communicating_ev_color_borders
+//#define cg_finish_communicating_borders finish_communicating_ev_color_borders
+
+#define CG_ADDITIONAL_VECTORS_ALLOCATION()				\
+  BASETYPE *t=nissa_malloc("DD_temp",BULK_VOL+BORD_VOL,BASETYPE);
+#define CG_ADDITIONAL_VECTORS_FREE()		\
+  nissa_free(t);
+
+//additional parameters
+#define CG_NARG 2
+#define AT1 bi_oct_su3**
+#define A1 conf
+#define AT2 double
+#define A2 m2
+
+#include "templates/cg_invert_template_threaded.cpp"
