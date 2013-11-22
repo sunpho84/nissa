f1d9df6aa1e8f32572b5f67f96a449ae95387a48
fr.sanfilippo <fr.sanfilippo@gmail.com>
2013-11-22 15:28:02 +0000

    adding working version of stout program

--- tools/conf_transform/smear/stout.cpp
+++ tools/conf_transform/smear/stout.cpp
@@ -9,68 +9,49 @@ int main(int narg,char **arg)
   //basic mpi initialization
   init_nissa(narg,arg);
   
-  if(narg<2) crash("use: %s input",arg[0]);
+  if(narg<7) crash("use: %s L T filein rho nlev fileout",arg[0]);
   
-  
+  stout_pars_t stout_pars;
+  int L=atoi(arg[1]);
+  int T=atoi(arg[2]);
+  char *pathin=arg[3];
+  double rho;
+  sscanf(arg[4],"%lg",&rho);
+  for(int i=0;i<4;i++) for(int j=0;j<4;j++) stout_pars.rho[i][j]=rho;
+  stout_pars.nlev=atoi(arg[5]);
+  char *pathout=arg[6];
   
   //Init the MPI grid 
   init_grid(T,L);
 
   //////////////////////////// read the conf /////////////////////////////
 
-  quad_su3 *in_conf=nissa_malloc("in_conf",loc_vol,quad_su3);
+  quad_su3 *in_conf[2]={nissa_malloc("in_conf_e",loc_volh+bord_volh+edge_volh,quad_su3),
+			nissa_malloc("in_conf_o",loc_volh+bord_volh+edge_volh,quad_su3)};
   
-  //print the plaquette and write the conf
-  read_ildg_gauge_conf(in_conf,arg[3]);
-  master_printf("Global plaquette: %16.16lg\n",global_plaquette_lx_conf(in_conf));
+  //read the conf and write plaquette
+  ILDG_message mess;
+  ILDG_message_init_to_last(&mess);
+  read_ildg_gauge_conf_and_split_into_eo_parts(in_conf,pathin,&mess);
+  master_printf("Original plaquette: %16.16lg\n",global_plaquette_eo_conf(in_conf));
 
-  //////////////////////////// convert the conf //////////////////////////
+  //////////////////////////// stout the conf //////////////////////////
   
-  su3 *out_conf=nissa_malloc("out_conf",4*loc_vol,su3);
-  
-  //add phases
-  addrem_stagphases_to_lx_conf(in_conf);
-  
-  //reorder the conf
-  int map_mu[4]={1,2,3,0};
-  for(int t=0;t<T;t++)
-    for(int z=0;z<L;z++)
-      for(int y=0;y<L;y++)
-	for(int x=0;x<L;x++)
-	  {
-	    int sum=x+y+z+t;
-	    int even=sum%2;
-	    int num=even*loc_volh + snum(x,y,z,t);
-	    
-	    coords c={t,x,y,z};
-	    int ivol=loclx_of_coord(c);
-	    
-	    for(int mu=0;mu<4;mu++)
-	      su3_copy(out_conf[mu*loc_vol+num],in_conf[ivol][map_mu[mu]]);
-	  }
-  
-  nissa_free(in_conf);
-  
-  /////////////////////////// write converted conf ////////////////////////
-  
-  //open the file
-  FILE *fout=fopen(arg[4],"w");
-  if(fout==NULL) crash("while opening %s",arg[4]);
+  //smear
+  quad_su3 *out_conf[2]={nissa_malloc("out_conf_e",loc_volh+bord_volh+edge_volh,quad_su3),
+			 nissa_malloc("out_conf_o",loc_volh+bord_volh+edge_volh,quad_su3)};
+  stout_smear(out_conf,in_conf,&stout_pars);
 
-  //write header
-  int nx=L,ny=L,nz=L,nt=T,nflav=2,ntraj=0;
-  double beta=1000,mass=1000;
-  if(fprintf(fout,"%d %d %d %d %lg %lg %d %d",nx,ny,nz,nt,beta,mass,nflav,ntraj)!=8) crash("writing header");
-  
-  //write the file
-  NISSA_LOC_VOL_LOOP(ivol)
-    for(int mu=0;mu<4;mu++)
-      write_su3(fout,out_conf[ivol*4+mu]);
-  
-  //close the file
-  fclose(fout);
-  
-  nissa_free(out_conf);
+  //print plaquette and write the conf
+  master_printf("Smeared plaquette: %16.16lg\n",global_plaquette_eo_conf(out_conf));
+  paste_eo_parts_and_write_ildg_gauge_conf(pathout,out_conf,64,&mess);
+
+  for(int eo=0;eo<2;eo++)
+    {
+      nissa_free(out_conf[eo]);
+      nissa_free(in_conf[eo]);
+    }
+  ILDG_message_free_all(&mess);
   
   ///////////////////////////////////////////
 
