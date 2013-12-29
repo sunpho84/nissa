05101fb897e62c3d6e7eb87f576393f8d8205c8b
fr.sanfilippo@gmail.com <fr.sanfilippo@gmail.com@35dfebc6-1a16-7261-d8b2-d758fa207421>
2013-12-29 08:56:11 +0000

    added timing

--- projects/generate_confs/generate_quenched.cpp
+++ projects/generate_confs/generate_quenched.cpp
@@ -616,16 +616,22 @@ void measure_gauge_obs(bool conf_created=false)
   FILE *file=open_file(gauge_obs_path,conf_created?"w":"a");
 
   //compute action
+  double time_action=-take_time();
   double action=compute_tlSym_action();
-  
+  master_printf("Action: %015.15lg measured in %lg sec\n",action,time_action+take_time());
+
   //paths
   double paths[2];
+  double time_paths=-take_time();
   global_plaquette_and_rectangles_lx_conf(paths,conf);
+  master_printf("Plaq: %015.15lg, rects: %015.15lg measured in %lg sec\n",paths[0],paths[1],time_paths+take_time());
   
   //polyakov loop
   complex pol;
+  double time_pol=-take_time();
   average_polyakov_loop_lx_conf(pol,conf,0);
-  
+  master_printf("Polyakov: %015.15lg, %015.15lg measured in %lg sec\n",pol[0],pol[1],time_pol+take_time());
+
   master_fprintf(file,"%6d\t%015.15lg\t%015.15lg\t%015.15lg\t%+015.15lg\t%+015.15lg\n",
 		 iconf,action,paths[0],paths[1],pol[0],pol[1]);
   
