26ea55cf304a110f61f20010448b38e4bcc6fefd
fr.sanfilippo@gmail.com <fr.sanfilippo@gmail.com@35dfebc6-1a16-7261-d8b2-d758fa207421>
2014-01-01 19:14:01 +0000

    improved su3 algebra

--- src/create_headers.sh
+++ src/create_headers.sh
@@ -16,5 +16,5 @@ then
     grep include $HFILE
 fi
 
-grep "(" $1|grep -v "//"|grep -v "basetype"|awk '{a=substr($0,0,1)}a!=" " && a!="\t" && a!= "{" && NF>=2'|grep -v define|grep -v pragma|grep -v main\(|sort|uniq|awk -F { '{print $1";"}' 
+egrep ' +[[:alnum:]_]+ [[:alnum:]_]+\([[:alnum:]_, &*]+\)' $1|grep -v "//"|sort|uniq|awk -F { '{print $1";"}' 
 echo "#endif"
