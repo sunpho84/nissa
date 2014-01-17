9bc38edb58de946ddca4426130f29d82e5072bc0
fr.sanfilippo@gmail.com <fr.sanfilippo@gmail.com@35dfebc6-1a16-7261-d8b2-d758fa207421>
2014-01-17 22:26:58 +0000

    forgot argument in two calls

--- projects/generate_confs/generate_quenched.cpp
+++ projects/generate_confs/generate_quenched.cpp
@@ -311,7 +311,7 @@ void close_simulation()
   master_printf("=========================================\n");
   master_printf("\n");
   
-  if(store_running_temp_conf==0||iconf%store_running_temp_conf!=0) write_conf();
+  if(store_running_temp_conf==0||iconf%store_running_temp_conf!=0) write_conf(conf_path);
   nissa_free(conf);
 }
 
@@ -432,7 +432,7 @@ void in_main(int narg,char **arg)
       if(top_meas_pars.flag && iconf%top_meas_pars.flag==0) measure_topology(top_meas_pars,conf,iconf,0);
 	
       // 3) increment id and write conf
-      if(store_running_temp_conf && iconf%store_running_temp_conf==0) write_conf();
+      if(store_running_temp_conf && iconf%store_running_temp_conf==0) write_conf(conf_path);
       
       // 4) if conf is multiple of store_conf_each copy it
       store_conf_if_necessary();
