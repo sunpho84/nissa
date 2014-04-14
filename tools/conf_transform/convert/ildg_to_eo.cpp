c5ce234eb291807d813fd74faad78b2d3b51f633
fr.sanfilippo <fr.sanfilippo@gmail.com>
2014-04-14 16:49:58 +0000

    fixed ildg->eo conversion

--- tools/conf_transform/convert/ildg_to_eo.cpp
+++ tools/conf_transform/convert/ildg_to_eo.cpp
@@ -1,130 +1,84 @@
 #include "nissa.hpp"
 
-int T,L;
-
 using namespace nissa;
+
+int T,L;
+vector_remap_t *remapper;
+
+void index_from_lx_to_Neo(int &rank_out,int &iel_out,int iel_in,void *pars)
+{
+  int mu_ord[4]={0,3,2,1};
+  int ilx=iel_in/4;
+  int mu=iel_in-ilx*4;
+  
+  //odd sites goes with themseleves
+  int shift_comp=(loclx_parity[ilx]==0);
+  
+  coords g;
+  for(int nu=0;nu<4;nu++) g[mu_ord[nu]]=glb_coord_of_loclx[ilx][nu];
+  if(shift_comp) g[mu_ord[mu]]=(g[mu_ord[mu]]+1)%glb_size[mu_ord[mu]];
+  
+  int glb_site_dest=(glblx_of_coord(g)/2)*8+mu_ord[mu]*2+shift_comp;
+  rank_out=glb_site_dest/(8*loc_volh);
+  iel_out=glb_site_dest-rank_out*8*loc_volh;
+}
 
 void conf_convert(char *outpath,char *inpath)
 {
   quad_su3 *conf=nissa_malloc("conf",loc_vol+bord_vol,quad_su3);
   read_ildg_gauge_conf(conf,inpath);
-  double plaq=global_plaquette_lx_conf(conf)*3;
   
-  FILE *fout=NULL;
+  //compute and convert the plaquette
+  double plaq=global_plaquette_lx_conf(conf)*3;
+  if(!little_endian) doubles_to_doubles_changing_endianness(&plaq,&plaq,1);
+  
+  //convert the lattice size
+  coords temp;
+  if(!little_endian) uint32s_to_uint32s_changing_endianness((uint32_t*)temp,(uint32_t*)glb_size,4);
+  else               memcpy(temp,glb_size,sizeof(coords));
+  
+  //write the header
+  FILE *fout=fopen(outpath,"w");
   if(rank==0)
     {
       int nw;
-      master_printf("Opening to write!\n");
-      fout=fopen(outpath,"w");  
-      master_printf("Opened to write!\n");
-  
-      coords temp;
-      if(!little_endian) uint32s_to_uint32s_changing_endianness((uint32_t*)temp,(uint32_t*)glb_size,4);
-      else               memcpy(temp,glb_size,sizeof(coords));
       nw=fwrite(temp,sizeof(coords),1,fout);
       if(nw!=1) crash("did not success in writing");
-      
-      if(!little_endian) doubles_to_doubles_changing_endianness(&plaq,&plaq,1);
       nw=fwrite(&plaq,sizeof(double),1,fout);
       if(nw!=1) crash("did not success in writing");
-   
-      fclose(fout);
     }
   MPI_Barrier(MPI_COMM_WORLD);
   
+  //if needed convert the endianess of the conf
   if(!little_endian) doubles_to_doubles_changing_endianness((double*)conf,(double*)conf,loc_vol*4*18);
   
+  //reorder
   char *buf=nissa_malloc("buf",loc_vol*sizeof(quad_su3),char);
-  int ibuf=0;
-  int remap[4]={0,3,2,1};
-  coords g;
-  int iscan=0;
-  for(g[0]=0;g[0]<T;g[0]++)
-    for(g[3]=0;g[3]<L;g[3]++)
-      for(g[2]=0;g[2]<L;g[2]++)
-	for(g[1]=0;g[1]<L;g[1]++)
-	  if((g[0]+g[1]+g[2]+g[3])%2)
-	    {
-	      int dest_rank=iscan/loc_vol;
-	      
-	      int iw,iw_rank;
-	      get_loclx_and_rank_of_coord(&iw,&iw_rank,g);
-	      for(int mu=0;mu<4;mu++)
-		{
-		  int nu=remap[mu];
-		  int iz,iz_rank;
-		  coords r;
-		  memcpy(r,g,sizeof(coords));
-		  r[nu]=(g[nu]+glb_size[nu]-1)%glb_size[nu];
-		  get_loclx_and_rank_of_coord(&iz,&iz_rank,r);
-		  
-		  if(rank==iw_rank)
-		    {
-		      if(dest_rank==rank)
-			{
-			  memcpy(buf+ibuf,conf[iw][nu],sizeof(su3));
-			  ibuf+=sizeof(su3);
-			}
-		      else MPI_Send(conf[iw][remap[mu]],sizeof(su3),MPI_CHAR,dest_rank,0,MPI_COMM_WORLD);
-		    }
-		  else
-		    if(rank==dest_rank)
-		      {
-			MPI_Recv(buf+ibuf,sizeof(su3),MPI_CHAR,iw_rank,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-			ibuf+=sizeof(su3);
-		      }
-		  
-		  if(rank==iz_rank)
-		    {
-		      if(dest_rank==rank)
-			{
-			  memcpy(buf+ibuf,conf[iz][nu],sizeof(su3));
-			  ibuf+=sizeof(su3);
-			}
-		      else MPI_Send(conf[iz][remap[mu]],sizeof(su3),MPI_CHAR,dest_rank,0,MPI_COMM_WORLD);
-		    }
-		  else
-		    if(rank==dest_rank)
-		      {
-			MPI_Recv(buf+ibuf,sizeof(su3),MPI_CHAR,iz_rank,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-			ibuf+=sizeof(su3);
-		      }
-		  
-		}
-	      iscan+=2;
-	    }
-  if(ibuf!=(int)sizeof(quad_su3)*loc_vol) crash("did not arrive to the end: %d %d",ibuf,sizeof(quad_su3)*loc_vol);
+  remapper->remap(buf,conf,sizeof(su3));
   
-  for(int irank=0;irank<nranks;irank++)
-    {
-      if(rank==irank)
-	{
-	  fout=fopen(outpath,"a");
-	  
-	  int nw=fwrite(buf,sizeof(quad_su3),loc_vol,fout);
-	  /*
-	    NISSA_LOC_VOL_LOOP(ivol)
-	    for(int mu=0;mu<4;mu++)
-	    int nw=fprintf(fout,"%lg\n",((quad_su3*)buf)[ivol][mu][0][0][0]);
-	  */
-	  if(nw!=loc_vol) crash("did not success in writing");
-	  
-	  fflush(fout);
-	  fclose(fout);
-	}
-      MPI_Barrier(MPI_COMM_WORLD);
-    }
+  //seek to the correct point
+  fseek(fout,24+rank*sizeof(quad_su3)*loc_vol,SEEK_SET);
+  MPI_Barrier(MPI_COMM_WORLD);
+
+  //write
+  int nw=fwrite(buf,sizeof(quad_su3),loc_vol,fout);
+  if(nw!=loc_vol) crash("did not success in writing");
+  MPI_Barrier(MPI_COMM_WORLD);
+ 
+  //flush
+  fflush(fout);
+  MPI_Barrier(MPI_COMM_WORLD);
+  
+  //close
+  fclose(fout);
+  MPI_Barrier(MPI_COMM_WORLD);
+
   nissa_free(buf);
   nissa_free(conf);
 }
 
-int main(int narg,char **arg)
+void in_main(int narg,char **arg)
 {
-  //basic mpi initialization
-  init_nissa(narg,arg);
-  
-  //if(nranks>1) crash("Cannot run in parallel");
-  
   if(narg<2) crash("Use: %s input",arg[0]);
   
   open_input(arg[1]);
@@ -138,6 +92,10 @@ int main(int narg,char **arg)
   //Init the MPI grid 
   init_grid(T,L);
   
+  //init the remapper
+  remapper=new vector_remap_t(4*loc_vol,index_from_lx_to_Neo,NULL);
+  
+  //read the number of gauge configurations
   int N;
   read_str_int("NGaugeConf",&N);
   
@@ -156,6 +114,12 @@ int main(int narg,char **arg)
   
   ///////////////////////////////////////////
   
+  delete remapper;
+}
+
+int main(int narg,char **arg)
+{
+  init_nissa_threaded(narg,arg,in_main);
   close_nissa();
   
   return 0;
---
+++ tools/conf_transform/create/create_hot.cpp
@@ -0,0 +1,32 @@
+#include "nissa.hpp"
+
+using namespace nissa;
+
+int main(int narg,char **arg)
+{
+  //basic mpi initialization
+  init_nissa(narg,arg);
+  
+  if(narg<5) crash("use: %s L T seed file_out",arg[0]);
+
+  int L=atoi(arg[1]);
+  int T=atoi(arg[2]);
+  int seed=atoi(arg[3]);
+
+  //Init the MPI grid 
+  init_grid(T,L);
+  start_loc_rnd_gen(seed);
+
+  //crete and write
+  quad_su3 *conf=nissa_malloc("conf",loc_vol,quad_su3);
+  generate_hot_lx_conf(conf);
+  write_ildg_gauge_conf(arg[4],conf,64);
+  
+  nissa_free(conf);
+
+  ///////////////////////////////////////////
+
+  close_nissa();
+
+  return 0;
+}
