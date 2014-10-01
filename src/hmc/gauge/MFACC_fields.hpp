81ae9cfa11fb590122dd641e2b4a5376d7057290
fr.sanfilippo <fr.sanfilippo@gmail.com>
2014-10-01 08:30:54 +0000

    adding Fourier acceleration-originated qcd force, still not working

--- src/hmc/gauge/MFACC_fields.hpp
+++ src/hmc/gauge/MFACC_fields.hpp
@@ -7,6 +7,7 @@ namespace nissa
   double MFACC_fields_action(su3 **phi);
   void evolve_MFACC_fields(su3 **phi,quad_su3 *conf,double kappa,su3 **pi,double dt);
   void evolve_MFACC_momenta(su3 **pi,su3 **phi,double dt);
+  void MFACC_momenta_QCD_force(quad_su3 *F,quad_su3 *conf,double kappa,su3 **pi);
 }
 
 #endif
