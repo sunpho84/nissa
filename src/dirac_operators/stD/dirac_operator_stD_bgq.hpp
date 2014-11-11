e8b05f23948547e941188713ccd62b37e0aa6a2b
fr.sanfilippo@gmail.com <fr.sanfilippo@gmail.com@35dfebc6-1a16-7261-d8b2-d758fa207421>
2014-11-11 21:55:49 +0000

    fixing pointless temp

--- src/dirac_operators/stD/dirac_operator_stD_bgq.hpp
+++ src/dirac_operators/stD/dirac_operator_stD_bgq.hpp
@@ -9,8 +9,8 @@
 
 namespace nissa
 {
-  void apply_stD2ee_m2_bgq(bi_color* out,bi_oct_su3 **conf,bi_color *temp,double mass2,bi_color *in);
-  void apply_single_stD2ee_m2_bgq(bi_single_color* out,bi_single_oct_su3 **conf,bi_single_color *temp,float mass2,bi_single_color *in);
+  void apply_stD2ee_m2_bgq(bi_color* out,bi_oct_su3 **conf,double mass2,bi_color *in);
+  void apply_single_stD2ee_m2_bgq(bi_single_color* out,bi_single_oct_su3 **conf,float mass2,bi_single_color *in);
 }
 
 #endif
