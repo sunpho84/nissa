333936bdd3c160fb44d63e954fa08ae0ecec1ab5
fr.sanfilippo@gmail.com <fr.sanfilippo@gmail.com@35dfebc6-1a16-7261-d8b2-d758fa207421>
2014-12-14 15:47:44 +0000

    fully implemented bgq tmclov Q2, working in emu mode, to be checked in true mode

--- src/inverters/tmclov/cgm_invert_tmclovQ2_bgq.hpp
+++ src/inverters/tmclov/cgm_invert_tmclovQ2_bgq.hpp
@@ -3,7 +3,7 @@
 
 namespace nissa
 {
-  void inv_tmclovQ2_cgm_bgq(bi_spincolor **sol,bi_oct_su3 *conf,double kappa,bi_opt_as2t_su3 *Cl,double *m,int nmass,int niter_max,double *req_res,bi_spincolor *source);
+  void inv_tmclovQ2_m2_cgm_bgq(bi_spincolor **sol,bi_oct_su3 *conf,double kappa,bi_opt_as2t_su3 *Cl,double *m2,int nmass,int niter_max,double *req_res,bi_spincolor *source);
 }
 
 #endif
