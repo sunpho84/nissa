d9164e2eb01a47ddde764ed170e9a5302aafe086
francesco <fr.sanfilippo@gmail.com>
2015-10-06 14:31:37 +0100

    changed default value

--- src/operations/su3_paths/topological_charge.hpp
+++ src/operations/su3_paths/topological_charge.hpp
@@ -1,11 +1,11 @@
-#ifndef _TOPOLOGICAL_CHARGE_H
-#define _TOPOLOGICAL_CHARGE_H
+#ifndef _TOPOLOGICAL_CHARGE_HPP
+#define _TOPOLOGICAL_CHARGE_HPP
 
 namespace nissa
 {
   void build_chromo_therm_from_anti_symmetric_four_leaves(quad_su3 out,as2t_su3 in);
   void measure_topology_eo_conf(top_meas_pars_t &pars,quad_su3 **uncooled_conf,int iconf,bool conf_created);
-  void measure_topology_lx_conf(top_meas_pars_t &pars,quad_su3 *uncooled_conf,int iconf,bool conf_created,bool presereve_uncooled=true);
+  void measure_topology_lx_conf(top_meas_pars_t &pars,quad_su3 *uncooled_conf,int iconf,bool conf_created,bool presereve_uncooled=false);
   double topodynamical_potential(double Q,topotential_pars_t &pars);
   void Pmunu_term(as2t_su3 *Pmunu,quad_su3 *conf);
   void opt_Pmunu_term(quad_su3 *C,quad_su3 *conf);
