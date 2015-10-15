a48fd761df253e7b33f42ad6e842efbe711c062d
francesco <fr.sanfilippo@gmail.com>
2015-10-15 18:13:21 +0100

    random gauge transform

--- tools/conf_transform/gaugefix/random.cpp
+++ tools/conf_transform/gaugefix/random.cpp
@@ -5,36 +5,23 @@ using namespace nissa;
 
 void in_main(int narg,char **arg)
 {
-  open_input(arg[1]);
-  
-  //Init the MPI grid
-  int L,T;
-  read_str_int("L",&L);
-  read_str_int("T",&T);
+  int L=atoi(arg[1]);
+  int T=atoi(arg[2]);
   init_grid(T,L);
   
-  char conf_in_path[1024];
-  read_str_str("InGaugePath",conf_in_path,1024);
-  double precision;
-  read_str_double("Precision",&precision);
-  char conf_out_path[1024];
-  read_str_str("OutGaugePath",conf_out_path,1024);
-  
-  close_input();
-  
-  start_loc_rnd_gen(1000);
+  start_loc_rnd_gen(atoi(arg[4]));
   
   ///////////////////////////////////////////
   
   quad_su3 *conf=nissa_malloc("Conf",loc_vol+bord_vol,quad_su3);
   quad_su3 *fix_conf=nissa_malloc("Conf2",loc_vol+bord_vol,quad_su3);
   
-  read_ildg_gauge_conf(conf,conf_in_path);
+  read_ildg_gauge_conf(conf,arg[3]);
   communicate_lx_quad_su3_borders(conf);
   
-  landau_gauge_fix(fix_conf,conf,precision);
+  perform_random_gauge_transform(fix_conf,conf);
   
-  write_ildg_gauge_conf(conf_out_path,fix_conf,64);
+  write_ildg_gauge_conf(arg[5],fix_conf,64);
   
   master_printf("plaq: %16.16lg\n",global_plaquette_lx_conf(conf));
   master_printf("plaq: %16.16lg\n",global_plaquette_lx_conf(fix_conf));
@@ -48,7 +35,7 @@ void in_main(int narg,char **arg)
 
 int main(int narg,char **arg)
 {
-  if(narg<2) crash("Use: %s input_file",arg[0]);
+  if(narg<6) crash("Use: %s L T path_in seed path_out",arg[0]);
   
   //basic mpi initialization
   init_nissa_threaded(narg,arg,in_main);
