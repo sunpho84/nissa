39d69c65d58a0a9cfde08afc081e4ddf7e0862fb
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2015-11-27 15:18:36 +0000

    adding stagphase

---
+++ tools/conf_transform/convert/gpu_stagphase.cpp
@@ -0,0 +1,43 @@
+#include "nissa.hpp"
+
+namespace nissa
+{
+  //multiply the whole conf for stag phases
+  THREADABLE_FUNCTION_1ARG(addrem_stagphases_to_lx_conf, quad_su3*,lx_conf)
+  {
+    //we must ensure that nobody is using the conf
+    THREAD_BARRIER();
+    
+    //work also on borders and edges if allocated and valid
+    int ending=loc_vol;
+    if(check_borders_allocated(lx_conf) && check_borders_valid(lx_conf)) ending+=bord_vol;
+    if(check_edges_allocated(lx_conf) && check_edges_valid(lx_conf)) ending+=edge_vol;
+    
+    GET_THREAD_ID();
+    NISSA_PARALLEL_LOOP(ivol,0,ending)
+      {
+	int d=0;
+	
+	//phase in direction 1 is always 0 so nothing has to be done in that dir
+	//if(d%2==1) su3_prod_double(lx_conf[ivol][1],lx_conf[ivol][1],-1);
+	
+	//direction 2
+	d+=glb_coord_of_loclx[ivol][1];
+	if(d%2==1) su3_prod_double(lx_conf[ivol][2],lx_conf[ivol][2],-1);
+	
+	//direction 3
+	d+=glb_coord_of_loclx[ivol][2];
+	if(d%2==1) su3_prod_double(lx_conf[ivol][3],lx_conf[ivol][3],-1);
+	
+	//direction 0
+	d+=glb_coord_of_loclx[ivol][3];
+	
+	//putting the anti-periodic condition on the temporal border
+	if(glb_coord_of_loclx[ivol][0]==glb_size[0]-1) d+=1;
+	if(d%2==1) su3_prod_double(lx_conf[ivol][0],lx_conf[ivol][0],-1);
+      }
+    
+    THREAD_BARRIER();
+  }
+  THREADABLE_FUNCTION_END
+}
---
+++ tools/conf_transform/convert/gpu_stagphase.hpp
@@ -0,0 +1,9 @@
+#ifndef _GPU_STAGPHASE_HPP
+#define _GPU_STAGPHASE_HPP
+
+namespace nissa
+{
+  void addrem_stagphases_to_lx_conf(quad_su3 *lx_conf);
+}
+
+#endif
