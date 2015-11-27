ecbd148f96fe0da42ee2de47b6098524a27dff5f
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2015-11-27 15:14:49 +0000

    putting back to work ildg_to_gpu

--- tools/conf_transform/convert/ildg_to_gpu.cpp
+++ tools/conf_transform/convert/ildg_to_gpu.cpp
@@ -19,27 +19,66 @@ void write_su3(FILE *out,su3 in)
       write_complex(out,in[i][j]);
 }
 
+//multiply the whole conf for stag phases
+THREADABLE_FUNCTION_1ARG(addrem_stagphases_to_lx_conf, quad_su3*,lx_conf)
+{
+  //we must ensure that nobody is using the conf
+  THREAD_BARRIER();
+  
+  //work also on borders and edges if allocated and valid
+  int ending=loc_vol;
+  if(check_borders_allocated(lx_conf) && check_borders_valid(lx_conf)) ending+=bord_vol;
+  if(check_edges_allocated(lx_conf) && check_edges_valid(lx_conf)) ending+=edge_vol;
+    
+  GET_THREAD_ID();
+  NISSA_PARALLEL_LOOP(ivol,0,ending)
+    {
+      int d=0;
+      
+      //phase in direction 1 is always 0 so nothing has to be done in that dir
+      //if(d%2==1) su3_prod_double(lx_conf[ivol][1],lx_conf[ivol][1],-1);
+      
+      //direction 2
+      d+=glb_coord_of_loclx[ivol][1];
+      if(d%2==1) su3_prod_double(lx_conf[ivol][2],lx_conf[ivol][2],-1);
+      
+      //direction 3
+      d+=glb_coord_of_loclx[ivol][2];
+      if(d%2==1) su3_prod_double(lx_conf[ivol][3],lx_conf[ivol][3],-1);
+      
+      //direction 0
+      d+=glb_coord_of_loclx[ivol][3];
+      
+      //putting the anti-periodic condition on the temporal border
+      if(glb_coord_of_loclx[ivol][0]==glb_size[0]-1) d+=1;
+      if(d%2==1) su3_prod_double(lx_conf[ivol][0],lx_conf[ivol][0],-1);
+    }
+    
+  THREAD_BARRIER();
+}
+THREADABLE_FUNCTION_END
+
 int main(int narg,char **arg)
 {
   //basic mpi initialization
   init_nissa(narg,arg);
   
   if(narg<5) crash("use: %s L T file_in file_out",arg[0]);
-
+  
   L=atoi(arg[1]);
   T=atoi(arg[2]);
-
-  //Init the MPI grid 
+  
+  //Init the MPI grid
   init_grid(T,L);
-
+  
   //////////////////////////// read the conf /////////////////////////////
-
+  
   quad_su3 *in_conf=nissa_malloc("in_conf",loc_vol,quad_su3);
   
   //print the plaquette and write the conf
   read_ildg_gauge_conf(in_conf,arg[3]);
   master_printf("Global plaquette: %16.16lg\n",global_plaquette_lx_conf(in_conf));
-
+  
   //////////////////////////// convert the conf //////////////////////////
   
   su3 *out_conf=nissa_malloc("out_conf",4*loc_vol,su3);
