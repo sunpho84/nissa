d5e92743da674d64e678cf78ee951a01da148968
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2015-12-11 12:14:08 +0100

    adding set_expansion

---
+++ src/hmc/multipseudo/set_expansions.hpp
@@ -0,0 +1,11 @@
+#ifndef _SET_EXPANSIONS_HPP
+#define _SET_EXPANSIONS_HPP
+
+#include "new_types/new_types_definitions.hpp"
+
+namespace nissa
+{
+  void set_expansions(hmc_evol_pars_t *evol_pars,quad_su3 **eo_conf,theory_pars_t *theory_pars);
+}
+
+#endif
