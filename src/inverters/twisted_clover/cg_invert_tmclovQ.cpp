d5745d629ab249e38c3fbf355024c7e48696b67f
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2016-06-20 13:28:07 +0100

    fixed wrong r remotion

--- src/inverters/twisted_clover/cg_invert_tmclovQ.cpp
+++ src/inverters/twisted_clover/cg_invert_tmclovQ.cpp
@@ -15,8 +15,9 @@ namespace nissa
     inv_tmclovQ2_cg(sol,NULL,conf,kappa,Cl,mu,niter,residue,source);
     spincolor *temp=nissa_malloc("temp",loc_vol+bord_vol,spincolor);
     
+    //remvoe the "wrong r"
     vector_copy(temp,sol);
-    apply_tmclovQ(sol,conf,kappa,Cl,mu,temp);
+    apply_tmclovQ(sol,conf,kappa,Cl,-mu,temp);
     
     nissa_free(temp);
   }
