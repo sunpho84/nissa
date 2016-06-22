498b862f87dde682b8a85ea98fe08cfd2124b457
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2016-06-22 10:48:01 +0100

    fixed g5

--- src/dirac_operators/tmclovD_eoprec/dirac_operator_tmclovD_eoprec_bgq.cpp
+++ src/dirac_operators/tmclovD_eoprec/dirac_operator_tmclovD_eoprec_bgq.cpp
@@ -46,17 +46,22 @@ namespace nissa
     
     if(in==out) crash("in==out!");
     
+    //mass and kapap
     double a=1/(2*kappa),b=mass;
-    double coef[2]={-0.25,+0.25};
-    vir_complex z[2]={{a,b, a,b},{-a,b, -a,b}};
+    vir_complex z[2]={{a,b, a,b},{a,-b, a,-b}};
+    
+    //coefficient for out
+    double coef=-0.25;
+    DECLARE_REG_VIR_COMPLEX(VIR_REG_COEF_OUT);
+    REG_SPLAT_VIR_COMPLEX(VIR_REG_COEF_OUT,coef);
+    
+    //gamma5
+    double g5[2]={1,-1};
     
     GET_THREAD_ID();
     NISSA_PARALLEL_LOOP(ivol,0,loc_volh/2)
       for(int high_low=0;high_low<2;high_low++)
 	{
-	  DECLARE_REG_VIR_COMPLEX(VIR_REG_COEF_OUT);
-	  REG_SPLAT_VIR_COMPLEX(VIR_REG_COEF_OUT,coef[high_low]);
-	  
 	  DECLARE_REG_VIR_COMPLEX(VIR_REG_COEF_IN);
 	  REG_LOAD_VIR_COMPLEX(VIR_REG_COEF_IN,z[high_low]);
 	  
@@ -66,18 +71,18 @@ namespace nissa
 	  REG_LOAD_VIR_HALFSPINCOLOR(VIR_REG_IN,in[ivol][NDIRAC/2*high_low]);
 	  
 	  //multiply OUT{[0],[1]} by-+1/4
-	  REG_LOAD_VIR_HALFSPINCOLOR(VIR_REG_OUT,out[ivol][0]);
+	  REG_LOAD_VIR_HALFSPINCOLOR(VIR_REG_OUT,out[ivol][2*high_low]);
 	  REG_VIR_HALFSPINCOLOR_PROD_4DOUBLE(VIR_REG_OUT,VIR_REG_OUT,VIR_REG_COEF_OUT);
 	  
 	  //summ Z*in
-	  REG_LOAD_VIR_HALFSPINCOLOR(VIR_REG_IN,in[ivol][0]);
+	  REG_LOAD_VIR_HALFSPINCOLOR(VIR_REG_IN,in[ivol][2*high_low]);
 	  REG_VIR_HALFSPINCOLOR_SUMM_THE_PROD_COMPLEX(VIR_REG_OUT,VIR_REG_IN,VIR_REG_COEF_IN);
 	  
 	  REORDER_BARRIER();
 	  
 	  //terms related to A
 	  DECLARE_REG_VIR_SU3(VIR_REG_Cl_0);
-	  REG_LOAD_VIR_SU3(VIR_REG_Cl_0,Cl[0]);
+	  REG_LOAD_VIR_SU3(VIR_REG_Cl_0,Cl[ivol][0+2*high_low]);
 	  REG_VIR_SU3_SUMM_THE_PROD_VIR_COLOR(VIR_REG_OUT_s0,VIR_REG_Cl_0,VIR_REG_IN_s0);
 	  REG_VIR_SU3_SUBT_THE_PROD_VIR_COLOR(VIR_REG_OUT_s1,VIR_REG_Cl_0,VIR_REG_IN_s1);
 	  
@@ -85,9 +90,16 @@ namespace nissa
 	  
 	  //terms related to B
 	  DECLARE_REG_VIR_SU3(VIR_REG_Cl_1);
-	  REG_LOAD_VIR_SU3(VIR_REG_Cl_1,Cl[1]);
+	  REG_LOAD_VIR_SU3(VIR_REG_Cl_1,Cl[ivol][1+2*high_low]);
 	  REG_VIR_SU3_DAG_SUMM_THE_PROD_VIR_COLOR(VIR_REG_OUT_s0,VIR_REG_Cl_1,VIR_REG_IN_s1);
 	  REG_VIR_SU3_SUMM_THE_PROD_VIR_COLOR(VIR_REG_OUT_s1,VIR_REG_Cl_1,VIR_REG_IN_s0);
+	  
+	  REORDER_BARRIER();
+	  
+	  //gamma5
+	  DECLARE_REG_VIR_COMPLEX(VIR_REG_COEF_G5);
+	  REG_SPLAT_VIR_COMPLEX(VIR_REG_COEF_G5,g5[high_low]);
+	  REG_VIR_HALFSPINCOLOR_PROD_4DOUBLE(VIR_REG_OUT,VIR_REG_OUT,VIR_REG_COEF_G5);
 	  
 	  STORE_REG_VIR_HALFSPINCOLOR(out[ivol][2*high_low],VIR_REG_OUT);
       }
