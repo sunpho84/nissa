edecc35a2699eec3702a3a589ce4ff6c57dfdc76
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2016-07-19 16:36:12 +0100

    improving readibility

--- src/hmc/momenta/momenta_action.cpp
+++ src/hmc/momenta/momenta_action.cpp
@@ -62,7 +62,7 @@ namespace nissa
       {
         //apply the kernel
         apply_MFACC(V,conf,kappa,pi[id]);
-        double_vector_glb_scalar_prod(&(glb_action_id[id]),(double*)V,(double*)V,sizeof(su3)/sizeof(double)*loc_vol);
+	glb_action_id[id]=double_vector_glb_norm2(V,loc_vol);
       }
     
     (*tot_action)=0;
--- src/hmc/momenta/momenta_action.hpp
+++ src/hmc/momenta/momenta_action.hpp
@@ -1,6 +1,8 @@
 #ifndef _MOMENTA_ACTION_HPP
 #define _MOMENTA_ACTION_HPP
 
+#include "new_types/su3.hpp"
+
 namespace nissa
 {
   double momenta_action(quad_su3 **H);
