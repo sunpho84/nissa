710d7f5e16b1f60215927601307ccbe07fc34287
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2016-10-05 02:18:26 +0100

    removed namespace

--- src/operations/stag/qed_corr.hpp
+++ src/operations/stag/qed_corr.hpp
@@ -7,7 +7,7 @@ namespace nissa
 {
   struct qed_corr_meas_pars_t : base_fermionic_meas_t
   {
-    std::string def_path(){return "qed";}
+    std::string def_path(){return "qed_corr";}
     
     int master_fprintf(FILE *fout,bool full) {return nissa::master_fprintf(fout,"%s",get_str().c_str());}
     std::string get_str(bool full=false);
@@ -24,11 +24,8 @@ namespace nissa
     {path=def_path();}
     virtual ~qed_corr_meas_pars_t(){}
   };
-
-  namespace stag
-  {
-    void measure_qed_corr(quad_su3 **conf,theory_pars_t theory_pars,qed_corr_meas_pars_t meas_pars,int iconf,int conf_created);
-  }
+  
+  void measure_qed_corr(quad_su3 **conf,theory_pars_t theory_pars,qed_corr_meas_pars_t meas_pars,int iconf,int conf_created);
 }
 
 #endif
