8295f40976900f086aaf6a1281930f21e30d6d64
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2016-10-06 17:48:38 +0100

    installing the new

--- config/install_valgrind.sh
+++ config/install_valgrind.sh
@@ -2,10 +2,9 @@
 
 mkdir comp
 cd comp
-wget http://valgrind.org/downloads/valgrind-3.10.1.tar.bz2 -O -|tar jxf -
-wget http://www.linuxfromscratch.org/patches/blfs/svn/valgrind-3.10.1-glibc_2.21-1.patch
+wget http://valgrind.org/downloads/valgrind-3.11.0.tar.bz2 -O -|tar jxf -
 cd val*
-patch -Np1 -i ../valgrind-3.10.1-glibc_2.21-1.patch
+
 ./configure --prefix=$HOME
 make -j 4
 make install
@@ -15,4 +14,4 @@ cd ..
 rm -fr comp
 
 echo "add \"export VALGRIND_LIB=\"$HOME/lib/valgrind\"\" to your .bashrc"
-echo "Use: LD_PRELOAD=$HOME/lib/valgrind/libmpiwrap-*.so mpirun -np np ~/bin/valgrind --track-origins=yes program"
+echo "Use: LD_PRELOAD=$(ls $HOME/lib/valgrind/libmpiwrap-*.so) mpirun -np np $HOME/bin/valgrind --track-origins=yes program"
