23fadf433905ab70cdb4c415c976635d072140b4
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2016-10-09 23:37:29 +0100

    spaces

--- tools/conf_transform/edit/grab_ildg_data.cpp
+++ tools/conf_transform/edit/grab_ildg_data.cpp
@@ -8,26 +8,25 @@ int main(int narg,char **arg)
   init_nissa(narg,arg);
   
   if(narg<5) crash("use: %s L T file_in file_out",arg[0]);
-
+  
   int L=atoi(arg[1]);
   int T=atoi(arg[2]);
-
-  //Init the MPI grid 
+  
+  //Init the MPI grid
   init_grid(T,L);
-
   //////////////////////////// read the conf /////////////////////////////
-
-  quad_su3 *conf=nissa_malloc("conf",loc_vol+bord_vol,quad_su3);  
+  
+  quad_su3 *conf=nissa_malloc("conf",loc_vol+bord_vol,quad_su3);
   read_ildg_gauge_conf(conf,arg[3]);
-
+  
   //////////////////////////// write the conf ////////////////////////////
   
   write_ildg_gauge_conf(arg[4],conf,64);
   nissa_free(conf);
   
   ///////////////////////////////////////////
-
+  
   close_nissa();
-
+  
   return 0;
 }
