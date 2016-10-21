8994329b48b8633aeae3cc100541287578ca2f60
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2016-10-21 19:32:36 +0100

    updating ref to patch

--- config/install_valgrind.sh
+++ config/install_valgrind.sh
@@ -3,8 +3,9 @@
 mkdir comp
 cd comp
 wget http://valgrind.org/downloads/valgrind-3.11.0.tar.bz2 -O -|tar jxf -
+wget http://www.linuxfromscratch.org/patches/blfs/svn/valgrind-3.11.0-upstream_fixes-1.patch
 cd val*
-
+patch -Np1 -i ../valgrind-3.11.0-upstream_fixes-1.patch
 ./configure --prefix=$HOME
 make -j 4
 make install
