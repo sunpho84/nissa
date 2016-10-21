7a87b203ace73f3c08fc3543057550f72239fc56
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2016-10-21 19:29:27 +0100

    removing protoype

--- src/operations/shift.hpp
+++ src/operations/shift.hpp
@@ -2,12 +2,12 @@
 #define _SHIFT_HPP
 
 #include <stdlib.h>
+#include "new_types/su3.hpp"
 
 namespace nissa
 {
-  double average_real_part_of_trace_of_rectangle_path(quad_su3 *conf,int mu,int nu,int nstep_mu,int nstep_nu,su3 *u);
-  void average_trace_of_rectangle_path(complex tra,quad_su3 *conf,int mu,int nu,int nstep_mu,int nstep_nu,su3 *u);
   void su3_vec_single_shift(su3 *u,int mu,int sign);
+  void quad_su3_vec_single_shift(su3 *u,int mu,int sign);
   inline void su3_vec_single_shift(su3 *u,int signed_mu)
   {su3_vec_single_shift(u,((signed_mu<0)?-1:+1),labs(signed_mu));}
 }
