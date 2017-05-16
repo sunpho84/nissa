657dc8924cf8c4b969fab88b61388f4630d7530b
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2017-05-17 01:04:26 +0200

    removed wrong useless wrapper

--- src/operations/shift.hpp
+++ src/operations/shift.hpp
@@ -8,8 +8,6 @@ namespace nissa
 {
   void su3_vec_single_shift(su3 *u,int mu,int sign);
   void quad_su3_vec_single_shift(su3 *u,int mu,int sign);
-  inline void su3_vec_single_shift(su3 *u,int signed_mu)
-  {su3_vec_single_shift(u,((signed_mu<0)?-1:+1),labs(signed_mu));}
 }
 
 #endif
