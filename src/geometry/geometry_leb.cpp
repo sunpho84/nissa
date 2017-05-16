98ca3ab5bbfc75e33222e2ad31959f79cbd62d86
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2017-05-16 10:55:30 +0200

    adding lebesgue

---
+++ src/geometry/geometry_leb.cpp
@@ -0,0 +1,16 @@
+#ifdef HAVE_CONFIG_H
+ #include "config.hpp"
+#endif
+
+namespace nissa
+{
+  //init the Lebesgue geometry
+  void set_Leb_geometry()
+  {
+  }
+  
+  //unset the Lebesgue geometry
+  void unset_Leb_geometry()
+  {
+  }
+}
---
+++ src/geometry/geometry_leb.hpp
@@ -0,0 +1,10 @@
+#ifndef _GEOMETRY_LEB_HPP
+#define _GEOMETRY_LEB_HPP
+
+namespace nissa
+{
+  void set_Leb_geometry();
+  void unset_Leb_geometry();
+}
+
+#endif
