d6bf7d596e8ddd37d8b9241f08954ebc4d26334a
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2017-08-14 23:31:39 +0200

    overriding check if magic number ignored

--- src/io/reader.cpp
+++ src/io/reader.cpp
@@ -37,9 +37,6 @@ namespace nissa
       crash("Opsss! The file contain %d bytes per site and it is supposed to contain not more than %d!",
 	    nbytes_per_site_read,nreals_per_site*sizeof(double));
     
-    //read
-    ILDG_File_read_ildg_data_all(out,file,header);
-    
     //check read size
     uint64_t nbytes_per_site_float=nreals_per_site*sizeof(float);
     uint64_t nbytes_per_site_double=nreals_per_site*sizeof(double);
@@ -54,9 +51,20 @@ namespace nissa
     if(nbytes_per_site_read==nbytes_per_site_float) single_double_flag=0;
     if(nbytes_per_site_read==nbytes_per_site_double) single_double_flag=1;
     if(single_double_flag==-1)
-      crash("Opsss! The file contain %d bytes per site and it is supposed to contain: %d (single) or %d (double)",
-	    nbytes_per_site_read,nbytes_per_site_float,nbytes_per_site_double);
+      {
+	if(ignore_ILDG_magic_number)
+	  {
+	    header.data_length=nbytes_per_site_double*glb_vol;
+	    master_printf("Ignoring message header and overriding header size to %d (double)\n",header.data_length);
+	    single_double_flag=1;
+	  }
+	else crash("Opsss! The file contain %d bytes per site and it is supposed to contain: %d (single) or %d (double)",
+		   nbytes_per_site_read,nbytes_per_site_float,nbytes_per_site_double);
+      }
     verbosity_lv3_master_printf("Vector is stored in %s precision\n",single_double_str[single_double_flag]);
+    
+    //read
+    ILDG_File_read_ildg_data_all(out,file,header);
     
     //change endianess
     if(little_endian)
@@ -66,7 +74,7 @@ namespace nissa
       }
     
     //check the checksum
-    if(read_check[0]!=0||read_check[1]!=0)
+    if(read_check[0]!=0 or read_check[1]!=0)
       {
 	master_printf("Checksums read:      %#010x %#010x\n",read_check[0],read_check[1]);
 	
@@ -76,7 +84,7 @@ namespace nissa
 	
 	//print the comparison between checksums
 	master_printf("Checksums computed:  %#010x %#010x\n",comp_check[0],comp_check[1]);
-	if((read_check[0]!=comp_check[0])||(read_check[1]!=comp_check[1]))
+	if((read_check[0]!=comp_check[0]) or (read_check[1]!=comp_check[1]))
 	  master_printf("Warning, checksums do not agree!\n");
       }
     else master_printf("Data checksum not found.\n");
