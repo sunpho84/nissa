410f5068931b1f5d8449c1e0f2b5cb4011d28447
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2017-08-25 17:48:46 -0600

    adding Laplace operator with 2 links

---
+++ src/operations/stag/Laplace_op_2links.hpp
@@ -0,0 +1,11 @@
+#ifndef _LAPLACE_OP_2LINKS_HPP
+#define _LAPLACE_OP_2LINKS_HPP
+
+#include <new_types/su3.hpp>
+
+namespace nissa
+{
+  void laplace_operator_2_links(color *out,quad_su3 *conf,color *in);
+}
+
+#endif
