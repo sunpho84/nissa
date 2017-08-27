2485534c0ef55f4bd67974819712325b2fe9fe49
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2017-08-26 23:13:34 -0600

    according to renaming

--- src/nissa.hpp
+++ src/nissa.hpp
@@ -144,7 +144,7 @@
 #include "operations/smearing/APE.hpp"
 #include "operations/smearing/HYP.hpp"
 #include "operations/smearing/gaussian.hpp"
-#include "operations/smearing/recursive_smoother.hpp"
+#include "operations/smearing/recursive_Wflower.hpp"
 #include "operations/smearing/stout.hpp"
 #include "operations/smearing/smooth.hpp"
 #include "operations/smearing/Wflow.hpp"
