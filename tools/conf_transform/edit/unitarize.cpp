dbf53ccfc89c13b5e89002e3a775d1961d6c1eab
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2017-09-20 22:21:37 +0200

    adding unitarize

---
+++ tools/conf_transform/edit/unitarize.cpp
@@ -0,0 +1,36 @@
+#include "nissa.hpp"
+
+using namespace nissa;
+
+int main(int narg,char **arg)
+{
+  //basic mpi initialization
+  init_nissa(narg,arg);
+  
+  if(narg<5) crash("use: %s L T file_in file_out",arg[0]);
+  
+  int L=atoi(arg[1]);
+  int T=atoi(arg[2]);
+  
+  //Init the MPI grid
+  init_grid(T,L);
+  //////////////////////////// read the conf /////////////////////////////
+  
+  quad_su3 *conf=nissa_malloc("conf",loc_vol+bord_vol,quad_su3);
+  read_ildg_gauge_conf(conf,arg[3]);
+  
+  NISSA_LOC_VOL_LOOP(ivol)
+    for(int mu=0;mu<NDIM;mu++)
+      su3_unitarize_explicitly_inverting(conf[ivol][mu],conf[ivol][mu]);
+  
+  //////////////////////////// write the conf ////////////////////////////
+  
+  write_ildg_gauge_conf(arg[4],conf,64);
+  nissa_free(conf);
+  
+  ///////////////////////////////////////////
+  
+  close_nissa();
+  
+  return 0;
+}
