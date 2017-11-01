985804f7a076fde834ecbed3c536ea697373d1be
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2017-11-01 23:32:16 +0100

    writing err

--- src/operations/gauge_fixing.cpp
+++ src/operations/gauge_fixing.cpp
@@ -416,8 +416,13 @@ namespace nissa
 	double b=(4*F[1]-F[2]-3*F[0])/(2*alpha);
 	double a=(F[2]-2*F[1]+F[0])/(2*sqr(alpha));
 	
-	VERBOSITY_MASTER_PRINTF("F:   %lg %lg %lg\n",F[0],F[1],F[2]);
 	VERBOSITY_MASTER_PRINTF("abc: %lg %lg %lg\n",a,b,c);
+	
+	VERBOSITY_MASTER_PRINTF("F:   %.16lg %.16lg %.16lg\n",F[0],F[1],F[2]);
+	VERBOSITY_MASTER_PRINTF("abc: %lg %lg %lg\n",a,b,c);
+	
+	double err=fabs((F[2]+F[0])/(2*F[1]))-1.0;
+	VERBOSITY_MASTER_PRINTF("abc: %.16lg\n",err);
 	
 	double vert=-b/(2*a);
 	pos_vert=(vert>0);
