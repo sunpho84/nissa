cb06a7bd73b95691796b47b38e00eac2aaab2e23
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2018-08-24 00:50:53 +0200

    Moved functions to cpp

--- src/inverters/templates/eigenvalues.hpp
+++ src/inverters/templates/eigenvalues.hpp
@@ -10,40 +10,9 @@ namespace nissa
 {
   namespace internal_eigenvalues
   {
-    //scalar product of (in1,in2)
-    void scalar_prod(complex out,complex *in1,complex *in2,complex *buffer,int loc_size)
-    {
-      GET_THREAD_ID();
-      
-      //local scalar product
-      NISSA_PARALLEL_LOOP(i,0,loc_size)
-	unsafe_complex_conj1_prod(buffer[i],in1[i],in2[i]);
-      THREAD_BARRIER();
-      
-      //reduction
-      complex_vector_glb_collapse(out,buffer,loc_size);
-    }
-    
-    //Ai=Ai-Bi*c
-    void complex_vector_subtssign_complex_vector_prod_complex(complex *a,complex *b,complex c,int n)
-    {
-      GET_THREAD_ID();
-      
-      NISSA_PARALLEL_LOOP(i,0,n)
-	complex_subt_the_prod(a[i],b[i],c);
-      set_borders_invalid(a);
-    }
-    
-    //orthogonalize v with respect to the nvec of V
-    void modified_GS(complex *v,complex **V,complex *buffer,int nvec,int vec_size)
-    {
-      for(int i=0;i<nvec;i++)
-	{
-	  complex s;
-	  scalar_prod(s,V[i],v,buffer,vec_size);
-	  complex_vector_subtssign_complex_vector_prod_complex(v,V[i],s,vec_size);
-	}
-    }
+    void scalar_prod(complex out,complex *in1,complex *in2,complex *buffer,int loc_size);
+    void complex_vector_subtssign_complex_vector_prod_complex(complex *a,complex *b,complex c,int n);
+    void modified_GS(complex *v,complex **V,complex *buffer,int nvec,int vec_size);
   }
   
   //find the neig eigenvalues closest to the target
@@ -62,12 +31,12 @@ namespace nissa
     complex *buffer=nissa_malloc("buffer",mat_size, complex);
     
     //fill V and orthonormalize
+    double useless_rat;
     for(int i=0;i<wspace_max_size;i++)
       {
 	filler(V[i]);
 	modified_GS(V[i],V,buffer,i,mat_size);
-	double rat;
-	double_vector_normalize(&rat,(double*)(V[i]),(double*)(V[i]),1.0,2*mat_size);
+	double_vector_normalize(&useless_rat,(double*)(V[i]),(double*)(V[i]),1.0,2*mat_size);
       }
     
     //free workspace
