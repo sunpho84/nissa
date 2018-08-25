d58be0f7ca6c7114f6ad5c1b8896c9b10d09188e
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2018-08-25 22:33:15 +0200

    Adding sorting

--- src/inverters/templates/eigenvalues.cpp
+++ src/inverters/templates/eigenvalues.cpp
@@ -44,5 +44,53 @@ namespace nissa
 	  complex_vector_subtssign_complex_vector_prod_complex(v,V[i],s,vec_size);
 	}
     }
+    
+    //find eigenvalues of M and sort them according to |\lambda_i-\tau|
+    std::vector<double> eigenvalues_of_hermatr_find_all_and_sort(complex *M,int size,double tau)
+    {
+      std::vector<double> eigvals(size);
+      
+      //structure to diagonalize
+      using namespace Eigen;
+      SelfAdjointEigenSolver<MatrixXcd> solver;
+      
+      //fill the matrix to be diagonalized
+      MatrixXcd matr(size,size);
+      for(int i=0;i<size;i++)
+	for(int j=0;j<=i;j++)
+	  matr(i,j)=std::complex<double>(M[j+size*i][RE],M[j+size*i][IM]);
+      
+      //diagonalize
+      solver.compute(matr);
+      
+      //sort the eigenvalues and eigenvectors
+      std::vector<std::tuple<double,double,int>> ei;
+      for(int i=0;i<size;i++)
+	{
+	  double lambda=solver.eigenvalues()(i);
+	  ei.push_back(std::make_tuple(fabs(lambda-tau),lambda,i));
+	}
+      std::sort(ei.begin(),ei.end());
+      
+      //fill output
+      for(int i=0;i<size;i++)
+	{
+	  //fill eigvalue
+	  using std::get;
+	  eigvals[i]=get<1>(ei[i]);
+	  
+	  //get index of what must be put in i
+	  int ori=get<2>(ei[i]);
+	  
+	  //fill eigvect
+	  for(int j=0;j<size;j++)
+	    {
+	      M[j+size*i][RE]=solver.eigenvectors()(ori).real();
+	      M[j+size*i][IM]=solver.eigenvectors()(ori).imag();
+	    }
+	}
+      
+      return eigvals;
+    }
   }
 }
--- src/inverters/templates/eigenvalues.hpp
+++ src/inverters/templates/eigenvalues.hpp
@@ -1,6 +1,10 @@
 #ifndef _EIGENVALUES_HPP
 #define _EIGENVALUES_HPP
 
+// #ifdef HAVE_EIGEN_DENSE
+ #include "eigen3/Eigen/Dense"
+// #endif
+
 #include "base/thread_macros.hpp"
 #include "base/vectors.hpp"
 #include "linalgs/linalgs.hpp"
@@ -13,15 +17,16 @@ namespace nissa
     void scalar_prod(complex out,complex *in1,complex *in2,complex *buffer,int loc_size);
     void complex_vector_subtssign_complex_vector_prod_complex(complex *a,complex *b,complex c,int n);
     void modified_GS(complex *v,complex **V,complex *buffer,int nvec,int vec_size);
+    std::vector<double> eigenvalues_of_hermatr_find_all_and_sort(complex *M,int size,double tau);
   }
   
   //find the neig eigenvalues closest to the target
   template <class Fmat,class Filler>
-  void eigenvalues_herm_find(complex **eig_vec,double *eig_val,int neig,bool min_max,
-			     const int mat_size,const int mat_size_to_allocate,const Fmat &imp_mat,
-			     const double tol,const int niter_max,
-			     const int wspace_min_size,const int wspace_max_size,
-			     const Filler &filler)
+  void eigenvalues_of_hermatr_find(complex **eig_vec,double *eig_val,int neig,double tau,bool min_max,
+				   const int mat_size,const int mat_size_to_allocate,const Fmat &imp_mat,
+				   const double tol,const int niter_max,
+				   const int wspace_min_size,const int wspace_max_size,
+				   const Filler &filler)
   {
     using namespace internal_eigenvalues;
     
@@ -47,12 +52,11 @@ namespace nissa
       {
 	imp_mat(temp,V[i]);
 	for(int j=0;j<=i;j++)
-	  {
-	    scalar_prod(M[i*wspace_size+j],V[j],temp,buffer,mat_size);
-	    master_printf("(%lg,%lg)\t",M[i*wspace_size+j][RE],M[i*wspace_size+j][IM]);
-	  }
-	master_printf("\n");
+	  scalar_prod(M[j+wspace_size*i],V[j],temp,buffer,mat_size);
       }
+    
+    //find all eigenvalues, sort them by distance with tau
+    eigenvalues_of_hermatr_find_all_and_sort(M,wspace_size,tau);
     
     //free workspace
     for(int i=0;i<wspace_max_size;i++) nissa_free(V[i]);
