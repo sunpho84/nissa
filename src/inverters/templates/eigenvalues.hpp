bda858a942977e8fdbcb336b70ddfcda4b6c2d7e
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2018-08-28 16:42:49 +0200

    Externed modern_cg

--- src/inverters/templates/eigenvalues.hpp
+++ src/inverters/templates/eigenvalues.hpp
@@ -6,6 +6,8 @@
 #include "linalgs/linalgs.hpp"
 #include "new_types/complex.hpp"
 
+#include "modern_cg.hpp"
+
 namespace nissa
 {
   namespace internal_eigenvalues
@@ -22,62 +24,6 @@ namespace nissa
     double iterated_classical_GS(complex *v,int vec_size,int nvec,complex **A,complex* buffer,const int max_cgs_it);
     void eigenvalues_of_hermatr_find_all_and_sort(complex *eig_vec,double *lambda,const complex *M,const int M_size,const int neig,const double tau);
     void combine_basis_to_restart(int nout,int nin,complex *coeffs,complex **vect,int vec_length);
-    
-    //solve using conjugate gradient algorithm
-    template <class Fmat> void cg_solve(complex *x,const Fmat& mat_impl,complex *b,int mat_size,int mat_size_to_allocate,double target_residue,int nmax_iters)
-    {
-      master_printf("Starting cg, target_residue: %lg, niter_max: %d\n",target_residue,nmax_iters);
-      
-      complex *p=nissa_malloc("p",mat_size_to_allocate,complex);
-      complex *r=nissa_malloc("r",mat_size_to_allocate,complex);
-      complex *t=nissa_malloc("t",mat_size_to_allocate,complex);
-      complex *ap=nissa_malloc("ap",mat_size_to_allocate,complex);
-      vector_copy(r,b);
-      vector_copy(p,b);
-      vector_reset(x);
-      
-      //compute norm of r and store it for computing relative residual
-      double rr=double_vector_glb_norm2(r,mat_size),rel_res;
-      double source_norm=sqrt(double_vector_glb_norm2(b,mat_size));
-      if(source_norm==0) crash("invalid norm");
-      
-      //count iterations
-      int iter=1;
-      do
-	{
-	  //compute (p,Ap)
-	  mat_impl(ap,p);
-	  double pap;
-	  double_vector_glb_scalar_prod(&pap,(double*)ap,(double*)p,mat_size*2);
-	  
-	  //compute alpha, store rr as old one
-	  double alpha=rr/pap;
-	  double roro=rr;
-	  
-	  //adjust new solution and residual,
-	  //compute new residual norm
-	  double_vector_summassign_double_vector_prod_double((double*)x,(double*)p,alpha,2*mat_size);
-	  double_vector_summassign_double_vector_prod_double((double*)r,(double*)ap,-alpha,2*mat_size);
-	  rr=double_vector_glb_norm2(r,mat_size);
-	  
-	  //adjust new krylov vector
-	  double beta=rr/roro;
-	  double_vector_summ_double_vector_prod_double((double*)p,(double*)r,(double*)p,beta,2*mat_size);
-	  
-	  //compute relative residue
-	  rel_res=sqrt((double)rr)/source_norm;
-	  
-	  master_printf("it: %d, res: %lg\n",iter,rel_res);
-	  
-	  iter++;
-	}
-      while(rel_res>target_residue and iter<nmax_iters);
-      
-      nissa_free(ap);
-      nissa_free(r);
-      nissa_free(t);
-      nissa_free(p);
-    }
   }
   
   //find the neig eigenvalues closest to the target
@@ -99,11 +45,14 @@ namespace nissa
     complex *temp=nissa_malloc("temp",mat_size,complex);
     
     //fill V and orthonormalize
-    double useless_rat;
     for(int i=0;i<wspace_max_size;i++)
       {
+	//fill
 	filler(V[i]);
+	//orthogonalize
 	modified_GS(V[i],V,buffer,i,mat_size);
+	//normalize
+	double useless_rat;
 	double_vector_normalize(&useless_rat,(double*)(V[i]),(double*)(V[i]),1.0,2*mat_size);
       }
     
