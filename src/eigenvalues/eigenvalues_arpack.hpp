2f9038482662340508436feac82abb5bf3dbe9dd
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2018-09-02 20:29:56 +0200

    Reorganized arpack and using nissa filler

--- src/eigenvalues/eigenvalues_arpack.hpp
+++ src/eigenvalues/eigenvalues_arpack.hpp
@@ -13,10 +13,36 @@
 #include "new_types/complex.hpp"
 #include "routines/ios.hpp"
 
+#ifndef EXTERN_ARPACK
+ #define EXTERN_ARPACK extern
+ #define INIT_ARPACK_TO(...)
+#else
+ #define INIT_ARPACK_TO(...) __VA_ARGS__
+#endif
+
 namespace nissa
 {
-  //type for cast
-  typedef _Complex double c_t;
+  namespace arpack_data
+  {
+    //normal eigenvalue problem
+    EXTERN_ARPACK char bmat[] INIT_ARPACK_TO(="I");
+    //largest or smallest
+    EXTERN_ARPACK char which[2][3] INIT_ARPACK_TO(={"SM","LM"});
+    //parameters
+    EXTERN_ARPACK int iparam[11];
+    //pointer to work
+    EXTERN_ARPACK int ipntr[14];
+    
+    //type for cast
+    typedef _Complex double c_t;
+    
+    void check_exit_status_arpack(const int info);
+    void init_params(int *iparam,int niter_max);
+    void eigenvalues_of_hermatr_find_arpack_compute(MPI_Fint &comm,complex **eig_vec,double *eig_val,int neig,bool min_max,
+						    const int mat_size,const double target_precision,const int wspace_size,
+						    complex *residue,complex *workd,complex *workl,const int lworkl,complex *rwork,int &info,
+						    complex *v,const int ldv);
+  }
   
   //use arpack
   template <class Fmat,class Filler>
@@ -25,43 +51,36 @@ namespace nissa
 					  const double target_precision,const int niter_max,
 					  const Filler &filler)
   {
+    using namespace arpack_data;
+    
     //convert the communicator
     MPI_Fint comm=MPI_Comm_c2f(MPI_COMM_WORLD);
     
-    //normal eigenvalue problem
-    const char bmat[]="I";
-    //largest or smallest
-    const char which[2][3]={"SM","LM"};
-    //vector of residue
+    //vector of residue: we fill it
     complex *residue=nissa_malloc("residue",mat_size_to_allocate,complex);
+    filler(residue);
+    int info=1; //do not use arpack random generator
+    
     //base for eigenvectors
     const int wspace_size=2*neig;
     //workspace
     complex *v=nissa_malloc("v",wspace_size*mat_size_to_allocate,complex);
     //size of each line
     const int ldv=mat_size_to_allocate;
-    //parameters
-    int iparam[11];
-    //pointer to work
-    int ipntr[14];
     //internal workspace
     complex *workd=nissa_malloc("workd",3*mat_size,complex);
-    int lworkl=(3*wspace_size+5)*wspace_size;
+    const int lworkl=(3*wspace_size+5)*wspace_size;
     complex *workl=nissa_malloc("workl",lworkl,complex);
     complex *rwork=nissa_malloc("rwork",wspace_size,complex);
     
-    //init params
-    iparam[0]=1;
-    iparam[2]=niter_max;
-    iparam[3]=1;
-    iparam[6]=1;
+    //initialize the parameters
+    init_params(iparam,niter_max);
     
     //temporary vectors
     complex *temp_x=nissa_malloc("temp_x",mat_size_to_allocate,complex);
     complex *temp_y=nissa_malloc("temp_y",mat_size_to_allocate,complex);
     
     //main loop to find Ritz basis
-    int info=0;
     int ido=0;
     int iter=0;
     do
@@ -79,64 +98,31 @@ namespace nissa
 	imp_mat(temp_y,temp_x);
 	memcpy(y,temp_y,sizeof(complex)*mat_size);
 	
+	if(ido!=99 and ido!=1) crash("ido %d not contemplated",ido);
+	
 	iter++;
       }
-    while(ido==1);
+    while(ido!=99);
     
-    //decrypt info
-    switch(info)
-      {
-      case 0: master_printf("Normal exit.\n");break;
-      case 1: crash("Maximum number of iterations taken.");break;
-      case 3: crash("No shifts could be applied during a cycle of the "
-			    "Implicitly restarted Arnoldi iteration. One possibility "
-			    "is to increase the size of NCV relative to NEV.");break;
-      case-1: crash("N must be positive.");break;
-      case-2: crash("NEV must be positive.");break;
-      case-3: crash("NCV-NEV >= 2 and less than or equal to N.");break;
-      case-4: crash("The maximum number of Arnoldi update iteration must be greater than zero.");break;
-      case-5: crash("WHICH must be one of 'LM', 'SM', 'LR', 'SR', 'LI', 'SI'");break;
-      case-6: crash("BMAT must be one of 'I' or 'G'.");break;
-      case-7: crash("Length of private work array is not sufficient.");break;
-      case-8: crash("Error return from LAPACK eigenvalue calculation;");break;
-      case-9: crash("Starting vector is zero.");break;
-      case-10: crash("IPARAM(7) must be 1,2,3.");break;
-      case-11: crash("IPARAM(7) = 1 and BMAT = 'G' are incompatable.");break;
-      case-12: crash("IPARAM(1) must be equal to 0 or 1.");break;
-      case-9999: crash("Could not build an Arnoldi factorization.");break;
-      default:crash("Unknown error code");break;
-      }
+    //check that the the routine exited correctly
+    check_exit_status_arpack(info);
     
     //free temporary vectors
     nissa_free(temp_x);
     nissa_free(temp_y);
     
-    //parameters for eigenvector calculation
-    const int rvec=1;
-    const char howmny[]="P";
-    complex ceig_val[neig];
-    const c_t sigma=0.0;
-    int select[wspace_size];
-    complex *workev=nissa_malloc("workev",2*wspace_size,complex);
-    complex *temp_vec=nissa_malloc("temp_vec",neig*mat_size,complex);
-    arpack::internal::pzneupd_c(comm,rvec,howmny,select,(c_t*)ceig_val,(c_t*)temp_vec,mat_size,sigma,(c_t*)workev,bmat,mat_size,which[min_max],neig,target_precision,(c_t*)residue,wspace_size,(c_t*)v,ldv,iparam,ipntr,(c_t*)workd,(c_t*)workl,lworkl,(c_t*)rwork,&info);
+    //finalize the eigenstuff calculation
+    eigenvalues_of_hermatr_find_arpack_compute(comm,eig_vec,eig_val,neig,min_max,mat_size,target_precision,wspace_size,residue,workd,workl,lworkl,rwork,info,v,ldv);
     
-    //store result
-    for(int i=0;i<neig;i++)
-      {
-	memcpy(eig_vec[i],temp_vec+mat_size*i,sizeof(complex)*mat_size);
-	eig_val[i]=ceig_val[i][RE];
-      }
-    
-    
-    nissa_free(temp_vec);
-    nissa_free(workev);
     nissa_free(rwork);
     nissa_free(workl);
     nissa_free(workd);
     nissa_free(v);
     nissa_free(residue);
   }
-} 
+}
+
+#undef EXTERN_ARPACK
+#undef INIT_ARPACK_TO
 
 #endif
