2287d8808a3f33a59e862403b437f138d8703250
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2018-10-20 22:09:45 +0200

    Reporting number of read in case of failure

--- src/base/random.hpp
+++ src/base/random.hpp
@@ -87,7 +87,7 @@ namespace nissa
 	if(fd==-1) crash("Opening %s",path);
 	
 	int rc=read(fd,&t,size);
-        if(rc!=size) crash("reading %zu bytes from %s",size,path);
+        if(rc!=size) crash("reading %zu bytes from %s, obtained: %d",size,path,rc);
 	if(close(fd)==-1) crash("Closing %s",path);
     }
     MPI_Bcast(&t,size,MPI_CHAR,0,MPI_COMM_WORLD);
