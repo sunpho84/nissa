b3e58e79e59fed41249d7812eb15c0dc313413b4
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2018-11-22 22:58:15 +0100

    Fixed parpack checkout of branch for travis

--- config/install_parpack.sh
+++ config/install_parpack.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 git clone https://github.com/opencollab/arpack-ng.git
-git checkout 94dd13dc2ce036ab40e070e1ee60af57cf0b90cb
 cd arpack-ng
+git checkout 94dd13dc2ce036ab40e070e1ee60af57cf0b90cb
 bash bootstrap
 mkdir build
 cd build
