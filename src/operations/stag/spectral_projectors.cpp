1917c048e668ae284023f651f77bf7e4fb955d8a
joek93 <giuseppe.clemente93@gmail.com>
2018-11-26 12:24:57 +0100

    some fix on measure of spectral projectors

--- src/operations/stag/spectral_projectors.cpp
+++ src/operations/stag/spectral_projectors.cpp
@@ -59,7 +59,7 @@ namespace nissa{
   // of the topological charge as Q = \sum_i \bar(u)_i gamma5 u_i,
   // where {u_i} are the first n eigenvectors.
 
-  THREADABLE_FUNCTION_6ARG(fill_eigenpart, complex**,eigvec, quad_su3**,conf,double*, charge_cut, double*, DD_reigs,int,neigs, double,eig_precision)
+  THREADABLE_FUNCTION_6ARG(fill_eigenpart, complex**,eigvec, quad_su3**,conf,double*, charge_cut, complex*, DD_eig_val,int,neigs, double,eig_precision)
   {
 //    GET_THREAD_ID();
     master_fprintf(stdout,"neigs = %d, eig_precision = %.2e\n", neigs, eig_precision);
@@ -69,13 +69,19 @@ namespace nissa{
     color *out_tmp_eo[2]={nissa_malloc("out_tmp_EVN",loc_volh+bord_volh,color),nissa_malloc("out_tmp_ODD",loc_volh+bord_volh,color)};
     color *in_tmp_eo[2]={nissa_malloc("in_tmp_EVN",loc_volh+bord_volh,color),nissa_malloc("in_tmp_ODD",loc_volh+bord_volh,color)};
     color *fill_tmp_eo[2]={nissa_malloc("fill_tmp_EVN",loc_volh+bord_volh,color),nissa_malloc("fill_tmp_ODD",loc_volh+bord_volh,color)};
-		
+	  vector_reset(out_tmp_eo[EVN]);
+    vector_reset(out_tmp_eo[ODD]);
+	  vector_reset(in_tmp_eo[EVN]);
+	  vector_reset(in_tmp_eo[ODD]);
+	  vector_reset(fill_tmp_eo[EVN]);
+	  vector_reset(fill_tmp_eo[ODD]);
+
 		quad_u1 *u1b[2];
-    u1b[0]=nissa_malloc("u1b",loc_vol+bord_vol,quad_u1);
-    u1b[1]=nissa_malloc("u1b",loc_vol+bord_vol,quad_u1);
+    u1b[0]=nissa_malloc("u1b",loc_volh+bord_volh,quad_u1);
+    u1b[1]=nissa_malloc("u1b",loc_volh+bord_volh,quad_u1);
     init_backfield_to_id(u1b);
-    const auto imp_mat=[conf,in_tmp_eo,out_tmp_eo](complex *out_lx,complex *in_lx){
-      split_lx_vector_into_eo_parts((color**)in_tmp_eo,(color*)in_lx);
+    const auto imp_mat=[conf,&in_tmp_eo,out_tmp_eo](complex *out_lx,complex *in_lx){
+      split_lx_vector_into_eo_parts(in_tmp_eo,(color*)in_lx);
 
       evn_apply_stD(out_tmp_eo[EVN],conf,0.01,(color**)in_tmp_eo);
       odd_apply_stD(out_tmp_eo[ODD],conf,0.01,(color**)in_tmp_eo);
@@ -83,7 +89,7 @@ namespace nissa{
       evn_apply_stD_dag(in_tmp_eo[EVN],conf,0.01,(color**)out_tmp_eo);
       odd_apply_stD_dag(in_tmp_eo[ODD],conf,0.01,(color**)out_tmp_eo);
       
-      paste_eo_parts_into_lx_vector((color*)out_lx,(color**)in_tmp_eo);
+      paste_eo_parts_into_lx_vector((color*)out_lx,in_tmp_eo);
 				
         set_borders_invalid(out_lx);
       };
@@ -92,12 +98,12 @@ namespace nissa{
 
     //parameters of the eigensolver
     const bool min_max=0;
-    const int mat_size=loc_vol*sizeof(color)/sizeof(complex);
-    const int mat_size_to_allocate=(loc_vol+bord_vol)*sizeof(color)/sizeof(complex);
+    const int mat_size=loc_vol*NCOL;
+    const int mat_size_to_allocate=(loc_vol+bord_vol)*NCOL;
     const int niter_max=100000;
     master_printf("mat_size=%d, mat_size_to_allocate=%d\n",mat_size,mat_size_to_allocate); 
     //wrap the generation of the test vector into an object that can be passed to the eigenfinder
-    const auto filler=[fill_tmp_eo](complex *out_lx)
+    const auto filler=[&fill_tmp_eo](complex *out_lx)
       {
         generate_fully_undiluted_eo_source((color**)fill_tmp_eo,RND_GAUSS,-1,0);
         paste_eo_parts_into_lx_vector((color*)out_lx,(color**)fill_tmp_eo);
@@ -105,13 +111,15 @@ namespace nissa{
     
     //launch the eigenfinder
     double eig_time=-take_time();
-    complex DD_eig_val[neigs];
+
+ 
+
     eigenvalues_of_hermatr_find(eigvec,DD_eig_val,neigs,min_max,mat_size,mat_size_to_allocate,imp_mat,eig_precision,niter_max,filler);
 
 //    double norm_cut[neigs];
-    complex *point_vec = nissa_malloc("point_vec",loc_vol+bord_vol,complex);
-
-    master_printf("\n\nEigenvalues of Id:\n");
+    color *point_vec = nissa_malloc("point_vec",(loc_vol+bord_vol),color);
+    vector_reset(point_vec);
+    master_printf("\n\nEigenvalues of D^+D:\n");
     for(int ieig=0; ieig<neigs; ++ieig){
       master_printf("%d (%.16lg,%.16lg)\n",ieig,DD_eig_val[ieig][RE],DD_eig_val[ieig][IM]);
       
@@ -128,11 +136,10 @@ namespace nissa{
       apply_stag_op(out_tmp_eo,conf,u1b,stag::GAMMA_5,stag::IDENTITY,fill_tmp_eo);
 
       //take hermitian product
-      stag::summ_the_trace(&charge_cut[ieig],point_vec,out_tmp_eo,fill_tmp_eo);
+      stag::summ_the_trace(&charge_cut[ieig],(complex*)point_vec,out_tmp_eo,fill_tmp_eo);
 //      stag::summ_the_trace((double*)&norm_cut[ieig],point_vec,fill_tmp_eo,fill_tmp_eo);
 //      master_printf("charge_cut[%d] = %.10f, norm_cut[%d] = %.10f\n",ieig,charge_cut[ieig],ieig,norm_cut[ieig]);
 
-      DD_reigs[ieig]=DD_eig_val[ieig][RE];
     }
     master_printf("\n\n\n");
 
@@ -147,7 +154,7 @@ namespace nissa{
     nissa_free(fill_tmp_eo[ODD]);
     nissa_free(u1b[0]);
     nissa_free(u1b[1]);
-    nissa_free(point_vec);
+//    nissa_free(point_vec);
   }
   THREADABLE_FUNCTION_END
 
@@ -160,12 +167,18 @@ namespace nissa{
     int neigs = meas_pars.neigs;
     
     double *charge_cut=nissa_malloc("charge_cut",neigs,double);
-    double *DD_reigs =nissa_malloc("DD_reigs",neigs,double);
+    vector_reset(charge_cut);
+    complex *DD_eig_val =nissa_malloc("DD_eig_Val",neigs,complex);
+    vector_reset(DD_eig_val);
+//    double charge_cut[neigs];
+//    double DD_reigs[neigs];
+
     add_or_rem_stagphases_to_conf(conf); 
 
-    complex *eigvec[neigs];
+    complex **eigvec= nissa_malloc("eigvec",neigs,complex*);
     for(int ieig=0;ieig<neigs;ieig++){
-		  eigvec[ieig]=nissa_malloc("eigvec",loc_vol*3,complex);
+		  eigvec[ieig]=nissa_malloc("eigvec_ieig",(loc_vol+bord_vol)*NCOL,complex);
+      vector_reset(eigvec[ieig]);
     }
             
     //loop over hits
@@ -174,12 +187,12 @@ namespace nissa{
     {
       verbosity_lv2_master_printf("Evaluating spectral topological charge nhits %d/%d\n",hit+1,nhits);
   
-      fill_eigenpart(eigvec,conf,charge_cut,DD_reigs,meas_pars.neigs,meas_pars.eig_precision);
+      fill_eigenpart(eigvec,conf,charge_cut,DD_eig_val,meas_pars.neigs,meas_pars.eig_precision);
        
     }
     master_fprintf(file,"%d\t",neigs); 
     for(int ieig=0; ieig<neigs; ++ieig){
-      master_fprintf(file,"%.16lg\t%.16lg\t",DD_reigs[ieig]-0.0001,charge_cut[ieig]);
+      master_fprintf(file,"%.16lg\t%.16lg\t",DD_eig_val[ieig][RE]-0.0001,charge_cut[ieig]);
     }
     master_fprintf(file,"\n");
 
@@ -189,8 +202,9 @@ namespace nissa{
     for(int ieig=0; ieig<neigs; ieig++){
       nissa_free(eigvec[ieig]);
     }
+    nissa_free(eigvec);
     nissa_free(charge_cut);
-    nissa_free(DD_reigs);
+    nissa_free(DD_eig_val);
     close_file(file);
 
   }
