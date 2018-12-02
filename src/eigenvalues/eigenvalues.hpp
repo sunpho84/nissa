9cec47580ab79e2167801476a25bc7b48f003b58
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2018-12-02 02:09:50 +0100

    Removing if

--- src/eigenvalues/eigenvalues.hpp
+++ src/eigenvalues/eigenvalues.hpp
@@ -33,8 +33,7 @@ namespace nissa
     if(use_arpack)
       {
 	master_printf("Using arpack\n");
-	if(IS_MASTER_THREAD)
-	  eigenvalues_of_hermatr_find_arpack(eig_vec,eig_val,neig,min_max,mat_size,mat_size_to_allocate,imp_mat,target_precision,niter_max,filler);
+	eigenvalues_of_hermatr_find_arpack(eig_vec,eig_val,neig,min_max,mat_size,mat_size_to_allocate,imp_mat,target_precision,niter_max,filler);
       }
     else
       {
