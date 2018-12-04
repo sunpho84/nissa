2d3496d292d4ae810c0228f0ad7d16ef0a78d8ae
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2018-12-04 12:09:00 +0100

    Suppressing ignore_ILDG_magic_number spurious

--- src/measures/fermions/minmax_eigenvalues.cpp
+++ src/measures/fermions/minmax_eigenvalues.cpp
@@ -3,8 +3,8 @@
 #endif
 
 #include "minmax_eigenvalues.hpp"
-#include "../../operations/remez/remez_algorithm.hpp"
-#include "../../new_types/rat_approx.hpp"
+#include "operations/remez/remez_algorithm.hpp"
+#include "new_types/rat_approx.hpp"
 
 #ifdef USE_THREADS
  #include "routines/thread.hpp"
@@ -18,12 +18,22 @@ namespace nissa
     FILE *file=open_file(meas_pars.path,conf_created?"w":"a");
     
     close_file(file);
-
-		rat_approx_t appr;
-		double minimum=0.003, maximum=1.0;
-		int num=-1, den=2;
-		double tollerance=0.01, minerr=0.002;
-		generate_approx(appr,minimum,maximum,num,den,minerr,tollerance);
+    
+    const double minimum=1e-6;
+    const double maximum=1.0;
+    const int num=-2;
+    const int den=3;
+    const double tollerance=0.01;
+    const double minerr=0.0;
+    
+    const double t_in=take_time();
+    rat_approx_t appr;
+    appr.resize(20);
+    double res=generate_approx(appr,minimum,maximum,num,den,minerr,tollerance);
+    master_printf("Result, res: %lg\n",res);
+    appr.master_fprintf(stdout);
+    appr.master_fprintf_expr(stdout);
+    printf("time required= %.10e secs\n",take_time()-t_in);
   }
   
   //print
