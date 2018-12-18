1715734b23aa51c6b30b38ed244e3f9a6c71ce8a
Marco Cardinali <mcardina@r000u06l01.marconi.cineca.it>
2018-12-19 00:28:01 +0100

    First try to use minmax_eignevalues function. Check needed.

--- src/measures/fermions/minmax_eigenvalues.cpp
+++ src/measures/fermions/minmax_eigenvalues.cpp
@@ -2,7 +2,10 @@
  #include "config.hpp"
 #endif
 
+
+#include "dirac_operators/overlap/dirac_operator_overlap.hpp"
 #include "minmax_eigenvalues.hpp"
+#include "eigenvalues/eigenvalues.hpp"
 #include "operations/remez/remez_algorithm.hpp"
 #include "new_types/rat_approx.hpp"
 
@@ -33,7 +36,49 @@ namespace nissa
     master_printf("Result, res: %lg\n",res);
     appr.master_fprintf_expr(stdout);
     printf("time required= %.10e secs\n",take_time()-t_in);
-  }
+   
+    ////////////////////////////////////////////////////
+    // FROM HERE C. BONANNO AND M.CARDINALI OVERLAP ////
+    ////////////////////////////////////////////////////
+
+    complex* D_ov_eig_val=nissa_malloc("D_ov_eig_val",meas_pars.neig,complex)
+    spincolor** eigvec=nissa_malloc("eigvec",meas_pars.neig,spincolor*);
+    for(int ieig=0;ieig<meas_pars.neig;ieig++)
+    eigvec[ieig]=nissa_malloc("eig", loc_vol+bord_vol, spincolor);
+
+    
+
+    master_printf("neigs=%d, eig_precision=%.2e\n",meas_pars.neigs,meas_pars.eig_precision);
+
+    //Application of the Overlap Operator
+    const auto imp_mat=[conf,M,minerr](complex* out_lx,complex *in_lx)
+	{
+           apply_overlap((spincolor*)out_lx,conf, theory_pars[0].mass_overlap, minerr, (spincolor*)in_lx);
+      	};
+    const auto filler=[](complex *out_lx){generate_undiluted_source((spincolor*)out_lx,RND_GAUSS,-1);};
+ 
+    //Parameters of the eigensolver
+    const int mat_size=loc_vol*NCOL;
+    const int mat_size_to_allocate=(loc_vol+bord_vol)*NCOL;
+    const int niter_max=100000000;
+    master_printf("mat_size=%d, mat_size_to_allocate=%d\n", mat_size, mat_size_to_allocate);
+ 
+    double eig_time=-take_time();
+
+    //Find eigenvalues and eigenvectors of the overlap
+    eigenvalues_of_hermatr_find(eigvec, D_ov_eig_val, meas_pars.neigs, meas_pars.min_max, mat_size, mat_size_to_allocate, imp_mat, meas_pars.eig_precision, niter_max, filler);
+ 
+    master_printf("\n\nEigenvalues of D Overlap:\n");
+    for(int ieig=0;ieig<meas_pars.neigs;++ieig)
+      {
+	 master_printf("%d (%.16lg,%.16lg)\n)",ieig, D_ov_eig_val[RE], D_ov_eig_val[IM]);
+      }
+
+    master_printf("\n\n\n");
+ 
+    eig_time+=take_time();
+    master_printf("Eigenvalues time: %lg\n", eig_time);
+    }
   
   //print
   std::string minmax_eigenvalues_meas_pars_t::get_str(bool full)
