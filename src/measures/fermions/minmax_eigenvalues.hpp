722a2f68431c8e657f35950c4e413e729755347e
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2018-12-19 15:54:57 +0100

    Accepted

--- src/measures/fermions/minmax_eigenvalues.hpp
+++ src/measures/fermions/minmax_eigenvalues.hpp
@@ -8,8 +8,14 @@ namespace nissa
 {
   struct minmax_eigenvalues_meas_pars_t : base_fermionic_meas_t
   {
-    std::string def_path(){return "colibri";}
+    std::string def_path(){return "plover";}
     
+    int neigs;
+    double eig_precision;
+    int def_neigs(){return 5;}
+    double def_eig_precision(){return 1e-5;}
+    int min_max;
+    int def_min_max(){return 0;}
     int master_fprintf(FILE *fout,bool full) {return nissa::master_fprintf(fout,"%s",get_str().c_str());}
     std::string get_str(bool full=false);
     
@@ -17,15 +23,22 @@ namespace nissa
     {
       return
 	base_fermionic_meas_t::is_nonstandard()||
-	path!=def_path();
+	path!=def_path() or
+        neigs!=def_neigs() or
+        eig_precision!=def_eig_precision() or
+        min_max!=def_min_max();
     }
     
     minmax_eigenvalues_meas_pars_t() :
-      base_fermionic_meas_t()
-    {path=def_path();}
+    base_fermionic_meas_t(),
+    neigs(def_neigs()),
+    eig_precision(def_eig_precision()),
+    min_max(def_min_max())
+    {
+      path=def_path();
+    }
     virtual ~minmax_eigenvalues_meas_pars_t(){}
   };
-  
   void measure_minmax_eigenvalues(quad_su3 **conf,theory_pars_t &theory_pars,minmax_eigenvalues_meas_pars_t &meas_pars,int iconf,int conf_created);
 }
 
