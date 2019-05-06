c17610e0cbdfefd23110330132e1ed08214a1fc6
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2019-05-06 17:04:21 +0200

    Adding spincolor Wflow

--- src/operations/smearing/Wflow.hpp
+++ src/operations/smearing/Wflow.hpp
@@ -18,6 +18,18 @@ namespace nissa
   {
     void update_arg(quad_su3 *arg,quad_su3 *conf,double dt,bool *dirs,int iter);
     void update_conf(quad_su3 *arg,quad_su3 *conf,bool *dirs);
+    
+    //call the 2-links Laplace operator, for staggered fields
+    inline void Laplace_operator_switch(color *out,quad_su3 *c,color *in)
+    {
+      Laplace_operator_2_links(out,c,in);
+    }
+    
+    //call the 1-link Laplace operator, for non-staggered fields
+    inline void Laplace_operator_switch(spincolor *out,quad_su3 *c,spincolor *in)
+    {
+      Laplace_operator(out,c,in);
+    }
   }
   
   //structure to Wilson flow
@@ -63,7 +75,8 @@ namespace nissa
   /////////////////////////////////////////////////// fermions /////////////////////////////////////////////////////////
   
   //store conf used to perform fermionic flow
-  template <int nint_steps=3>
+  template <typename T=color,
+	    int nint_steps=3>
   struct internal_fermion_flower_t
   {
     int nd;
@@ -77,10 +90,8 @@ namespace nissa
     //storage for staples
     quad_su3 *arg;
     //creator
-    internal_fermion_flower_t(double dt,bool *ext_dirs,bool stag) : dt(dt)
+    internal_fermion_flower_t(double dt,bool *ext_dirs) : dt(dt)
     {
-      if(not stag) crash("non-staggered case not implemented yet");
-      
       //copy dirs
       for(int mu=0;mu<NDIM;mu++) dirs[mu]=ext_dirs[mu];
       //allocate confs
@@ -89,12 +100,14 @@ namespace nissa
       //alllocate staple
       arg=nissa_malloc("arg",loc_vol,quad_su3);
       
-      nd=loc_vol*sizeof(color)/sizeof(double);
+      nd=loc_vol*sizeof(T)/sizeof(double);
     }
     
     //add or remove backfield
     void add_or_rem_backfield_to_confs(bool add_rem,quad_u1 **u1)
-    {for(int i=0;i<nint_steps;i++) add_or_rem_backfield_with_or_without_stagphases_to_conf(conf[i],add_rem,u1,true);}
+    {
+      for(int i=0;i<nint_steps;i++) add_or_rem_backfield_with_or_without_stagphases_to_conf(conf[i],add_rem,u1,true);
+    }
     
     //setup from a given conf - nb: the conf is always evolved forward
     void generate_intermediate_steps(quad_su3 *ori_conf)
@@ -124,40 +137,41 @@ namespace nissa
   };
   
   //forward fermion flower
-  template <int nint_steps=3>
-  struct fermion_flower_t : public internal_fermion_flower_t<nint_steps>
+  template <typename T,
+	    int nint_steps=3>
+  struct fermion_flower_t : public internal_fermion_flower_t<T,nint_steps>
   {
     //aux fields
-    color *df0,*df1,*df2,*f1,*f2;
+    T *df0,*df1,*df2,*f1,*f2;
     
     //creator
-    fermion_flower_t(double dt,bool *ext_dirs,bool stag) : internal_fermion_flower_t<nint_steps>(dt,ext_dirs,stag)
+    fermion_flower_t(double dt,bool *ext_dirs) : internal_fermion_flower_t<T,nint_steps>(dt,ext_dirs)
     {
-      df0=nissa_malloc("df0",loc_vol+bord_vol,color);
-      df1=nissa_malloc("df1",loc_vol+bord_vol,color);
-      df2=nissa_malloc("df2",loc_vol+bord_vol,color);
-      f1=nissa_malloc("f1",loc_vol+bord_vol,color);
-      f2=nissa_malloc("f2",loc_vol+bord_vol,color);
+      df0=nissa_malloc("df0",loc_vol+bord_vol,T);
+      df1=nissa_malloc("df1",loc_vol+bord_vol,T);
+      df2=nissa_malloc("df2",loc_vol+bord_vol,T);
+      f1=nissa_malloc("f1",loc_vol+bord_vol,T);
+      f2=nissa_malloc("f2",loc_vol+bord_vol,T);
     }
     
     //flow a field
-    void flow_fermion(color *field)
+    void flow_fermion(T *field)
     {
       quad_su3 **conf=this->conf;
       double &dt=this->dt;
       int &nd=this->nd;
       
-      color *f0=field;
+      T *f0=field;
       
       //zero step: phi1 = phi0 + de0/4
-      Laplace_operator_2_links(df0,conf[0],f0);
+      Wflow::Laplace_operator_switch(df0,conf[0],f0);
       double_vector_summ_double_vector_prod_double((double*)f1,(double*)f0,(double*)df0,dt/4,nd);
       //first step: phi2 = phi0 + de1*8/9 - df0*2/9
-      Laplace_operator_2_links(df1,conf[1],f1);
+      Wflow::Laplace_operator_switch(df1,conf[1],f1);
       double_vector_summ_double_vector_prod_double((double*)f2,(double*)f0,(double*)df1,8.0*dt/9,nd);
       double_vector_summassign_double_vector_prod_double((double*)f2,(double*)df0,-2.0*dt/9,nd);
       //second step: f = phi3 = phi1 + df2*3/4
-      Laplace_operator_2_links(df2,conf[2],f2);
+      Wflow::Laplace_operator_switch(df2,conf[2],f2);
       double_vector_summ_double_vector_prod_double((double*)f0,(double*)f1,(double*)df2,3.0*dt/4,nd);
     }
     
@@ -180,38 +194,39 @@ namespace nissa
   };
   
   //adjoint fermion flower
-  template <int nint_steps=3>
-  struct fermion_adjoint_flower_t : public internal_fermion_flower_t<nint_steps>
+  template <typename T=color,
+	    int nint_steps=3>
+  struct fermion_adjoint_flower_t : public internal_fermion_flower_t<T,nint_steps>
   {
     //aux fields
-    color *l1,*l2;
+    T *l1,*l2;
     
     //creator
-    fermion_adjoint_flower_t(double dt,bool *ext_dirs,bool stag) : internal_fermion_flower_t<nint_steps>(dt,ext_dirs,stag)
+    fermion_adjoint_flower_t(double dt,bool *ext_dirs) : internal_fermion_flower_t<T,nint_steps>(dt,ext_dirs)
     {
-      l2=nissa_malloc("l2",loc_vol+bord_vol,color);
-      l1=nissa_malloc("l1",loc_vol+bord_vol,color);
+      l2=nissa_malloc("l2",loc_vol+bord_vol,T);
+      l1=nissa_malloc("l1",loc_vol+bord_vol,T);
     }
     
     //flow a field
-    void flow_fermion(color *field)
+    void flow_fermion(T *field)
     {
       quad_su3 **conf=this->conf;
       double &dt=this->dt;
       int &nd=this->nd;
       
-      color *l3=field,*l0=l3;
+      T *l3=field,*l0=l3;
       
       //zero step: l2 = d2l3*3/4
-      Laplace_operator_2_links(l2,conf[2],l3);
+      Wflow::Laplace_operator_switch(l2,conf[2],l3);
       double_vector_prodassign_double((double*)l2,3.0*dt/4,nd);
       //first step: l1 = l3 + d1l2*8/9
-      Laplace_operator_2_links(l1,conf[1],l2);
+      Wflow::Laplace_operator_switch(l1,conf[1],l2);
       double_vector_summ_double_vector_prod_double((double*)l1,(double*)l3,(double*)l1,8.0*dt/9,nd);
       //second step: l0 = l1 + l2 + d0 (l1 - l2*8/9)/4
       double_vector_summ((double*)l0,(double*)l1,(double*)l2,nd);                            //l0 = l1 + l2
       double_vector_summassign_double_vector_prod_double((double*)l1,(double*)l2,-8.0/9,nd); //l1 = l1 - l2*8/9
-      Laplace_operator_2_links(l2,conf[0],l1);                                               //l2 = d0 (l1 - l2*8/9)
+      Wflow::Laplace_operator_switch(l2,conf[0],l1);                                         //l2 = d0 (l1 - l2*8/9)
       double_vector_summassign_double_vector_prod_double((double*)l0,(double*)l2,dt/4,nd);   //l0+= d0 (l1 - l2*8/9)/4
     }
     
@@ -224,8 +239,6 @@ namespace nissa
   };
   
   void Wflow_lx_conf(quad_su3 *conf,double dt,bool *dirs=all_dirs);
-  void Wflow_fermion_fields(quad_su3 *ori_conf,double dt,bool *dirs,int nfields,color **ferm,bool stag);
-  void Wflow_fermion_fields_adjoint(quad_su3 *ori_conf,double dt,bool *dirs,int nfields,color **ferm,bool stag);
 }
 
 #endif
