d7993c4f752a0f45803b28a440acaba363e3ac17
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2019-10-21 00:30:56 +0200

    Splitting thread_macros

--- src/threads/openmp_threads.hpp
+++ src/threads/openmp_threads.hpp
@@ -1,14 +1,12 @@
-#ifndef _THREAD_MACROS_HPP
-#define _THREAD_MACROS_HPP
+#ifndef _OPENMP_THREADS_HPP
+#define _OPENMP_THREADS_HPP
 
 #ifdef HAVE_CONFIG_H
  #include "config.hpp"
 #endif
 
-#include "debug.hpp"
-#ifdef USE_THREADS
- #include <omp.h>
-#endif
+#include "base/debug.hpp"
+#include <omp.h>
 
 #include "routines/thread.hpp"
 
@@ -17,88 +15,47 @@
  #include "bgq/bgq_barrier.hpp"
 #endif
 
-//////////////////////////////////////////////////////////////////////////////////////
-
-#ifndef USE_THREADS
- #define NACTIVE_THREADS 1
- #define MANDATORY_PARALLEL
- #define MANDATORY_NOT_PARALLEL
-#else
- #define NACTIVE_THREADS ((thread_pool_locked)?1:nthreads)
- #define MANDATORY_PARALLEL if(nthreads>1 && thread_pool_locked) crash("this cannot be called when threads are locked")
- #define MANDATORY_NOT_PARALLEL if(nthreads>1 && !thread_pool_locked) crash("this cannot be called when threads are not locked")
-#endif
+#define NACTIVE_THREADS ((thread_pool_locked)?1:nthreads)
+#define MANDATORY_PARALLEL if(nthreads>1 && thread_pool_locked) crash("this cannot be called when threads are locked")
+#define MANDATORY_NOT_PARALLEL if(nthreads>1 && !thread_pool_locked) crash("this cannot be called when threads are not locked")
 
 #define IS_PARALLEL (NACTIVE_THREADS!=1)
 
-#define NISSA_CHUNK_WORKLOAD(START,CHUNK_LOAD,END,EXT_START,EXT_END,CHUNK_ID,NCHUNKS) \
-  int WORKLOAD=EXT_END-EXT_START,					\
-  CHUNK_LOAD=(WORKLOAD+NCHUNKS-1)/NCHUNKS,				\
-  START=EXT_START+CHUNK_ID*CHUNK_LOAD,					\
-  END=START+CHUNK_LOAD< EXT_END ? START+CHUNK_LOAD : EXT_END
-
-#define NISSA_CHUNK_LOOP(INDEX,EXT_START,EXT_END,CHUNK_ID,NCHUNKS)	\
-  for(NISSA_CHUNK_WORKLOAD(START,CHUNK_LOAD,END,EXT_START,EXT_END,CHUNK_ID,NCHUNKS),INDEX=START;INDEX<END;INDEX++)
-
-#ifdef USE_THREADS
-
- #if defined BGQ && !defined BGQ_EMU
+#if defined BGQ && !defined BGQ_EMU
   #include <spi/include/kernel/location.h>
   //#define GET_THREAD_ID() uint32_t thread_id=Kernel_ProcessorID() //works only if 64 threads are used...
   #define GET_THREAD_ID() uint32_t thread_id=omp_get_thread_num()
  #else
   #define GET_THREAD_ID() uint32_t thread_id=omp_get_thread_num()
  #endif
- #define THREAD_ID thread_id
+
+#define THREAD_ID thread_id
  
- #ifdef THREAD_DEBUG
-  #define THREAD_BARRIER_FORCE() thread_barrier_internal()
-  #define THREAD_BARRIER()       if(!thread_pool_locked) thread_barrier_with_check(__FILE__,__LINE__)
- #else
-  #define THREAD_BARRIER_FORCE() thread_barrier_internal()
-  #define THREAD_BARRIER()       if(!thread_pool_locked) thread_barrier_without_check()
- #endif
+#ifdef THREAD_DEBUG
+ #define THREAD_BARRIER_FORCE() thread_barrier_internal()
+ #define THREAD_BARRIER()       if(!thread_pool_locked) thread_barrier_with_check(__FILE__,__LINE__)
+#else
+ #define THREAD_BARRIER_FORCE() thread_barrier_internal()
+ #define THREAD_BARRIER()       if(!thread_pool_locked) thread_barrier_without_check()
+#endif
  
- #define IS_MASTER_THREAD (THREAD_ID==0)
- 
- #define NISSA_PARALLEL_LOOP(INDEX,START,END)			\
+#define IS_MASTER_THREAD (THREAD_ID==0)
+
+#define NISSA_PARALLEL_LOOP(INDEX,START,END)			\
   NISSA_CHUNK_LOOP(INDEX,START,END,thread_id,NACTIVE_THREADS)
 
- #define THREAD_ATOMIC_EXEC(inst) do{THREAD_BARRIER();inst;THREAD_BARRIER();}while(0)
- #define THREAD_BROADCAST(out,in)			\
-   if(IS_MASTER_THREAD) broadcast_ptr=(void*)&in;	\
-   THREAD_ATOMIC_EXEC(memcpy(&out,broadcast_ptr,sizeof(out)));
- #define THREAD_BROADCAST_PTR(out,in)		\
-   if(IS_MASTER_THREAD) broadcast_ptr=in;	\
-   THREAD_ATOMIC_EXEC(memcpy(&out,&broadcast_ptr,sizeof(void*)));
-
-#else
-
- #define GET_THREAD_ID()
- #define THREAD_ID (0)
- #define THREAD_BARRIER_FORCE()
- #define THREAD_BARRIER()
- #define IS_MASTER_THREAD (1)
- #define NISSA_PARALLEL_LOOP(INDEX,EXT_START,EXT_END) for(int INDEX=EXT_START;INDEX<EXT_END;INDEX++)
- #define THREAD_ATOMIC_EXEC(inst) inst
- #define THREAD_BROADCAST(out,in) (out)=(in)
-#define THREAD_BROADCAST_PTR(out,in) THREAD_BROADCAST(out,in)
-#endif
-
-//////////////////////////////////////////////////////////////////////////////////////
-
-#ifdef USE_THREADS
+#define THREAD_ATOMIC_EXEC(inst) do{THREAD_BARRIER();inst;THREAD_BARRIER();}while(0)
+#define THREAD_BROADCAST(out,in)			\
+  if(IS_MASTER_THREAD) broadcast_ptr=(void*)&in;		\
+  THREAD_ATOMIC_EXEC(memcpy(&out,broadcast_ptr,sizeof(out)));
+#define THREAD_BROADCAST_PTR(out,in)		\
+  if(IS_MASTER_THREAD) broadcast_ptr=in;				\
+  THREAD_ATOMIC_EXEC(memcpy(&out,&broadcast_ptr,sizeof(void*)));
 
 //external argument to exchange info between function and worker
 #define EXTERNAL_ARG(FUNC_NAME,LINE,ARG_TYPE,ARG) namespace{ARG_TYPE NAME4(FUNC_NAME,LINE,EXT_ARG,ARG);}
 #define EXPORT_ARG(FUNC_NAME,LINE,ARG) NAME4(FUNC_NAME,LINE,EXT_ARG,ARG)=ARG;
 #define IMPORT_ARG(FUNC_NAME,LINE,ARG_TYPE,ARG) ARG_TYPE ARG=NAME4(FUNC_NAME,LINE,EXT_ARG,ARG);
-
-#endif
-
-//////////////////////////////////////////////////////////////////////////////////////
-
-#ifdef USE_THREADS
 
 //headers: external parameters
 #define THREADABLE_FUNCTION_0ARG_EXTERNAL_ARGS(FUNC_NAME,LINE)
@@ -519,31 +476,6 @@ inline void thread_barrier_internal()
 #endif
 }
 
-#else
-
-#define THREADABLE_FUNCTION_0ARG(FUNC_NAME) void FUNC_NAME(){
-#define THREADABLE_FUNCTION_1ARG(FUNC_NAME,AT1,A1) void FUNC_NAME(AT1 A1){
-#define THREADABLE_FUNCTION_2ARG(FUNC_NAME,AT1,A1,AT2,A2) void FUNC_NAME(AT1 A1,AT2 A2){
-#define THREADABLE_FUNCTION_3ARG(FUNC_NAME,AT1,A1,AT2,A2,AT3,A3) void FUNC_NAME(AT1 A1,AT2 A2,AT3 A3){
-#define THREADABLE_FUNCTION_4ARG(FUNC_NAME,AT1,A1,AT2,A2,AT3,A3,AT4,A4) void FUNC_NAME(AT1 A1,AT2 A2,AT3 A3,AT4 A4){
-#define THREADABLE_FUNCTION_5ARG(FUNC_NAME,AT1,A1,AT2,A2,AT3,A3,AT4,A4,AT5,A5) void FUNC_NAME(AT1 A1,AT2 A2,AT3 A3,AT4 A4,AT5 A5){
-#define THREADABLE_FUNCTION_6ARG(FUNC_NAME,AT1,A1,AT2,A2,AT3,A3,AT4,A4,AT5,A5,AT6,A6) void FUNC_NAME(AT1 A1,AT2 A2,AT3 A3,AT4 A4,AT5 A5,AT6 A6){
-#define THREADABLE_FUNCTION_7ARG(FUNC_NAME,AT1,A1,AT2,A2,AT3,A3,AT4,A4,AT5,A5,AT6,A6,AT7,A7) void FUNC_NAME(AT1 A1,AT2 A2,AT3 A3,AT4 A4,AT5 A5,AT6 A6,AT7 A7){
-#define THREADABLE_FUNCTION_8ARG(FUNC_NAME,AT1,A1,AT2,A2,AT3,A3,AT4,A4,AT5,A5,AT6,A6,AT7,A7,AT8,A8) void FUNC_NAME(AT1 A1,AT2 A2,AT3 A3,AT4 A4,AT5 A5,AT6 A6,AT7 A7,AT8 A8){
-#define THREADABLE_FUNCTION_9ARG(FUNC_NAME,AT1,A1,AT2,A2,AT3,A3,AT4,A4,AT5,A5,AT6,A6,AT7,A7,AT8,A8,AT9,A9) void FUNC_NAME(AT1 A1,AT2 A2,AT3 A3,AT4 A4,AT5 A5,AT6 A6,AT7 A7,AT8 A8,AT9 A9){
-#define THREADABLE_FUNCTION_10ARG(FUNC_NAME,AT1,A1,AT2,A2,AT3,A3,AT4,A4,AT5,A5,AT6,A6,AT7,A7,AT8,A8,AT9,A9,AT10,A10) void FUNC_NAME(AT1 A1,AT2 A2,AT3 A3,AT4 A4,AT5 A5,AT6 A6,AT7 A7,AT8 A8,AT9 A9,AT10 A10){
-#define THREADABLE_FUNCTION_11ARG(FUNC_NAME,AT1,A1,AT2,A2,AT3,A3,AT4,A4,AT5,A5,AT6,A6,AT7,A7,AT8,A8,AT9,A9,AT10,A10,AT11,A11) void FUNC_NAME(AT1 A1,AT2 A2,AT3 A3,AT4 A4,AT5 A5,AT6 A6,AT7 A7,AT8 A8,AT9 A9,AT10 A10,AT11 A11){
-
-#define cache_flush()
-
-inline void thread_barrier_internal()
-{
-}
-
-#endif //use_threads
-
-#define THREADABLE_FUNCTION_END }
-
 #define FORM_TWO_THREAD_TEAMS()						\
   bool is_in_first_team,is_in_second_team;				\
   unsigned int nthreads_in_team,thread_in_team_id;			\
@@ -568,4 +500,5 @@ inline void thread_barrier_internal()
 	  thread_in_team_id=thread_id-nthreads/2;			\
 	}								\
     }
+
 #endif
---
+++ src/threads/threads.hpp
@@ -0,0 +1,32 @@
+#ifndef _THREADS_HPP
+#define _THREADS_HPP
+
+#ifdef HAVE_CONFIG_H
+ #include "config.hpp"
+#endif
+
+#define NISSA_CHUNK_WORKLOAD(START,CHUNK_LOAD,END,EXT_START,EXT_END,CHUNK_ID,NCHUNKS) \
+  int WORKLOAD=EXT_END-EXT_START,					\
+  CHUNK_LOAD=(WORKLOAD+NCHUNKS-1)/NCHUNKS,				\
+  START=EXT_START+CHUNK_ID*CHUNK_LOAD,					\
+  END=START+CHUNK_LOAD< EXT_END ? START+CHUNK_LOAD : EXT_END
+
+#define NISSA_CHUNK_LOOP(INDEX,EXT_START,EXT_END,CHUNK_ID,NCHUNKS)	\
+  for(NISSA_CHUNK_WORKLOAD(START,CHUNK_LOAD,END,EXT_START,EXT_END,CHUNK_ID,NCHUNKS),INDEX=START;INDEX<END;INDEX++)
+
+#define THREADABLE_FUNCTION_END }
+
+#if THREADS_TYPE == NO_THREADS
+ #include "threads/no_threads.hpp"
+#elif THREADS_TYPE == OPENMP_THREADS
+ #include "threads/openmp_threads.hpp"
+#elif THREADS_TYPE == CUDA_THREADS
+#else
+ #error Unknown thread paralleization THREADS_TYPE !
+#endif
+
+namespace nissa
+{
+}
+
+#endif
