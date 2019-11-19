bfd309d4eddbbd042dd82cd89addf963a3f69236
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2019-11-19 14:57:43 +0100

    trying array

--- projects/ib/contr.cpp
+++ projects/ib/contr.cpp
@@ -573,7 +573,8 @@ namespace nissa
     complex *loc_contr=new complex[bar2pts_contr_size];
     memset(loc_contr,0,sizeof(complex)*bar2pts_contr_size);
     
-    const int eps[3][2]={{1,2},{2,0},{0,1}},sign[2]={1,-1};
+    const int eps[3][2]={{1,2},{2,0},{0,1}};
+    std::array<int,2> sign={1,-1};
     
     void (*list_fun[2])(complex,const complex,const complex)={complex_summ_the_prod,complex_subt_the_prod};
     UNPAUSE_TIMING(bar2pts_contr_time);
