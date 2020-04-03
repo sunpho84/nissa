89d8d071761fee2707afff7bb6f21a3904338bce
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2020-04-03 18:48:24 +0200

    DDalpha not working for the square

--- src/inverters/twisted_clover/cg_64_invert_tmclovD_eoprec.cpp
+++ src/inverters/twisted_clover/cg_64_invert_tmclovD_eoprec.cpp
@@ -99,23 +99,24 @@ namespace nissa
     nissa_free(vir_invCl_evn);
     if(guess!=NULL) nissa_free(vir_guess);
 #elif defined USE_DDALPHAAMG
-    if(use_DD and fabs(mu)<=DD::max_mass)
-	{
-	  quad_su3 *lx_conf=nissa_malloc("lx_conf",loc_vol,quad_su3);
-	  paste_eo_parts_into_lx_vector(lx_conf,eo_conf);
-	  spincolor *tmp_in=nissa_malloc("tmp_in",loc_vol,spincolor);
-	  spincolor *tmp_out=nissa_malloc("tmp_out",loc_vol,spincolor);
-	  vector_reset(tmp_in);
-	  double_vector_copy((double*)tmp_in,(double*)source,loc_volh*sizeof(spincolor)/sizeof(double));
-	  DD::solve(tmp_out,lx_conf,kappa,cSW,mu,residue,tmp_in,true);
-	  nissa_free(lx_conf);
-	  inv_tmclovDkern_eoprec_square_eos_cg_64_portable(sol,guess,eo_conf,kappa,Cl_odd,invCl_evn,mu,niter,residue,source);
-	  master_printf("%lg %lg\n",tmp_out[0][0][0][0],sol[0][0][0][0]);
-	  master_printf("%lg %lg\n",tmp_out[0][0][0][1],sol[0][0][0][1]);
-	  nissa_free(tmp_out);
-	  nissa_free(tmp_in);
-	}
-    else
+    // if(use_DD and fabs(mu)<=DD::max_mass)
+    // 	{
+    // 	  quad_su3 *lx_conf=nissa_malloc("lx_conf",loc_vol,quad_su3);
+    // 	  paste_eo_parts_into_lx_vector(lx_conf,eo_conf);
+    // 	  spincolor *tmp_in=nissa_malloc("tmp_in",loc_vol,spincolor);
+    // 	  spincolor *tmp_out=nissa_malloc("tmp_out",loc_vol,spincolor);
+    // 	  vector_reset(tmp_in);
+    // 	  double_vector_copy((double*)tmp_in,(double*)source,loc_volh*sizeof(spincolor)/sizeof(double));
+    // 	  DD::solve(tmp_out,lx_conf,kappa,cSW,mu,residue,tmp_in,true);
+    // 	  nissa_free(lx_conf);
+    // 	  inv_tmclovDkern_eoprec_square_eos_cg_64_portable(sol,guess,eo_conf,kappa,Cl_odd,invCl_evn,mu,niter,residue,source);
+    // 	  master_printf("%lg %lg\n",tmp_out[0][0][0][0],sol[0][0][0][0]);
+    // 	  master_printf("%lg %lg\n",tmp_out[0][0][0][1],sol[0][0][0][1]);
+    // 	  nissa_free(tmp_out);
+    // 	  nissa_free(tmp_in);
+    // 	}
+    // else
+    master_printf("DDalpha not yet working, probably expecting a different layout\n");
 #endif
     inv_tmclovDkern_eoprec_square_eos_cg_64_portable(sol,guess,eo_conf,kappa,Cl_odd,invCl_evn,mu,niter,residue,source);
   }
