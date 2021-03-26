6a89c3985f644f2a6471db66bbd4e3db5254ecbf
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2021-03-26 18:33:25 +0100

    Fixed *p[i]

--- src/measures/contract/tm_baryon.cpp
+++ src/measures/contract/tm_baryon.cpp
@@ -90,8 +90,8 @@ namespace nissa
 	      for(int co_so=0;co_so<NCOL;co_so++)
 		for(int co_si=0;co_si<NCOL;co_si++)
 		  {
-		    master_printf("Copying: ivol=%d co_si=%d co_so=%d sp_si=%d sp_so=%d \n",ivol,co_si,co_so,sp_si,sp_so);
-		    complex_copy((*p)[i][co_si][co_so][sp_si][sp_so],Q[i][co_so+NCOL*sp_so][ivol][sp_si][co_si]);
+		    //master_printf("Copying: ivol=%d co_si=%d co_so=%d sp_si=%d sp_so=%d \n",ivol,co_si,co_so,sp_si,sp_so);
+		    complex_copy(*(p[i])[co_si][co_so][sp_si][sp_so],Q[i][co_so+NCOL*sp_so][ivol][sp_si][co_si]);
 		  }
 	
 	//Color source
