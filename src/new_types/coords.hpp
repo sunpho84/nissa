fbb790a9c11ad2bcd92e61f97c8a7ef5928e7f87
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2021-04-12 01:33:44 +0200

    Adding coords in a separate file

---
+++ src/new_types/coords.hpp
@@ -0,0 +1,14 @@
+#ifndef _COORDS_HPP
+#define _COORDS_HPP
+
+#ifdef HAVE_CONFIG_H
+ #include <config.hpp>
+#endif
+
+namespace nissa
+{
+  typedef int coords[NDIM];
+  typedef double momentum_t[NDIM];
+}
+
+#endif
