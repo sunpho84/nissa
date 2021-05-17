4a236f2200c60f521a3e26615d8f06d11b511a7b
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2021-05-17 18:40:05 +0200

    Improved readibility of newexpr test

--- projects/ib/testNewExpr.cpp
+++ projects/ib/testNewExpr.cpp
@@ -2,27 +2,45 @@
 
 using namespace nissa;
 
-void in_main(int narg,char** arg)
+void test(Tensor<OfComps<Dir,ColorRow,ComplId>>& v,
+	  Tensor<OfComps<Dir,ColorRow,ComplId>>& z)
 {
-  Tensor<OfComps<Dir,ColorRow>> v;
-  FOR_ALL_DIRS(mu)
-    FOR_ALL_ROW_COLORS(cr)
-      v(mu,cr)=cr()+NCOL*mu();
   
   /// Scalar product on color index
   Tensor<OfComps<Dir>> res1;
-  res1=dag(v)*v;
+  ASM_BOOKMARK_BEGIN("scalProd");
+  res1=real(dag(v)*z);
+  ASM_BOOKMARK_END("scalProd");
   
-  master_printf("%lg\n",res1(Dir(0))); //5
-  master_printf("%lg\n",res1(Dir(1))); //50
+  master_printf("%lg\n",res1(Dir(0))); //55
+  //master_printf("%lg\n",res1(Dir(1))); //50
   
   /// Outer product on color index
-  Tensor<OfComps<Dir,ColorRow,ColorCln>> res2;
-  res2=v*dag(v);
+  Tensor<OfComps<Dir,ColorRow,ColorCln,ComplId>> res2;
+  ASM_BOOKMARK_BEGIN("outerProd");
+  res2=v*dag(z);
+  ASM_BOOKMARK_END("outerProd");
+  
+  master_printf("%lg\n",res2(Dir(0),ColorRow(0),ColorCln(0),Re)); //1
   
   /// Comp by comp product on color index
-  Tensor<OfComps<Dir,ColorRow>> res3;
-  res3=v*v;
+  Tensor<OfComps<Dir,ColorRow,ComplId>> res3;
+  ASM_BOOKMARK_BEGIN("compByCompProd");
+  res3=v*z;
+  ASM_BOOKMARK_END("compByCompProd");
+  
+  master_printf("%lg\n",res3(Dir(0),ColorRow(0),Re)); //-1
+}
+
+void in_main(int narg,char** arg)
+{
+  Tensor<OfComps<Dir,ColorRow,ComplId>> v;
+  FOR_ALL_DIRS(mu)
+    FOR_ALL_ROW_COLORS(cr)
+    FOR_REIM_PARTS(ri)
+    v(mu,cr,ri)=ri()+2*(cr()+NCOL*mu());
+  
+  test(v,v);
 }
 
 int main(int narg,char** arg)
