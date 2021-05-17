2cdf6d8a6450ec5ab5128206f106c646854c61ed
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2021-05-17 18:41:01 +0200

    Tuple can be filtered passing a tuple as a crivel

--- src/metaProgramming/tuple.hpp
+++ src/metaProgramming/tuple.hpp
@@ -206,13 +206,28 @@ namespace nissa
     return std::make_tuple(std::get<Tout>(in)...);
   }
   
-  /// Get the list elements from
+  /// Get the list elements from a tuple
   template <typename...Tout,
 	    typename...Tin>
   void tupleFillWithSubset(std::tuple<Tout...>& out,
 			   const std::tuple<Tin...>& in)
   {
     out=tupleGetMany<Tout...>(in);
+  }
+  
+  /// Get the list elements of the passed tuple
+  ///
+  /// \example
+  /// auto tupleGetSubset<std::tuple<int>>(std::make_tuple(1,10.0));
+  template <typename TpOut,
+	    typename...Tin>
+  auto tupleGetSubset(const std::tuple<Tin...>& in)
+  {
+    TpOut out;
+    
+    tupleFillWithSubset(out,in);
+    
+    return out;
   }
   
   /////////////////////////////////////////////////////////////////
