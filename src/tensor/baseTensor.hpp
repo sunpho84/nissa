fb979775ed68c67e47a2766d6e292c497196f288
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2021-06-02 01:18:13 +0200

    Moved to decl

--- src/tensor/baseTensor.hpp
+++ src/tensor/baseTensor.hpp
@@ -10,9 +10,9 @@
 /// \brief Implements basic functionalities of tensors
 
 #include <memory/memoryManager.hpp>
-#include <memory/storLoc.hpp>
 #include <metaProgramming/crtp.hpp>
 #include <metaProgramming/refOrVal.hpp>
+#include <tensor/tensorDecl.hpp>
 #include <tensor/expr.hpp>
 #include <tensor/indexComputer.hpp>
 
@@ -109,51 +109,6 @@ namespace nissa
     /// Import assignement operator from ex
     using Ex::operator=;
   };
-  
-  /////////////////////////////////////////////////////////////////
-  
-  enum class TensorDynamicity{STACKED_TENSOR,DYNAMIC_TENSOR};
-  
-  namespace details
-  {
-    template <typename TC,
-	      typename Fund,
-	      StorLoc SL>
-    struct _TensorStackedDecider
-    {
-      using IC=
-	IndexComputer<TC>;
-      
-      static constexpr auto staticSize=
-	IC::staticPartMaxValue*sizeof(Fund);
-      
-      /// Threshold beyond which allocate dynamically in any case
-      static constexpr
-      Size MAX_STACK_SIZE=2304;
-      
-      static constexpr bool isSmall=
-	staticSize<MAX_STACK_SIZE;
-      
-      static constexpr bool hasNoDynamicComps=
-	 IC::allCompsAreStatic;
-      
-      static constexpr bool compilingForSameLoc=
-	     ((CompilingForDevice==true  and SL==StorLoc::ON_GPU) or
-	      (CompilingForDevice==false and SL==StorLoc::ON_CPU));
-      
-      static constexpr
-      TensorDynamicity tensorDynamicity=
-	(isSmall and hasNoDynamicComps and compilingForSameLoc)?
-	TensorDynamicity::STACKED_TENSOR:
-	TensorDynamicity::DYNAMIC_TENSOR;
-    };
-  }
-  
-  template <typename TC,
-	    typename Fund=double,
-	    StorLoc SL=DefaultStorage,
-	    TensorDynamicity Dynamicity=details::_TensorStackedDecider<TC,Fund,SL>::tensorDynamicity>
-  struct Tensor;
 }
 
 #endif
