1fcf100f2fe291f6f5085c587c2da3dc4c648b6a
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2021-07-09 18:14:08 +0200

    Fixed tpe

--- src/base/multiGridParams.hpp
+++ src/base/multiGridParams.hpp
@@ -30,7 +30,7 @@ namespace nissa
     
     EXTERN_MULTIGRID bool setup_valid INIT_TO(false);
     EXTERN_MULTIGRID int use_multiGrid INIT_TO(1);
-    EXTERN_MULTIGRID bool use_deflated_solver INIT_TO(false);
+    EXTERN_MULTIGRID int use_deflated_solver INIT_TO(0);
     
     /// If DDalphaamg is available, check if requested and if the mass is below the maximal
     inline bool checkIfDDalphaAvailableAndRequired(const double& mass)
