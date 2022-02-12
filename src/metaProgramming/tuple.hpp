dd0a7ec93369dd3ec42a729a5eb6958b89de1803
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2022-02-12 23:48:18 +0100

    Using unique_tuple which avoids to fully instantiate the discarded class

--- src/metaProgramming/tuple.hpp
+++ src/metaProgramming/tuple.hpp
@@ -170,25 +170,44 @@ namespace nissa
   {
     /// Tuple with unique types from a list
     ///
-    /// Internal implementation, default case
-    template <typename T,
-	      typename... Ts>
-    struct _UniqueTuple
+    /// Forward definition
+    template <typename U,
+	      typename...Os>
+    struct _UniqueTuple;
+    
+    /// Tuple with unique types from a list
+    ///
+    /// End case with no type to add
+    template <typename...Us>
+    struct _UniqueTuple<std::tuple<Us...>>
     {
-      using type=T;
+      /// Provide the list passed as input
+      using type=
+	std::tuple<Us...>;
     };
     
     /// Tuple with unique types from a list
     ///
-    /// Internal implementation
-    template <typename... Ts,
-	      typename U,
-	      typename... Us>
-    struct _UniqueTuple<std::tuple<Ts...>,U,Us...>
-      : std::conditional_t<(std::is_same_v<U,Ts> || ...),
-			   _UniqueTuple<std::tuple<Ts...>,Us...>,
-			   _UniqueTuple<std::tuple<Ts...,U>,Us...>>
+    /// Default case processing type O
+    template <typename...Us,
+	      typename O,
+	      typename...Os>
+    struct _UniqueTuple<std::tuple<Us...>,
+			O,Os...>
     {
+      /// Check if the type O is unique
+      static constexpr bool isOUnique=
+	((not std::is_same_v<Us,O>)&...&true);
+      
+      /// Compute next list of unique type which might include O
+      using NU=
+	std::conditional_t<isOUnique,
+			   std::tuple<Us...,O>,
+			   std::tuple<Us...>>;
+      
+      /// Get the full list from recursive analysis
+      using type=
+	typename _UniqueTuple<NU,Os...>::type;
     };
   }
   
