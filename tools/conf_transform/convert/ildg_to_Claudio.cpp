f35abb7f32d418ea967a4bcc121616aec30d843c
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2022-02-23 22:59:03 +0100

    Going to enable to run ildg_to_Claudio in parallel

--- tools/conf_transform/convert/ildg_to_Claudio.cpp
+++ tools/conf_transform/convert/ildg_to_Claudio.cpp
@@ -27,6 +27,14 @@ void write_to_binary_file(FILE *fp,su3 A)
     crash("Problems in writing Su3 matrix");
 }
 
+void index(int &irank_to,int &iel_to,int iel_fr,void *pars)
+{
+  const coords_t& c=glbCoordOfLoclx[iel_fr];
+  int num=snum(c[1],c[2],c[3],c[0]);
+  irank_to=iel_to/locVol;
+  iel_to=num-irank_to*locVol;
+}
+
 int main(int narg,char **arg)
 {
   char *in_conf_name,*out_conf_name;
@@ -34,8 +42,8 @@ int main(int narg,char **arg)
   //basic mpi initialization
   init_nissa(narg,arg);
   
-  if(nranks>1)
-    crash("cannot run in parallel");
+  // if(nranks>1)
+  //   crash("cannot run in parallel");
   
   if(narg<7) crash("use: %s T LX LY LZ file_in file_out",arg[0]);
   
@@ -75,61 +83,67 @@ int main(int narg,char **arg)
   
   quad_su3 *out_conf=nissa_malloc("out_conf",locVol,quad_su3);
   
-  //reorder data
-  for(int t=0;t<glbSize[0];t++)
-    for(int z=0;z<glbSize[3];z++)
-      for(int y=0;y<glbSize[2];y++)
-	for(int x=0;x<glbSize[1];x++)
-	  {
-	    int num=snum(x,y,z,t);
-	    
-	    coords_t c={t,x,y,z};
-	    int ivol=loclx_of_coord(c);
-	    
-	    for(int mu=0;mu<NDIM;mu++) su3_copy(out_conf[num][mu],in_conf[ivol][mu]);
-	  }
+  vector_remap_t(locVol,index,nullptr).remap(out_conf,in_conf,sizeof(quad_su3));
   
   nissa_free(in_conf);
   
   //open the file
-  FILE *fout=open_file(out_conf_name,"w");
-  if(fout==NULL) crash("while opening %s",out_conf_name);
+  FILE *fout;
   
-  fprintf(fout,"4 %d %d %d %d %d ",glbSize[0],glbSize[1],glbSize[2],glbSize[3],itraj);
+  if(rank==0)
+    {
+      fout=open_file(out_conf_name,"w");
+      if(fout==NULL) crash("while opening %s",out_conf_name);
+      
+      fprintf(fout,"4 %d %d %d %d %d ",glbSize[0],glbSize[1],glbSize[2],glbSize[3],itraj);
   
-  char res[2*MD5_DIGEST_LENGTH+1]="";
+      char res[2*MD5_DIGEST_LENGTH+1]="";
 #ifdef USE_SSL
-  MD5_Init(&mdContext);
-  NISSA_LOC_VOL_LOOP(ivol)
-    for(int mu=0;mu<NDIM;mu++)
-      for(int icol=0;icol<NCOL;icol++)
-	for(int jcol=0;jcol<NCOL;jcol++)
-	  {
-	    complex& c=out_conf[ivol][mu][icol][jcol];
-	    complex d;
-	    if(little_endian)
-	      change_endianness((double*)d,(double*)c,sizeof(complex)/sizeof(double));
-	    else
-	      complex_copy(d,c);
-	    MD5_Update(&mdContext,d,sizeof(complex));
-	  }
-  MD5_Final(c,&mdContext);
-  
-  for(int r=0;r<MD5_DIGEST_LENGTH;r++)
-    sprintf(&(res[2*r]), "%02x", c[r]);
-  res[2*MD5_DIGEST_LENGTH]='\0';
-  
-  master_printf("res: %s\n",res);
+      if(nranks==1)
+	{
+	  MD5_Init(&mdContext);
+	  NISSA_LOC_VOL_LOOP(ivol)
+	    for(int mu=0;mu<NDIM;mu++)
+	      for(int icol=0;icol<NCOL;icol++)
+		for(int jcol=0;jcol<NCOL;jcol++)
+		  {
+		    complex& c=out_conf[ivol][mu][icol][jcol];
+		    complex d;
+		    if(little_endian)
+		      change_endianness((double*)d,(double*)c,sizeof(complex)/sizeof(double));
+		    else
+		      complex_copy(d,c);
+		    MD5_Update(&mdContext,d,sizeof(complex));
+		  }
+	  MD5_Final(c,&mdContext);
+	  
+	  for(int r=0;r<MD5_DIGEST_LENGTH;r++)
+	    sprintf(&(res[2*r]), "%02x", c[r]);
+	  res[2*MD5_DIGEST_LENGTH]='\0';
+	  
+	  master_printf("res: %s\n",res);
+	}
 #endif
-  fprintf(fout,"%s\n",res);
+      fprintf(fout,"%s\n",res);
+      fclose(fout);
+    }
+  MPI_Barrier(MPI_COMM_WORLD);
   
-  //write the data
-  NISSA_LOC_VOL_LOOP(ivol)
-    for(int mu=0;mu<NDIM;mu++)
-	write_to_binary_file(fout,out_conf[ivol][mu]);
-  
-  //close the file
-  fclose(fout);
+  for(int iRank=0;iRank<nranks;iRank++)
+    {
+      if(rank==iRank)
+	{
+	  fout=open_file(out_conf_name,"a");
+	  //write the data
+	  NISSA_LOC_VOL_LOOP(ivol)
+	    for(int mu=0;mu<NDIM;mu++)
+	      write_to_binary_file(fout,out_conf[ivol][mu]);
+	  
+	  //close the file
+	  fclose(fout);
+	}
+      MPI_Barrier(MPI_COMM_WORLD);
+    }
   
   nissa_free(out_conf);
   
