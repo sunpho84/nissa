7a088d4f132a36558167e1b406cff56a5b4e7a50
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2022-12-26 23:49:02 +0100

    Duplicating

--- projects/ib/test_wip.cpp
+++ projects/ib/test_wip.cpp
@@ -17,6 +17,54 @@ using namespace nissa;
 CUDA_MANAGED double* pt;
 CUDA_MANAGED int io;
 
+  template <typename IMin,
+	    typename IMax,
+	    typename F>
+  __global__
+  void cuda_generic_kernel2(const IMin min,
+			    const IMax max,
+			    const F& f)
+  {
+    const auto i=min+blockIdx.x*blockDim.x+threadIdx.x;
+    if(i<max)
+      f(i);
+  }
+  
+  template <typename IMin,
+	    typename IMax,
+	    typename F>
+  void cuda_parallel_for2(const int line,
+			 const char* file,
+			 const IMin min,
+			 const IMax max,
+			 const F& f)
+  {
+    const auto length=(max-min);
+    const dim3 block_dimension(NUM_THREADS);
+    const dim3 grid_dimension((length+block_dimension.x-1)/block_dimension.x);
+    
+    double initTime=0;
+    extern int rank,verbosity_lv;
+    const bool print=(verbosity_lv>=1// 2
+		      and rank==0);
+    if(print)
+      {
+	printf("at line %d of file %s launching kernel on loop [%ld,%ld) using blocks of size %d and grid of size %d\n",
+	   line,file,(int64_t)min,(int64_t)max,block_dimension.x,grid_dimension.x);
+	initTime=take_time();
+      }
+    
+    if(length>0)
+      {
+	cuda_generic_kernel2<<<grid_dimension,block_dimension>>>(min,max,f);
+	cudaDeviceSynchronize();
+      }
+    
+    if(print)
+      printf(" finished in %lg s\n",take_time()-initTime);
+  }
+
+
 void in_main(int narg,char **arg)
 {
   const int T=16,L=16;
@@ -34,9 +82,9 @@ void in_main(int narg,char **arg)
     
   }
 
-  cuda_parallel_for(
+  cuda_parallel_for2(
       44, "/home/francesco/QCD/SORGENTI/nissa_origi/projects/ib/test_wip.cpp",
-      0, 1, [confa = conf.getReadable()]__device__(const int &ivol) mutable {
+      0, 1, [confa = conf.getReadable()]__device__(const int &ivol) {
         pt = confa._data;
         io = confa.externalSize;
       });
