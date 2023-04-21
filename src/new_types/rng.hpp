7724ff1e95e73381345566dfa893f4900d416128
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2023-04-21 11:43:52 +0200

    Implemented the view over the flow

--- src/new_types/rng.hpp
+++ src/new_types/rng.hpp
@@ -10,29 +10,42 @@
 #include <cmath>
 #include <random>
 
+#include <routines/ios.hpp>
 #include <new_types/encypter.hpp>
 #include <new_types/float128class.hpp>
 #include <new_types/multiUnsignedInt.hpp>
 
 namespace nissa
 {
-  struct Rng
+  /// Random number generator status
+  ///
+  /// Keeps track of the point in the stream of random numbers
+  struct RngState
   {
+    /// Encrypter used to draw 32 bits of randomness
     Encrypter encrypter;
     
-    MultiUint<uint64_t,4> counter;
+    /// Type used to keep track of the state
+    using Counter=MultiUint<uint64_t,4>;
     
-    CUDA_HOST_AND_DEVICE INLINE_FUNCTION
-    Rng(const uint64_t& seed):
+    /// Keep track of the state
+    Counter counter;
+    
+    /// Generates the state
+    RngState(const uint64_t& seed=3472291050,
+	     const Counter& counter=0lu):
       encrypter(createEncrypterFromSeed(seed)),
-      counter({})
+      counter(counter)
     {
     }
     
+    /// Generate the encrypter from the seed
     static Encrypter createEncrypterFromSeed(const uint64_t& seed)
     {
+      /// Temporary word
       Encrypter::Word word;
       
+      /// Generator used to blow the seed
       std::mt19937_64 gen(seed);
       
       for(int i=0;i<4;i++)
@@ -40,11 +53,26 @@ namespace nissa
       
       return word;
     }
+  };
+  
+  /// Keeps track of a window over a stream of uint32_t
+  ///
+  /// Takes care of not treepassing the number of reserved numbers
+  struct RngView
+  {
+    /// State of the uint32_t flow
+    const RngState state;
+    
+    /// Number of reserved uint32_t
+    const uint64_t nReserved;
     
     /// Draw 32 bits of randomness
     CUDA_HOST_AND_DEVICE INLINE_FUNCTION
-    uint32_t draw32bits()
+    uint32_t draw32bitsWithOffset(const uint64_t& offset) const
     {
+      if(offset>=nReserved)
+	crash("going beyond the number of rserved uint32_t");
+      
       union
       {/// Temporary result of the encpter
 	Encrypter::Word temp;
@@ -53,76 +81,201 @@ namespace nissa
 	const uint32_t out{};
       };
       
-      temp=encrypter.encrypt(counter.val);
-      
-      counter++;
+      temp=state.encrypter.encrypt((state.counter+offset).val);
       
       return out;
     }
     
-    /// Transforms two uint32_t into a high precision uniform
-    CUDA_HOST_AND_DEVICE INLINE_FUNCTION
-    static Float128 transform32bitsListIntoUniform(const uint32_t& e,
-						   const uint32_t& f)
+    /// Create the view and advances
+    RngView(RngState& state,
+	    const uint64_t& nReserved) :
+      state(state),
+      nReserved(nReserved)
     {
-      constexpr double a=1.0/(~0u+1.0);
-      constexpr double b=a*a;
-      const double e1=e*b,e2=f*a;
-      const Float128 d=Float128::sum(e1,e2);
+      state.counter+=nReserved;
+    }
+  };
+  
+  /// Keeps track of a window over a stream of float
+  ///
+  /// Takes care of not treepassing the number of reserved
+  /// numbers. The target distribution is parametrized with the D type
+  template <typename D>
+  struct RngDistrView
+  {
+    /// View over the uint32_t stream
+    const RngView view;
+    
+    /// Number of target type reserved
+    const uint64_t nReserved;
+    
+    /// Generates a random number according to the target distribution
+    CUDA_HOST_AND_DEVICE INLINE_FUNCTION
+    auto draw(const uint64_t& offset) const
+    {
+      if(offset>=nReserved)
+	crash("going beyond the number of reserved uint32_t");
       
-      return d;
+      /// Set of uint32_t needed to draw number from the target distribution
+      std::array<uint32_t,D::nDraw> tmp;
+      
+      for(int i=0;i<D::nDraw;i++)
+	tmp[i]=view.draw32bitsWithOffset(D::nDraw*offset+i);
+      
+      return D::transform(tmp);
     }
     
-    /// Generates a random number distributed in the range [0,1)
+    /// Construct from the RngState and reserved numbers
+    RngDistrView(RngState& state,
+		 const uint64_t& nReserved) :
+      view(state,D::nDraw*nReserved),
+      nReserved(nReserved)
+    {
+      if(nReserved>=~0lu/D::nDraw)
+	crash("asking to reserve too many numbers");
+    }
+  };
+  
+  namespace ProbDistr
+  {
+    /// Parametrize numbers uniformly distributed in the range [0,1)
     ///
     /// The Extended accuracy is needed to ensure that the
     /// distribution is evenly spaced close to 1, which in turn is
     /// needed to ensure that subsequent transformation do not deform
     /// the queues of the target distribution
-    CUDA_HOST_AND_DEVICE INLINE_FUNCTION
-    Float128 getUniform()
+    struct UniformRngDistr
     {
-      return transform32bitsListIntoUniform(draw32bits(),draw32bits());
-    }
-    
-    /// Generates a random number distributed in the range [0,1)
+      /// Transforms two uint32_t into a high precision uniform
+      CUDA_HOST_AND_DEVICE INLINE_FUNCTION
+      static Float128 transform(const std::array<uint32_t,2>& vals)
+      {
+	const uint32_t& e=vals[0];
+	const uint32_t& f=vals[1];
+	constexpr double a=1.0/(~0u+1.0);
+	constexpr double b=a*a;
+	const double e1=e*b,e2=f*a;
+	const Float128 d=Float128::sum(e1,e2);
+	
+	return d;
+      }
+      
+      /// Number of uint32_t needed to draw a number
+      static constexpr int nDraw=2;
+    };
+  }
+  
+  /// Generate numbers uniformly distributed in the range [0,1)
+  using RngUniformHighPrecDistrView=
+    RngDistrView<ProbDistr::UniformRngDistr>;
+  
+  /////////////////////////////////////////////////////////////////
+  
+  namespace ProbDistr
+  {
+    /// Parametrize normally distributed with average 0 and sigma 1
     ///
-    /// Do not use for making large transformation of the argument!
-    CUDA_HOST_AND_DEVICE INLINE_FUNCTION
-    double getUniformNoHighPrec()
+    /// Uses the Box-Muller transformation of two Float128 uniformly distributed
+    struct GaussRngDistr
     {
-      return getUniform().roundDown();
-    }
+      
+      /// Transforms two uint32_t into cos() with x uniformly distributed
+      ///
+      /// We draw a number between 0 and 1, then we multiply by 2,
+      /// this way we realize exactly the parity symmetry
+      CUDA_HOST_AND_DEVICE INLINE_FUNCTION
+      static double transformCos(const std::array<uint32_t,2>& vals)
+      {
+	// printf("%u %u\n",vals[0],vals[1]);
+	
+	/// Number needed to draw the angle
+	const Float128 b=
+	  2*UniformRngDistr::transform(vals);
+	// printf("b: %.017lg (0.5-b): %.017lg (1.5-b): %.017lg\n",b.roundDown(),(0.5-b).roundDown(),(1.5-b).roundDown());
+	
+	/// We need to tell apart the case close to pi/2 and -pi/2
+	const int c=(b>=5.0/4 and b<7.0/4)?2:
+	  ((b>=1.0/4 and b<3.0/4)?1:0);
+	
+	// printf("? %d\n",c);
+	// printf("2: %lg\n",sin(M_PI*(1.5-b).roundUp()));
+	// printf("1: %lg\n",sin(M_PI*(0.5-b).roundUp()));
+	// printf("0: %lg\n",cos(M_PI*b.roundDown()));
+	/// Computes cos(pi*b) taking care of the case close to 1/2
+	if(c==2)
+	  return
+	    sin(M_PI*(1.5-b).roundUp());
+	else
+	  if(c==1)
+	    return
+	      sin(M_PI*(0.5-b).roundUp());
+	  else
+	    return
+	      cos(M_PI*b.roundDown());
+      }
+      
+      /// Transforms two uint32_t into log(1-x) with x uniformly distributed
+      CUDA_HOST_AND_DEVICE INLINE_FUNCTION
+      static double transformLog(const std::array<uint32_t,2>& vals)
+      {
+	/// Number needed to draw the radius
+	const Float128 a=
+	  UniformRngDistr::transform(vals);
+	
+	/// Computes log(1-a) taking care of the case close to 1
+	const double c=
+	  (a<0.5)?
+	  std::log1p(-a.roundDown()):
+	  log((1-a).roundUp());
+	
+	return c;
+      }
+	
+      /// Transforms four uint32_t into a normally distributed
+      CUDA_HOST_AND_DEVICE INLINE_FUNCTION
+      static double transform(const std::array<uint32_t,4>& vals)
+      {
+	
+	return sqrt(-2*transformLog({vals[0],vals[1]}))*
+	  transformCos({vals[2],vals[3]});
+      }
+      
+      /// Number of uint32_t needed to draw a number
+      static constexpr int nDraw=4;
+    };
+  }
+  
+  /// Generate numbers normally distributed with average 0 and sigma 1
+  using RngGaussDistrView=
+    RngDistrView<ProbDistr::GaussRngDistr>;
+  
+  //   /// Generates a random number distributed in the range [0,1)
+  //   ///
+  //   /// Do not use for making large transformation of the argument!
+  //   CUDA_HOST_AND_DEVICE INLINE_FUNCTION
+  //   double getUniformNoHighPrec(const uint64_t& offset)
+  //   {
+  //     return draw(offset).roundDown();
+  //   }
+  // };
+  
+  
+  // /// Draw a random number following a Gauss distribution
+  //   CUDA_HOST_AND_DEVICE INLINE_FUNCTION
+  //   double getGauss()
+  //   {
+  //   }
     
-    /// Draw a random number following a Gauss distribution
-    CUDA_HOST_AND_DEVICE INLINE_FUNCTION
-    double getGauss()
-    {
-      const Float128 a=getUniform();
-      const Float128 b=2*getUniform();
+  //   /// Generate a gaussian number without aking care of all the details
+  //   CUDA_HOST_AND_DEVICE INLINE_FUNCTION
+  //   double getGaussNoHighPrec()
+  //   {
+  //     const double r=sqrt(-2*log(1-getUniformNoHighPrec()));
+  //     const double q=2*M_PI*getUniformNoHighPrec();
       
-      const double c=
-	(a<0.5)?
-	std::log1p(-a.roundDown()):
-	log((1-a).roundUp());
-      const double d=
-	((b>=1.0/4 and b<3.0/4) or (b>=5.0/4 and b<7.0/4))?
-	sin(M_PI*(0.5-b).roundUp()):
-	cos(M_PI*b.roundDown());
-      
-      return sqrt(-2*c)*d;
-    }
-    
-    /// Generate a gaussian number without aking care of all the details
-    CUDA_HOST_AND_DEVICE INLINE_FUNCTION
-    double getGaussNoHighPrec()
-    {
-      const double r=sqrt(-2*log(1-getUniformNoHighPrec()));
-      const double q=2*M_PI*getUniformNoHighPrec();
-      
-      return r*cos(q);
-    }
-  };
+  //     return r*cos(q);
+  //   }
+  // };
 }
 
 #endif
