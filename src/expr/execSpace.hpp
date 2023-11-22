4511833bdb92aeec02e1ee4e16b5460f0faa6005
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2023-11-22 19:21:52 +0100

    Added documentation

--- src/expr/execSpace.hpp
+++ src/expr/execSpace.hpp
@@ -11,20 +11,25 @@
 
 namespace nissa
 {
+  /// Stores the execution space of an expression
   struct ExecSpace
   {
+    /// Inner mask
     const uint32_t m;
     
+    /// Initialize the mask explictly
     explicit constexpr ExecSpace(const uint32_t& m) :
       m{m}
     {
     }
     
+    /// Initialize from a memory type
     explicit constexpr ExecSpace(const MemoryType& m) :
       m{(uint32_t)m}
     {
     }
     
+    /// Combines different execution spaces additively
     template <typename...T>
     requires(std::is_convertible_v<T,MemoryType> and...)
     explicit constexpr ExecSpace(const T&...t) :
@@ -32,30 +37,29 @@ namespace nissa
     {
     }
     
-    template <MemoryType MT>
-    constexpr bool runOn() const
-    {
-      return m&(int)MT;
-    }
-    
+    /// Combines additively with other execution space
     constexpr ExecSpace operator+(const ExecSpace& oth) const
     {
       return ExecSpace(m|oth.m);
     }
     
+    /// Conjunt with other execution space
     constexpr ExecSpace operator*(const ExecSpace& oth) const
     {
       return ExecSpace{m&oth.m};
     }
     
+    /// Determines if has a unique execution space
     constexpr bool hasUniqueExecSpace() const
     {
       return std::has_single_bit(m);
     }
     
+    /// Compares with another exectuion space
     constexpr auto operator<=>(const ExecSpace&) const = default;
   };
   
+  /// Gets the memory type to be used for a specific execution space
   template <ExecSpace E>
   requires(E.hasUniqueExecSpace())
   constexpr MemoryType getMemoryType()
@@ -67,8 +71,10 @@ namespace nissa
       GPU;
   }
   
+  /// Default execution space is the same of the default memory type
   constexpr ExecSpace defaultExecSpace(defaultMemoryType);
   
+  /// Defined to GPU execution space if device is supported
   constexpr ExecSpace maybeGpuExecSpace(maybeGpuMemoryType);
   
   /// Concept to catch a unique exec space
@@ -76,6 +82,7 @@ namespace nissa
   concept UniqueExecSpace=
   E.hasUniqueExecSpace();
   
+  /// Copy from two execution spaces
   template <ExecSpace TO,
 	    ExecSpace FROM>
   requires(UniqueExecSpace<TO> and UniqueExecSpace<FROM>)
@@ -86,16 +93,21 @@ namespace nissa
     memcpy<getMemoryType<TO>(),getMemoryType<FROM>()>(dst,src,count);
   }
   
+  /// Declares the execution space CPU
   constexpr ExecSpace execOnCPU(MemoryType::CPU);
   
+  /// Declares the execution space GPU
   constexpr ExecSpace execOnGPU(MemoryType::GPU);
   
+  /// Declares to be executable on both CPU and GPU
   constexpr ExecSpace execOnCPUAndGPU=
 	      execOnCPU+execOnGPU;
   
+  /// Declares that can execute on both CPU and GPU
   constexpr ExecSpace currentExecSpace=
 	      compilingForDevice?execOnGPU:execOnCPU;
   
+  /// Returns the memory manager suitable for the execution space
   template <ExecSpace ES>
   inline MemoryManager* memoryManager()
   {
