590bbd8c7dcb655f551f5106904b37902fe146a5
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2023-11-24 13:03:28 +0100

    Adding mu and nu

--- tools/conf_transform/create/create_U1.cpp
+++ tools/conf_transform/create/create_U1.cpp
@@ -195,7 +195,9 @@ void inMain(int narg,char **arg)
 	{
 	  int ivol,r;
 	  get_loclx_and_rank_of_coord(ivol,r,{0,0,d,0});
-	  if(r==rank) printf("A_mu_nu(t=0,x=0,y=%d,z=0); %lg\n",d,prop[d][mu][nu][RE]);
+	  double f=prop[d][mu][nu][RE];
+	  if(fabs(f)<1e-15) f=0;
+	  if(r==rank) printf("A_mu=%d_nu=%d(t=0,x=0,y=%d,z=0); %lg\n",mu,nu,d,f);
 	}
   nissa_free(prop);
   // NISSA_PARALLEL_LOOP(loclx,0,locVol)
