7d811ba457e7ef0f54b0ffbf700951eb99166701
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2023-11-27 18:44:20 +0100

    Added product copy and move constructor

--- src/expr/prod.hpp
+++ src/expr/prod.hpp
@@ -314,6 +314,22 @@ namespace nissa
       dynamicSizes(dynamicSizes)
     {
     }
+    
+    /// Copy constructor
+    CUDA_HOST_AND_DEVICE INLINE_FUNCTION constexpr
+    Producer(const Producer& oth)
+      requires(std::is_copy_constructible_v<std::decay_t<_E>> and...)
+      : subExprs{oth.subExprs.template get<Is>()...}
+    {
+    }
+    
+    /// Move constructor
+    CUDA_HOST_AND_DEVICE INLINE_FUNCTION constexpr
+    Producer(Producer&& oth)
+      requires(std::is_move_constructible_v<std::decay_t<_E>> and...)
+      : subExprs{std::move(oth.subExprs.template get<Is>())...}
+    {
+    }
   };
   
   template <DerivedFromNode..._E>
