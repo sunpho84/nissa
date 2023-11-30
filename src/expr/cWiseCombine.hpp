96377ce74050565e29cdec8669ba6227c398f135
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2023-11-30 13:11:06 +0100

    Fixed copy and move constructors of cWise

--- src/expr/cWiseCombine.hpp
+++ src/expr/cWiseCombine.hpp
@@ -183,22 +183,15 @@ namespace nissa
     }
     
     /// Copy constructor
-    CUDA_HOST_AND_DEVICE INLINE_FUNCTION constexpr
-    CWiseCombiner(const CWiseCombiner& oth)
-      requires(std::is_copy_constructible_v<std::decay_t<_E>> and...)
-      :subExprs{oth.subExprs},
-       dynamicSizes(oth.dynamicSizes)
+    INLINE_FUNCTION constexpr CUDA_HOST_AND_DEVICE
+    CWiseCombiner(const CWiseCombiner& oth) :
+      subExprs{{oth.subExprs.template get<Is>()}...}
     {
     }
     
     /// Move constructor
-    CUDA_HOST_AND_DEVICE INLINE_FUNCTION constexpr
-    CWiseCombiner(CWiseCombiner&& oth)
-      requires(std::is_move_constructible_v<std::decay_t<_E>> and...)
-      : subExprs{std::move(oth.subExprs)},
-	dynamicSizes(oth.dynamicSizes)
-    {
-    }
+    INLINE_FUNCTION constexpr 
+    CWiseCombiner(CWiseCombiner&& oth) = default;
   };
   
   template <typename Comb,
