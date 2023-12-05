e4704a937cc53cc1661b19e8666f00f7c12b1fc9
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2023-12-05 18:07:25 +0100

    Improved numeric conversion check

--- src/metaprogramming/typeConversion.hpp
+++ src/metaprogramming/typeConversion.hpp
@@ -11,6 +11,7 @@
 #include <type_traits>
 #include <utility>
 
+#include <metaprogramming/concepts.hpp>
 #include <metaprogramming/hasMember.hpp>
 #include <metaprogramming/inline.hpp>
 
@@ -23,53 +24,59 @@ namespace nissa
     /// Generic case
     template <typename From,
 	      typename To>
-    constexpr bool _isSafeNumericConversion(From*,To*)
+    constexpr bool _isExplicitlySafeNumericConversion(From*,To*)
     {
       return
 	std::is_same_v<From,To>;
     }
     
-#define DECLARE_SAFE_THE_NUMERIC_CONVERSION_FROM_TO(FROM,TO)		\
+#define DECLARE_EXPLICITLY_SAFE_THE_NUMERIC_CONVERSION_FROM_TO(FROM,TO)	\
     /*! Report whether the conversion from FROM to TO is numerically safe */ \
-    constexpr bool _isSafeNumericConversion(FROM*,TO*)			\
+    constexpr bool _isExplicitlySafeNumericConversion(FROM*,TO*)	\
     {									\
       return true;							\
     }
     
-    DECLARE_SAFE_THE_NUMERIC_CONVERSION_FROM_TO(bool,int32_t);
-    DECLARE_SAFE_THE_NUMERIC_CONVERSION_FROM_TO(int8_t,int32_t);
-    DECLARE_SAFE_THE_NUMERIC_CONVERSION_FROM_TO(int16_t,int32_t);
+    DECLARE_EXPLICITLY_SAFE_THE_NUMERIC_CONVERSION_FROM_TO(bool,int32_t);
+    DECLARE_EXPLICITLY_SAFE_THE_NUMERIC_CONVERSION_FROM_TO(int8_t,int32_t);
+    DECLARE_EXPLICITLY_SAFE_THE_NUMERIC_CONVERSION_FROM_TO(int16_t,int32_t);
     
-    DECLARE_SAFE_THE_NUMERIC_CONVERSION_FROM_TO(bool,int64_t);
-    DECLARE_SAFE_THE_NUMERIC_CONVERSION_FROM_TO(int8_t,int64_t);
-    DECLARE_SAFE_THE_NUMERIC_CONVERSION_FROM_TO(int16_t,int64_t);
-    DECLARE_SAFE_THE_NUMERIC_CONVERSION_FROM_TO(int32_t,int64_t);
-    DECLARE_SAFE_THE_NUMERIC_CONVERSION_FROM_TO(uint32_t,int64_t);
+    DECLARE_EXPLICITLY_SAFE_THE_NUMERIC_CONVERSION_FROM_TO(bool,int64_t);
+    DECLARE_EXPLICITLY_SAFE_THE_NUMERIC_CONVERSION_FROM_TO(int8_t,int64_t);
+    DECLARE_EXPLICITLY_SAFE_THE_NUMERIC_CONVERSION_FROM_TO(int16_t,int64_t);
+    DECLARE_EXPLICITLY_SAFE_THE_NUMERIC_CONVERSION_FROM_TO(int32_t,int64_t);
+    DECLARE_EXPLICITLY_SAFE_THE_NUMERIC_CONVERSION_FROM_TO(uint32_t,int64_t);
     
-    DECLARE_SAFE_THE_NUMERIC_CONVERSION_FROM_TO(bool,float);
-    DECLARE_SAFE_THE_NUMERIC_CONVERSION_FROM_TO(int8_t,float);
-    DECLARE_SAFE_THE_NUMERIC_CONVERSION_FROM_TO(int16_t,float);
-    DECLARE_SAFE_THE_NUMERIC_CONVERSION_FROM_TO(int32_t,float);
-    DECLARE_SAFE_THE_NUMERIC_CONVERSION_FROM_TO(uint32_t,float);
+    DECLARE_EXPLICITLY_SAFE_THE_NUMERIC_CONVERSION_FROM_TO(bool,float);
+    DECLARE_EXPLICITLY_SAFE_THE_NUMERIC_CONVERSION_FROM_TO(int8_t,float);
+    DECLARE_EXPLICITLY_SAFE_THE_NUMERIC_CONVERSION_FROM_TO(int16_t,float);
+    DECLARE_EXPLICITLY_SAFE_THE_NUMERIC_CONVERSION_FROM_TO(int32_t,float);
+    DECLARE_EXPLICITLY_SAFE_THE_NUMERIC_CONVERSION_FROM_TO(uint32_t,float);
     
-    DECLARE_SAFE_THE_NUMERIC_CONVERSION_FROM_TO(bool,double);
-    DECLARE_SAFE_THE_NUMERIC_CONVERSION_FROM_TO(int8_t,double);
-    DECLARE_SAFE_THE_NUMERIC_CONVERSION_FROM_TO(int16_t,double);
-    DECLARE_SAFE_THE_NUMERIC_CONVERSION_FROM_TO(int32_t,double);
-    DECLARE_SAFE_THE_NUMERIC_CONVERSION_FROM_TO(int64_t,double);
-    DECLARE_SAFE_THE_NUMERIC_CONVERSION_FROM_TO(uint32_t,double);
-    DECLARE_SAFE_THE_NUMERIC_CONVERSION_FROM_TO(uint64_t,double);
+    DECLARE_EXPLICITLY_SAFE_THE_NUMERIC_CONVERSION_FROM_TO(bool,double);
+    DECLARE_EXPLICITLY_SAFE_THE_NUMERIC_CONVERSION_FROM_TO(int8_t,double);
+    DECLARE_EXPLICITLY_SAFE_THE_NUMERIC_CONVERSION_FROM_TO(int16_t,double);
+    DECLARE_EXPLICITLY_SAFE_THE_NUMERIC_CONVERSION_FROM_TO(int32_t,double);
+    DECLARE_EXPLICITLY_SAFE_THE_NUMERIC_CONVERSION_FROM_TO(int64_t,double);
+    DECLARE_EXPLICITLY_SAFE_THE_NUMERIC_CONVERSION_FROM_TO(uint32_t,double);
+    DECLARE_EXPLICITLY_SAFE_THE_NUMERIC_CONVERSION_FROM_TO(uint64_t,double);
     
-#undef DECLARE_SAFE_THE_NUMERIC_CONVERSION_FROM_TO
+#undef DECLARE_EXPLICITLY_SAFE_THE_NUMERIC_CONVERSION_FROM_TO
   }
   
-  /// Report whether the conversion from From to To is numerically safe
+  /// Report whether the conversion from From to To is explicitly numerically safe
   ///
   /// Gives visibility to impl implementation
   template <typename From,
 	    typename To>
+  static constexpr bool isExplicitlySafeNumericConversion=
+    impl::_isExplicitlySafeNumericConversion((std::decay_t<From>*)nullptr,(std::decay_t<To>*)nullptr);
+  
+  /// Report whether the conversion from From to To is numerically safe
+  template <typename From,
+	    typename To>
   static constexpr bool isSafeNumericConversion=
-    impl::_isSafeNumericConversion((std::decay_t<From>*)nullptr,(std::decay_t<To>*)nullptr);
+    isExplicitlySafeNumericConversion<From,To> or HasCastOperatorTo<From,To>;
   
   /////////////////////////////////////////////////////////////////
   
