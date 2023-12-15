f1a817bcda9c6fa422718d05e3f4980b32feeb82
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2023-12-15 01:11:52 +0100

    Improved checksum init

--- src/io/checksum.hpp
+++ src/io/checksum.hpp
@@ -2,7 +2,7 @@
 #define _CHECKSUM_HPP
 
 #ifdef HAVE_CONFIG_H
-# include "config.hpp"
+# include <config.hpp>
 #endif
 
 #include <stdint.h>
@@ -16,12 +16,14 @@
 
 namespace nissa
 {
-  using Checksum=std::array<uint32_t,2>;
+  using Checksum=
+    std::array<uint32_t,2>;
   
-  CUDA_HOST_AND_DEVICE INLINE_FUNCTION
+  constexpr CUDA_HOST_AND_DEVICE INLINE_FUNCTION
   uint32_t crcValue(uint32_t c)
   {
-    constexpr uint32_t poly=0xedb88320L;
+    constexpr uint32_t poly=
+      0xedb88320L;
     
     for(uint32_t j=0;j<8;j++)
       c=(c&1)?poly^(c>>1):(c>>1);
