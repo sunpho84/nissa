676eaf24a144117c5b7639a086bf60c548304084
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2024-10-24 17:27:50 +0200

    Improved gaussian smearing test

--- tools/Gaussiansmear_test.cpp
+++ tools/Gaussiansmear_test.cpp
@@ -1,3 +1,5 @@
+#include <set>
+
 #include <nissa.hpp>
 
 using namespace nissa;
@@ -83,9 +85,9 @@ double expected_radius(double kappa,int nlevels,double plaq)
 //hold a tern to keep density
 struct dens_t
 {
-  int n;
-  double s;
-  dens_t() : n(0),s(0.0) {}
+  int64_t n{};
+  double s{};
+  double s2{};
 };
 
 typedef std::map<int,dens_t> mapdens_t;
@@ -93,17 +95,17 @@ typedef std::map<int,dens_t> mapdens_t;
 //compute the density distribution
 void compute_density(FILE *fout,color *source,coords_t *source_pos)
 {
-  mapdens_t density[glbSize[0]];
+  mapdens_t density;
   
   NISSA_LOC_VOL_LOOP(ivol)
     {
       int t=glbCoordOfLoclx[ivol][0];
       
       //get site norm
-      double n=0.0;
+      double s2=0.0;
       for(int ic=0;ic<NCOL;ic++)
 	for(int ri=0;ri<2;ri++)
-	  n+=sqr(source[ivol][ic][ri]);
+	  s2+=sqr(source[ivol][ic][ri]);
       
       //compute distance
       int rho=0.0;
@@ -115,31 +117,29 @@ void compute_density(FILE *fout,color *source,coords_t *source_pos)
 	}
       
       //increment
-      dens_t &it=density[t][rho];
+      dens_t &it=density[rho];
       it.n++;
-      it.s+=n;
+      it.s+=s2;
+      it.s2+=s2*s2;
     }
   
   //reduce and print
-  master_fprintf(fout," NDists %d\n",(int)density[0].size());
-
-  for(int t=0;t<glbSize[0];t++)
+  // master_fprintf(fout," NDists %d\n",(int)density.size());
+  
+  for(mapdens_t::iterator it=density.begin();it!=density.end();it++)
     {
-      master_fprintf(fout," t %d\n",t);
+      int r2=it->first;
+      dens_t d=it->second;
+      non_loc_reduce(&d.n);
+      non_loc_reduce(&d.s);
+      non_loc_reduce(&d.s2);
+      int64_t n=d.n;
+      double s=d.s/n;
+      double s2=d.s2/n-s*s;
       
-      for(mapdens_t::iterator it=density[t].begin();it!=density[t].end();it++)
-	{
-	  int r2=it->first;
-	  dens_t d=it->second;
-	  non_loc_reduce(&d.n);
-	  non_loc_reduce(&d.s);
-	  double n=d.n;
-	  double s=d.s/n;
-	  
-	  master_fprintf(fout,"%d %lg\n",r2,s);
-	}
-      master_fprintf(fout,"\n");
+      master_fprintf(fout,"%d %lg %lg\n",r2,s,sqrt(s2/n));
     }
+  master_fprintf(fout,"\n");
 }
 
 void in_main(int narg,char **arg)
@@ -170,37 +170,51 @@ void in_main(int narg,char **arg)
   ape_spatial_smear_conf(conf,conf,ape_smearing_alpha,ape_smearing_niters);
   
   //read Gaussian smearing pars
-  int ncoeffs;
   double kappa;
   read_str_double("Kappa",&kappa);
-  read_str_int("NCoeffs",&ncoeffs);
-  std::map<size_t,double> coeffs;
-  for(size_t iCoeff=0;iCoeff<ncoeffs;iCoeff++)
+  size_t nPoly;
+  {
+    int n;
+    read_str_int("NPoly",&n);
+    nPoly=n;
+  }
+  std::vector<std::map<size_t,double>> coeffs(nPoly);
+  
+  std::vector<color*> tot(nPoly);
+  for(auto& t : tot)
     {
-      double w;
-      read_double(&w);
-      int n;
-      read_int(&n);
-      
-      coeffs[n]+=w;
+      t=nissa_malloc("tot",locVol,color);
+      vector_reset(t);
     }
   
-  //output file
-  char out_path[1024];
-  read_str_str("Output",out_path,1024);
-  FILE *fout=open_file(out_path,"w");
-  master_fprintf(fout,"T %d\n",glbSize[0]);
-  master_fprintf(fout,"Kappa %lg\n",kappa);
-  for(const auto [n,w] : coeffs)
-    master_fprintf(fout,"%lg*H^%zu",w,n);
-  master_fprintf(fout,"\n");
+  std::set<size_t> nList;
+  std::vector<std::string> outPaths(nPoly);
+  for(size_t iPoly=0;iPoly<nPoly;iPoly++)
+    {
+      char outPath[1024];
+      read_str_str("Output",outPath,1024);
+      outPaths[iPoly]=outPath;
+      
+      int nCoeffs;
+      read_str_int("NCoeffs",&nCoeffs);
+      for(int iCoeff=0;iCoeff<nCoeffs;iCoeff++)
+	{
+	  double w;
+	  read_double(&w);
+	  int n;
+	  read_int(&n);
+	  
+	  coeffs[iPoly][n]+=w;
+	  
+	  nList.insert(n);
+	}
+    }
   
   //print spatial plaquette
   double plaqs[2];
   global_plaquette_lx_conf(plaqs,conf);
-  master_fprintf(fout,"TimePlaquette %16.16lg\n",plaqs[0]);
-  master_fprintf(fout,"SpatPlaquette %16.16lg\n",plaqs[1]);
-  master_fprintf(fout,"\n");
+  master_printf("TimePlaquette %16.16lg\n",plaqs[0]);
+  master_printf("SpatPlaquette %16.16lg\n",plaqs[1]);
   
   //set the source
   color *source=nissa_malloc("source",locVol+bord_vol,color);
@@ -220,14 +234,24 @@ void in_main(int narg,char **arg)
       if(rank==r) source[l][0][0]=1;
     }
     
-  color *tot=nissa_malloc("tot",locVol+bord_vol,color);
-  vector_reset(tot);
   size_t p=0;
-  for(const auto [n,w] : coeffs)
+  for(const auto n : nList)
     {
       gaussian_smearing(source,source,conf,kappa,n-p);
+      
+      for(size_t iPoly=0;iPoly<nPoly;iPoly++)
+	{
+	  const auto nw=coeffs[iPoly].find(n);
+	  if(nw!=coeffs[iPoly].end())
+	    {
+	      const auto& [n,w]=*nw;
+	      master_printf("Adding %lg*H^%zu (computed with %zu new steps) to poly %zu\n",w,n,n-p,iPoly);
+	      
+	      double_vector_summassign_double_vector_prod_double(tot[iPoly][0][0],source[0][0],w,locVol*sizeof(color)/sizeof(double));
+	    }
+	}
+      
       p=n;
-      double_vector_summassign_double_vector_prod_double(tot[0][0],source[0][0],w,locVol*sizeof(color)/sizeof(double));
     }
   
   // //compute gaussianity
@@ -246,12 +270,26 @@ void in_main(int narg,char **arg)
   // master_printf("\n");
       
   //     master_fprintf(fout," Smearlevel %d\n",ilev);
-  compute_density(fout,tot,source_pos);
+  //output file
+  for(size_t iPoly=0;iPoly<nPoly;iPoly++)
+    {
+      FILE* f=open_file(outPaths[iPoly],"w");
+      
+      // master_fprintf(f,"T %d\n",glbSize[0]);
+      // master_fprintf(f,"Kappa %lg\n",kappa);
+      // for(const auto [n,w] : coeffs[iPoly])
+      // 	master_fprintf(f,"%+lg*H^%zu",w,n);
+      // master_fprintf(f,"\n");
+      
+      master_fprintf(f,"\n");
+      
+      compute_density(f,tot[iPoly],source_pos);
+      
+      close_file(f);
+    }
   
-  //smear
-  
-  close_file(fout);
-  
+  for(size_t iPoly=0;iPoly<nPoly;iPoly++)
+    nissa_free(tot[iPoly]);
   nissa_free(source);
   nissa_free(conf);
 }
