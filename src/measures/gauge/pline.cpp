30ac8150fb6ce8584fe0f9db8e9b7794f767eb17
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2024-10-24 19:39:44 +0200

    Format string fixes

--- src/measures/gauge/pline.cpp
+++ src/measures/gauge/pline.cpp
@@ -208,9 +208,9 @@ namespace nissa
     if(rank==0)
       {
 	off_t nwr=fwrite(&itraj,sizeof(int),1,file);
-	if(nwr!=1) crash("wrote %d int instead of 1",nwr);
+	if(nwr!=1) crash("wrote %ld int instead of 1",nwr);
 	nwr=fwrite(tra,sizeof(double),2,file);
-	if(nwr!=2) crash("wrote %d doubles instead of 2",nwr);
+	if(nwr!=2) crash("wrote %ld doubles instead of 2",nwr);
       }
     else
       if(fseek(file,sizeof(int)+sizeof(complex),SEEK_CUR)) crash("seeking");
@@ -242,7 +242,7 @@ namespace nissa
       {
 	int nbytes_to_write=loc_data*sizeof(complex);
 	off_t nbytes_wrote=fwrite(loop,1,nbytes_to_write,file);
-	if(nbytes_wrote!=nbytes_to_write) crash("wrote %d bytes instead of %d",nbytes_wrote,nbytes_to_write);
+	if(nbytes_wrote!=nbytes_to_write) crash("wrote %ld bytes instead of %d",nbytes_wrote,nbytes_to_write);
       }
     
     //point to after the data
