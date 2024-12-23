3d534bfb6373ee2093380ea64a796d8c62d28212
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2024-12-23 01:04:55 +0100

    Removed cuda.cpp

--- src/base/cuda.hpp
+++ src/base/cuda.hpp
@@ -1,22 +1,44 @@
 #ifndef _CUDA_HPP
 #define _CUDA_HPP
 
-#ifndef EXTERN_CUDA
- #define EXTERN_CUDA extern
- #define INIT_TO(var)
-#else
- #define INIT_TO(var) =var
+#ifdef HAVE_CONFIG_H
+# include "config.hpp"
 #endif
+
+#include <routines/mpi_routines.hpp>
+
+#include <geometry/geometry_lx.hpp>
 
 namespace nissa
 {
-  EXTERN_CUDA int nCudaDevices;
-  EXTERN_CUDA int iCudaDevice;
+  /// Number of cuda device
+  inline int nCudaDevices;
   
-  void init_cuda();
+  /// Id of the current cuda device
+  inline int iCudaDevice;
+  
+  /// Properties of the curren device
+  inline cudaDeviceProp deviceProp;
+  
+  /// Initialize cuda
+  inline void init_cuda()
+  {
+    if(cudaGetDeviceCount(&nCudaDevices)!=cudaSuccess)
+      crash("no CUDA enabled device");
+    
+    printf("Number of CUDA enabled devices on rank[%d] (%s) : %d\n",rank,MPI_get_processor_name().c_str(),nCudaDevices);
+    for(int i=0;i<nCudaDevices;i++)
+      {
+	cudaGetDeviceProperties(&deviceProp,i);
+	printf(" rank %d CUDA Enabled device %d/%d: %d.%d\n",rank,i,nCudaDevices,deviceProp.major,deviceProp.minor);
+      }
+    //assumes that if we are seeing multiple gpus, there are nDevices ranks to attach to each of it
+    if(nCudaDevices!=1)
+      {
+	iCudaDevice=rank%nCudaDevices;
+	decript_cuda_error(cudaSetDevice(iCudaDevice),"Unable to set device %d",iCudaDevice);
+      }
+  }
 }
-
-#undef EXTERN_CUDA
-#undef INIT_TO
 
 #endif
