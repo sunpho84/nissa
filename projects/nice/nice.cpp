f178d95e2640ebb0c823e20df5b93d02dc8dad07
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2026-01-01 23:07:51 +0100

    Adding preliminary nice

---
+++ projects/nice/nice.cpp
@@ -0,0 +1,184 @@
+// Nissa Interface for Correlator Evaluation
+
+/**
+  The logic
+
+  Creation of the AST
+  -------------------
+  Within a script, one can define sources, operators, and functions,
+  which are multiplied or summed. This are represented in terms of an
+  AST, including function definitions and calls, assignement, etc.
+  
+  Execution of the AST
+  -------------------
+  When the AST is exectued, variables are
+  
+  Creation of the lines
+  -------------------------------
+   (contraction for now) triggers the
+  creation of the lines
+  
+**/
+
+#include <memory>
+#include <variant>
+
+#include <nissa.hpp>
+
+using namespace nissa;
+
+using CAction=
+  std::tuple<std::string,std::function<void()>,std::vector<std::string>>;
+
+/// Creates a source
+struct SourcePars
+{
+  static inline int glbId;
+  
+  int id;
+  
+  rnd_t noiseType;
+  
+  bool spinDilute;
+  
+  bool colDilute;
+  
+  SourcePars(const rnd_t& noiseType=RND_Z4,
+	     const bool& spinDilute=true,
+	     const bool& colDilute=false) :
+    id{glbId++},
+    noiseType{noiseType},
+    spinDilute{spinDilute},
+    colDilute{colDilute}
+  {
+  }
+};
+
+/// Select a time
+struct TimeSelectPars
+{
+  int t;
+};
+
+using ASTNodePars=
+  std::variant<std::monostate,
+		 SourcePars,
+		 TimeSelectPars>;
+/////////////////////////////////////////////////////////////////
+
+enum OpType{SUM,PROD};
+
+struct ASTNodeOp
+{
+  std::variant<ASTNodePars,std::pair<OpType,std::array<std::shared_ptr<ASTNodeOp>,2>>> subNodes;
+  
+  template <typename A>
+  ASTNodeOp(const A& a) :
+    subNodes(a)
+  {
+  }
+};
+
+std::shared_ptr<ASTNodeOp> source(const rnd_t& noiseType=RND_Z4,
+				  const bool& spinDilute=true,
+				  const bool& colDilute=false)
+{
+  return std::make_shared<ASTNodeOp>(SourcePars{noiseType,spinDilute,colDilute});
+}
+
+std::shared_ptr<ASTNodeOp> timeSelect(const int& t)
+{
+  return std::make_shared<ASTNodeOp>(TimeSelectPars{t});
+}
+
+// #define PROVIDE_AST_BINOP(A,B)						\
+//   std::shared_ptr<ASTNodeOp> operator A(const std::shared_ptr<ASTNodeOp>& op1, \
+// 					const std::shared_ptr<ASTNodeOp>& op2) \
+//   {									\
+//     return std::make_shared<ASTNodeOp>(std::make_pair(B,std::array<std::shared_ptr<ASTNodeOp>,2>{op1,op2})); \
+//   }
+
+// PROVIDE_AST_BINOP(+,SUM);
+// PROVIDE_AST_BINOP(*,PROD);
+
+// #undef PROVIDE_AST_BINOP
+
+enum LineT{QuarkLine,LepLine};
+
+struct Statement
+{
+  std::string out;
+  
+  LineT lineT;
+  
+  std::function<void(void)> eval;
+  
+  std::vector<std::string> in;
+};
+
+Statement operator*(const TimeSelectPars& pars,
+		    const Statement& oth)
+{
+  return {"DeltaT*"+oth.out,
+    oth.lineT,
+    [](){},
+    {oth.out}};
+}
+
+template <typename...Ts>
+struct Overload :
+  Ts...
+{
+  using Ts::operator()...;
+};
+
+template <typename...Ts>
+Overload(Ts...) -> Overload<Ts...>;
+
+int main()
+{
+  const auto eta=
+    source();
+  
+  // MASTER_PRINTF("%d %d\n",SourcePars::glbId,std::get_if<SourcePars>(&eta->pars)->id);
+  
+  // auto p=
+  //   timeSelect(0)*eta;
+  
+  // auto pr=
+  //   [](const auto& pr,
+  //      const auto& v)->void
+  //   {
+  //     auto npr=
+  // 	Overload
+  // 	{[](ASTNodePars pars)
+  // 	{
+  // 	  std::visit(Overload
+  // 		     {[](const std::monostate& dum)
+  // 		     {
+  // 		       CRASH("monostate should never appear");
+  // 		     },
+  // 			 [](const TimeSelectPars& tPars)
+  // 			 {
+  // 			   MASTER_PRINTF("Selecting time: %d\n",tPars.t);
+  // 			 },
+  // 			 [](const SourcePars& sPars)
+  // 			 {
+  // 			   MASTER_PRINTF("Source with diluted spin: %d\n",sPars.spinDilute);
+  // 			 }},pars);
+  // 	},
+  // 	 [&pr](const std::pair<OpType,std::array<std::shared_ptr<ASTNodeOp>,2>>& leafs)
+  // 	 {
+  // 	   MASTER_PRINTF("%s of: \n",(leafs.first==SUM)?"sum":"prod");
+  // 	   pr(pr,leafs.second[0]->subNodes);
+  // 	   pr(pr,leafs.second[1]->subNodes);
+  // 	 }
+  // 	};
+      
+  //     std::visit(npr,v);
+  //   };
+  
+  // pr(pr,p->subNodes);
+  
+  return 0;
+}
