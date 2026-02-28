06b86a11da2852135e2849c9e81a7ba9c0d315f7
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2026-02-28 23:50:36 +0100

    Disbaling ildg_to_Claudio

--- tools/conf_transform/convert/ildg_to_Claudio.cpp
+++ tools/conf_transform/convert/ildg_to_Claudio.cpp
@@ -33,162 +33,164 @@ std::pair<int,int> indrem(const int& iel_fr)
 
 int main(int narg,char **arg)
 {
-  char *in_conf_name,*out_conf_name;
+  // char *in_conf_name,*out_conf_name;
   
   //basic mpi initialization
   initNissa(narg,arg);
   
-  // if(nranks>1)
-  //   CRASH("cannot run in parallel");
+  CRASH("reimplement");
   
-  if(narg<7)
-    CRASH("use: %s T LX LY LZ file_in file_out",arg[0]);
+//   // if(nranks>1)
+//   //   CRASH("cannot run in parallel");
   
-  /// Set the global sizes
-  Coords _glbSize;
-  _glbSize[0]=atoi(arg[1]);
-  _glbSize[1]=atoi(arg[2]);
-  _glbSize[2]=atoi(arg[3]);
-  _glbSize[3]=atoi(arg[4]);
-  set_glbSize(_glbSize);
+//   if(narg<7)
+//     CRASH("use: %s T LX LY LZ file_in file_out",arg[0]);
   
-  in_conf_name=arg[5];
-  out_conf_name=arg[6];
+//   /// Set the global sizes
+//   Coords _glbSize;
+//   _glbSize[0]=atoi(arg[1]);
+//   _glbSize[1]=atoi(arg[2]);
+//   _glbSize[2]=atoi(arg[3]);
+//   _glbSize[3]=atoi(arg[4]);
+//   set_glbSize(_glbSize);
   
-  //Init the MPI grid
-  initGrid(0,0);
+//   in_conf_name=arg[5];
+//   out_conf_name=arg[6];
   
-  //////////////////////////////// read the file /////////////////////////
+//   //Init the MPI grid
+//   initGrid(0,0);
   
-  LxField<quad_su3> in_conf("in_conf",WITH_HALO);
+//   //////////////////////////////// read the file /////////////////////////
   
-  //init messages
-  ILDG_message mess;
-  ILDG_message_init_to_last(&mess);
+//   LxField<quad_su3> in_conf("in_conf",WITH_HALO);
   
-  read_ildg_gauge_conf(in_conf,in_conf_name,&mess);
-  // int file_size=get_file_size(in_conf_name);
-  // MASTER_PRINTF("File size: %d\n",file_size);
+//   //init messages
+//   ILDG_message mess;
+//   ILDG_message_init_to_last(&mess);
   
-  int itraj=0;
-  for(ILDG_message *cur_mess=&mess;cur_mess->is_last==false;cur_mess=cur_mess->next)
-    if(strcasecmp(cur_mess->name,"ConfID")==0 or
-       strcasecmp(cur_mess->name,"MD_traj")==0)
-      sscanf(cur_mess->data,"%d",&itraj);
+//   read_ildg_gauge_conf(in_conf,in_conf_name,&mess);
+//   // int file_size=get_file_size(in_conf_name);
+//   // MASTER_PRINTF("File size: %d\n",file_size);
   
-  //free all messages
-  ILDG_message_free_all(&mess);
-  MASTER_PRINTF("Traj ID: %d\n",itraj);
+//   int itraj=0;
+//   for(ILDG_message *cur_mess=&mess;cur_mess->is_last==false;cur_mess=cur_mess->next)
+//     if(strcasecmp(cur_mess->name,"ConfID")==0 or
+//        strcasecmp(cur_mess->name,"MD_traj")==0)
+//       sscanf(cur_mess->data,"%d",&itraj);
   
-  ////////////////////////////// convert conf ////////////////////////////
+//   //free all messages
+//   ILDG_message_free_all(&mess);
+//   MASTER_PRINTF("Traj ID: %d\n",itraj);
   
-  LxField<quad_su3> temp("temp",WITH_HALO);
-  vector_remap_t(locVol,indrem).remap(temp.template getPtr<defaultMemorySpace>(),
-	    in_conf.template getSurelyWithSpaceTimeLayout<SpaceTimeLayout::CPU>().template getPtr<defaultMemorySpace>(),
-	    sizeof(quad_su3));
+//   ////////////////////////////// convert conf ////////////////////////////
   
-  LxField<quad_su3> out_conf("out_conf");
-  out_conf=temp.getSurelyReadableOn<MemorySpace::CPU>().getSurelyWithSpaceTimeLayout<SpaceTimeLayout::CPU>();
+//   LxField<quad_su3> temp("temp",WITH_HALO);
+//   vector_remap_t(locVol,indrem).remap(temp.template getPtr<defaultMemorySpace>(),
+// 	    in_conf.template getSurelyWithSpaceTimeLayout<SpaceTimeLayout::CPU>().template getPtr<defaultMemorySpace>(),
+// 	    sizeof(quad_su3));
   
-  //open the file
-  FILE *fout;
+//   LxField<quad_su3> out_conf("out_conf");
+//   out_conf=temp.getSurelyReadableOn<MemorySpace::CPU>().getSurelyWithSpaceTimeLayout<SpaceTimeLayout::CPU>();
   
-  if(rank==0)
-    {
-      fout=open_file(out_conf_name,"w");
-      if(fout==NULL) CRASH("while opening %s",out_conf_name);
+//   //open the file
+//   FILE *fout;
+  
+//   if(rank==0)
+//     {
+//       fout=open_file(out_conf_name,"w");
+//       if(fout==NULL) CRASH("while opening %s",out_conf_name);
       
-      fprintf(fout,"4 %d %d %d %d %d ",glbSize[0],glbSize[1],glbSize[2],glbSize[3],itraj);
-    }
+//       fprintf(fout,"4 %d %d %d %d %d ",glbSize[0],glbSize[1],glbSize[2],glbSize[3],itraj);
+//     }
   
-  char res[2*MD5_DIGEST_LENGTH+1]="";
-#ifdef USE_SSL
+//   char res[2*MD5_DIGEST_LENGTH+1]="";
+// #ifdef USE_SSL
   
-  if(rank==0)
-    MD5_Init(&mdContext);
+//   if(rank==0)
+//     MD5_Init(&mdContext);
   
-  int prevRank=0;
-  for(int64_t gvol=0;gvol<glbVol;gvol++)
-    {
-      const auto [irank,ivol]=getLoclxAndRankOfCoords(glbCoordOfGlblx(gvol));
-      if(prevRank!=irank)
-	{
-	  if(prevRank==rank) MPI_Send(&mdContext,sizeof(MD5_CTX),MPI_CHAR,irank,12,MPI_COMM_WORLD);
-	  if(irank==rank) MPI_Recv(&mdContext,sizeof(MD5_CTX),MPI_CHAR,prevRank,12,MPI_COMM_WORLD,MPI_STATUSES_IGNORE);
-	}
-      prevRank=irank;
+//   int prevRank=0;
+//   for(int64_t gvol=0;gvol<glbVol;gvol++)
+//     {
+//       const auto [irank,ivol]=getLoclxAndRankOfCoords(glbCoordOfGlblx(gvol));
+//       if(prevRank!=irank)
+// 	{
+// 	  if(prevRank==rank) MPI_Send(&mdContext,sizeof(MD5_CTX),MPI_CHAR,irank,12,MPI_COMM_WORLD);
+// 	  if(irank==rank) MPI_Recv(&mdContext,sizeof(MD5_CTX),MPI_CHAR,prevRank,12,MPI_COMM_WORLD,MPI_STATUSES_IGNORE);
+// 	}
+//       prevRank=irank;
       
-      if(rank==irank)
-	for(int mu=0;mu<NDIM;mu++)
-	  for(int icol=0;icol<NCOL;icol++)
-	    for(int jcol=0;jcol<NCOL;jcol++)
-	      {
-		union
-		{
-		  complex o;
-		  std::array<double,2> d2;
-		};
-		complex_copy(o,out_conf[ivol][mu][icol][jcol]);
+//       if(rank==irank)
+// 	for(int mu=0;mu<NDIM;mu++)
+// 	  for(int icol=0;icol<NCOL;icol++)
+// 	    for(int jcol=0;jcol<NCOL;jcol++)
+// 	      {
+// 		union
+// 		{
+// 		  complex o;
+// 		  std::array<double,2> d2;
+// 		};
+// 		complex_copy(o,out_conf[ivol][mu][icol][jcol]);
 		
-		if(nativeEndianness==LittleEndian)
-		  fixToNativeEndianness<BigEndian>(d2);
+// 		if(nativeEndianness==LittleEndian)
+// 		  fixToNativeEndianness<BigEndian>(d2);
 		
-		double od[2]{o[RE],o[IM]};
+// 		double od[2]{o[RE],o[IM]};
 		
-		MD5_Update(&mdContext,od,sizeof(complex));
-	      }
-    }
+// 		MD5_Update(&mdContext,od,sizeof(complex));
+// 	      }
+//     }
   
-  if(prevRank!=0)
-    {
-      if(prevRank==rank) MPI_Send(&mdContext,sizeof(MD5_CTX),MPI_CHAR,0,12,MPI_COMM_WORLD);
-      if(0==rank) MPI_Recv(&mdContext,sizeof(MD5_CTX),MPI_CHAR,prevRank,12,MPI_COMM_WORLD,MPI_STATUSES_IGNORE);
-    }
+//   if(prevRank!=0)
+//     {
+//       if(prevRank==rank) MPI_Send(&mdContext,sizeof(MD5_CTX),MPI_CHAR,0,12,MPI_COMM_WORLD);
+//       if(0==rank) MPI_Recv(&mdContext,sizeof(MD5_CTX),MPI_CHAR,prevRank,12,MPI_COMM_WORLD,MPI_STATUSES_IGNORE);
+//     }
   
-  if(rank==0)
-    MD5_Final(c,&mdContext);
-  MPI_Bcast(c,sizeof(c),MPI_CHAR,0,MPI_COMM_WORLD);
+//   if(rank==0)
+//     MD5_Final(c,&mdContext);
+//   MPI_Bcast(c,sizeof(c),MPI_CHAR,0,MPI_COMM_WORLD);
   
-  for(int r=0;r<MD5_DIGEST_LENGTH;r++)
-    sprintf(&(res[2*r]), "%02x", c[r]);
-  res[2*MD5_DIGEST_LENGTH]='\0';
+//   for(int r=0;r<MD5_DIGEST_LENGTH;r++)
+//     sprintf(&(res[2*r]), "%02x", c[r]);
+//   res[2*MD5_DIGEST_LENGTH]='\0';
   
-  MASTER_PRINTF("res: %s\n",res);
-#endif
+//   MASTER_PRINTF("res: %s\n",res);
+// #endif
   
-  if(rank==0)
-    {
-      fprintf(fout,"%s\n",res);
-      fclose(fout);
-    }
-  MPI_Barrier(MPI_COMM_WORLD);
+//   if(rank==0)
+//     {
+//       fprintf(fout,"%s\n",res);
+//       fclose(fout);
+//     }
+//   MPI_Barrier(MPI_COMM_WORLD);
   
-  for(int iRank=0;iRank<nranks;iRank++)
-    {
-      if(rank==iRank)
-	{
-	  fout=fopen(out_conf_name,"a");
-	  if(fout==nullptr)
-	    CRASH("Unable to open for append %s",out_conf_name);
+//   for(int iRank=0;iRank<nranks;iRank++)
+//     {
+//       if(rank==iRank)
+// 	{
+// 	  fout=fopen(out_conf_name,"a");
+// 	  if(fout==nullptr)
+// 	    CRASH("Unable to open for append %s",out_conf_name);
 	  
-	  //write the data
-	  for (int ivol = 0; ivol < locVol; ivol++)
-	    for(int mu=0;mu<NDIM;mu++)
-	      {
-		su3 A;
-		su3_copy(A,out_conf[ivol][mu]);
-		if(fwrite(A,sizeof(su3),1,fout)!=1)
-		  CRASH("Problems in writing Su3 matrix");
-	      }
+// 	  //write the data
+// 	  for (int ivol = 0; ivol < locVol; ivol++)
+// 	    for(int mu=0;mu<NDIM;mu++)
+// 	      {
+// 		su3 A;
+// 		su3_copy(A,out_conf[ivol][mu]);
+// 		if(fwrite(A,sizeof(su3),1,fout)!=1)
+// 		  CRASH("Problems in writing Su3 matrix");
+// 	      }
 	  
-	  fclose(fout);
-	}
-      MPI_Barrier(MPI_COMM_WORLD);
-    }
+// 	  fclose(fout);
+// 	}
+//       MPI_Barrier(MPI_COMM_WORLD);
+//     }
   
-  ///////////////////////////////////////////
+//   ///////////////////////////////////////////
   
-  closeNissa();
+//   closeNissa();
   
   return 0;
 }
